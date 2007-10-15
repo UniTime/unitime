7feff6e0164bb61e0eedb2f555426af3014099cd
tomas13 <tomas13@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2007-10-15 20:21:50 +0000

    MySQL versions of existing change scripts

---
+++ trunk/Documentation/Database/MySQL/Changes/00 Package Renames.sql
@@ -0,0 +1,24 @@
+/*
+ * UniTime 3.0 (University Course Timetabling & Student Sectioning Application)
+ * Copyright (C) 2007, UniTime.org
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+update `timetable`.`solver_info_def` set `implementation`=replace(`implementation`,'edu.purdue.smas','org.unitime');
+update `timetable`.`solver_parameter_def` set `default_value`=replace(`default_value`,'edu.purdue.smas','org.unitime') where `default_value` like 'edu.purdue.smas%';
+update `timetable`.`change_log` set `obj_type`=replace(`obj_type`,'edu.purdue.smas','org.unitime');
+
+commit;
---
+++ trunk/Documentation/Database/MySQL/Changes/03 On fly student sectioning.sql
@@ -0,0 +1,47 @@
+/*
+ * UniTime 3.0 (University Course Timetabling & Student Sectioning Application)
+ * Copyright (C) 2007, UniTime.org
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+select `next_hi` into @id from `timetable`.`hibernate_unique_key`;
+
+select max(`ord`)+1 into @ord from `timetable`.`solver_parameter_group`;
+
+insert into `timetable`.`solver_parameter_group`
+	(`uniqueid`, `name`, `description`, `condition`, `ord`) values 
+	(@id, 'OnFlySectioning', 'On Fly Student Sectioning', '', @ord);
+	
+insert into `timetable`.`solver_parameter_def`
+	(`uniqueid`, `name`, `default_value`, `description`, `type`, `ord`, `visible`, `solver_param_group_id`) values
+	(@id+1, 'OnFlySectioning.Enabled', 'false', 'Enable on fly sectioning (if enabled, students will be resectioned after each iteration)', 'boolean', 0, 1, @id),
+	(@id+2, 'OnFlySectioning.Recursive', 'true', 'Recursively resection lectures affected by a student swap', 'boolean', 1, 1, @id),
+	(@id+3, 'OnFlySectioning.ConfigAsWell', 'false', 'Resection students between configurations as well', 'boolean', 2, 1, @id); 
+
+update `timetable`.`hibernate_unique_key` set `next_hi`=`next_hi`+4;
+		
+/* 
+-- Uncomment the following lines to enable on fly student sectioning in the default solver configuration
+
+select s.`uniqueid` into @ds from `timetable`.`solver_predef_setting` s where s.`name`='Default.Solver';
+insert into `timetable`.`solver_parameter`
+	(`uniqueid`, `value`, `solver_param_def_id`, `solution_id`, `solver_predef_setting_id`) values
+	(@id+4, 'on', @id+1, NULL, @ds);
+update `timetable`.`hibernate_unique_key` set `next_hi`=`next_hi`+1;
+
+*/
+
+commit;
