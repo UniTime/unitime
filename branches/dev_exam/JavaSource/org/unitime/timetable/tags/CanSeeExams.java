78ae603f68c0e31c91a5ebe6ac9875f09846e86c
tomas13 <tomas13@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-02-08 19:32:15 +0000

    added tag canSeeExams (content of the tag is included only when the current user can see exams)

---
+++ branches/dev_exam/JavaSource/org/unitime/timetable/tags/CanSeeExams.java
@@ -0,0 +1,49 @@
+/*
+ * UniTime 3.0 (University Course Timetabling & Student Sectioning Application)
+ * Copyright (C) 2007, UniTime.org, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+package org.unitime.timetable.tags;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.unitime.commons.User;
+import org.unitime.commons.web.Web;
+import org.unitime.timetable.model.Session;
+import org.unitime.timetable.model.TimetableManager;
+
+
+/**
+ * @author Tomas Muller
+ */
+public class CanSeeExams extends TagSupport {
+	
+    public int doStartTag() {
+        try {
+            User user = Web.getUser(pageContext.getSession());
+            TimetableManager manager = TimetableManager.getManager(user);
+            Session session = Session.getCurrentAcadSession(user);
+            if (manager.canSeeExams(session, user))
+                return EVAL_BODY_INCLUDE;
+        } catch (Exception e) {}
+        return SKIP_BODY;
+    }
+    
+    public int doEndTag() {
+        return EVAL_PAGE;
+    }
+}
