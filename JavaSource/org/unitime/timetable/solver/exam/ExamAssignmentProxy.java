3d501133d20021d87562beedf8014f34dfadb337
Tomáš Müller <muller@unitime.org>
2008-02-25 20:44:53 +0000

    Examination Timetabling 	- Exam Detail page, assignment section -- added conflicts table

--- JavaSource/org/unitime/timetable/solver/exam/ExamAssignmentProxy.java
+++ JavaSource/org/unitime/timetable/solver/exam/ExamAssignmentProxy.java
@@ -2,4 +2,5 @@ package org.unitime.timetable.solver.exam;
 
 public interface ExamAssignmentProxy {
     public ExamAssignment getAssignment(long examId);
+    public ExamAssignmentInfo getAssignmentInfo(long examId);
 }
