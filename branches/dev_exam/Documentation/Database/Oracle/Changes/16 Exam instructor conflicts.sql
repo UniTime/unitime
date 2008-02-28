e4d54fa306327bcf9356b2dc9066b0f3321fb4fd
tomas13 <tomas13@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-02-28 22:19:48 +0000

    Examination Timetabling 	- added instructor conflicts (direct, >2 exams a day, back-to-back) into the database

---
+++ branches/dev_exam/Documentation/Database/Oracle/Changes/16 Exam instructor conflicts.sql
@@ -0,0 +1,49 @@
+/*
+ * UniTime 3.0 (University Course Timetabling & Student Sectioning Application)
+ * Copyright (C) 2007, UniTime.org
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+/**
+ * Add relation between exam-conflict table and instructors (M:N)
+ **/
+ 
+create table xconflict_instructor (
+	conflict_id number(20,0) constraint nn_xconflict_in_conf  not null,
+	instructor_id number(20,0) constraint nn_xconflict_in_student  not null
+);
+
+alter table xconflict_instructor
+  add constraint pk_xconflict_instructor primary key (conflict_id, instructor_id);
+
+alter table xconflict_instructor
+  add constraint fk_xconflict_in_conf foreign key (conflict_id)
+  references xconflict (uniqueid) on delete cascade;
+
+alter table xconflict_instructor
+  add constraint fk_xconflict_in_instructor foreign key (instructor_id)
+  references departmental_instructor (uniqueid) on delete cascade;
+  
+alter table exam add assigned_pref varchar2(100);
+  
+/*
+ * Update database version
+ */
+
+update application_config set value='16' where name='tmtbl.db.version';
+
+commit;
