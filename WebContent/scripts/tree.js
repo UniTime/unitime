8778356f78ce8853307310f0c307ed77f573f149
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2008-03-06 15:47:29 +0000

    Timetabling menu -- memorize opened items using cookies and few other minor improvements

--- WebContent/scripts/tree.js
+++ WebContent/scripts/tree.js
@@ -17,6 +17,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+var defaultMenuItems = "";
+
 function displayLoading() {
 	if (parent.__idContentFrame && parent.__idContentFrame.displayElement) {
 		parent.__idContentFrame.displayElement('contentMain', false);
@@ -25,42 +27,105 @@ function displayLoading() {
 }
 
 function toggle(item) {
-	obj=document.getElementById("__idMenuDiv"+item);
-	visible=(obj.style.display!="none")
-	img=document.getElementById("__idMenuImg" + item);
-	menu=document.getElementById("__idMenu" + item);
-	if (visible) {obj.style.display="none";img.src="images/expand_node_btn.gif";menu.title='Expand '+menu.name;}
-	else {obj.style.display="block";img.src="images/collapse_node_btn.gif";menu.title='Collapse '+menu.name;}
+	var obj=document.getElementById("__idMenuDiv"+item);
+	var img=document.getElementById("__idMenuImg"+item);
+	var visible=(obj.style.display!="none");
+	setToggle(item,!visible);
+	if (visible) {
+		obj.style.display="none";
+		img.src="images/expand_node_btn.gif";
+		img.title='Expand '+img.name;
+	} else {
+		obj.style.display="block";
+		img.src="images/collapse_node_btn.gif";
+		img.title='Collapse '+img.name;
+	}
 }
+
 function menu_item(id,name,title,page,type) {
+	menu_item(id,name,title,page,type,'__idContentFrame');
+}
+
+function menu_item(id,name,title,page,type,target) {
+	if (hasMenuCookie())
+		type = (getToggle(id)?'collapse':'expand');
+	else if (type=='collapse') defaultMenuItems += '<'+id+'>';
 	document.write('<div style="margin:5px;margin-right:0;">');
-	document.write('<A style="border:0;background:0" id="__idMenu'+id+'" href="javascript:toggle(\''+id+'\')" name="'+name+'" title="Expand '+name+'">');
-	document.write('<img id="__idMenuImg'+id+'" border="0" src="images/'+type+'_node_btn.gif" align="absmiddle"></A>');
-	if (page=='')
-		document.write('&nbsp;<span title="'+title+'">'+name+'</span><br>');
-	else
-		document.write('&nbsp;<A onClick="displayLoading();" target="__idContentFrame" '+(page==''?'':'href="'+page+'" ')+'title="'+title+'" >'+name+'</A><br>');
+	document.write('<img border="0" src="images/'+type+'_node_btn.gif" align="absmiddle" '+
+		'id="__idMenuImg'+id+'" title="Expand '+name+'" name="'+name+'" '+
+		'onMouseOver="javascript:this.style.cursor=\'hand\';this.style.cursor=\'pointer\';" '+
+		'onclick="javascript:toggle(\''+id+'\')">');
+	makelink(page,name,title,target);
 	document.write('</div>');
 	document.write('<div ID="__idMenuDiv'+id+'" style="display:'+(type=='collapse'?'block':'none')+';position:relative;margin-left:18px;">');
 }
+
 function leaf_item(name,title,page) {
 	leaf_item(name,title,page, '');
 }
+
 function leaf_item(name,title,page,target) {
-	tgt = '__idContentFrame';
-	onClick = 'onClick="displayLoading();"';
-	if (target!='' && target!=null) {
-		tgt = target;
-		onClick = '';
-	}
 	document.write('<div style="margin:5px;margin-right:0;">');
 	document.write('<img border="0" src="images/end_node_btn.gif" align="absmiddle">');
-	if (page=='')
-		document.write('&nbsp;<span title="'+title+'">'+name+'</span><br>');
-	else
-		document.write('&nbsp;<A ' + onClick + ' target="' + tgt + '" '+(page==''?'':'href="'+page+'" ')+'title="'+title+'" >'+name+'</A><br>');
+	makelink(page,name,title,target);
 	document.write('</div>');
 }
+
 function enditem() {
 	document.write('</div>');
+}
+
+function makelink(page, name, title, target) {
+	if (page=='') {
+		document.write('&nbsp;<span title="'+title+'">'+name+'</span><br>');
+	} else { 
+		var onClick = '';
+		if (target==null || target=='') target='__idContentFrame';
+		if (target=='__idContentFrame') onClick='onClick=" if (!event.ctrlKey) displayLoading();"';
+		document.write('&nbsp;<A ' + onClick + ' target="' + target + '" href="'+page+'" title="'+title+'" >'+name+'</A>');
+	}
+}
+
+function setDefaultCookie(items) {
+	
+}
+
+function setMenuCookie(value) {
+	var expires = new Date();
+  	expires.setDate(expires.getDate()+30);
+	document.cookie = "menu="+value+"; expires="+expires.toGMTString();
+}
+
+function hasMenuCookie() {
+	return (document.cookie.length>0?document.cookie.indexOf("menu=")>=0:false);
+}
+
+function getMenuCookie() {
+	if (document.cookie.length>0) {
+		var c_start=document.cookie.indexOf("menu=");
+		if (c_start!=-1) {
+			c_start=c_start + 5;
+			var c_end=document.cookie.indexOf(";",c_start);
+    		if (c_end==-1) c_end=document.cookie.length;
+    		return unescape(document.cookie.substring(c_start,c_end));
+    	}
+    }
+	return "";
+}
+
+function setToggle(item, visible) {
+	var mc = getMenuCookie();
+	if (!hasMenuCookie()) mc = defaultMenuItems;
+	var ix = '<'+item+'>';
+	if (visible) {
+		mc += ix;
+	} else {
+		var idx = mc.indexOf(ix);
+		if (idx>=0) mc = mc.substring(0,idx) + mc.substring(idx+ix.length);
+	}
+	setMenuCookie(mc);
+}
+
+function getToggle(item) {
+	return (getMenuCookie().indexOf('<'+item+'>')>=0);
 }
