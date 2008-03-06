8f6d82d44c46113ce071a284ea5fb6f4081aeedb
tomas13 <tomas13@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-03-06 05:40:56 +0000

    MySQL version of adding location permanent id was incorrect, following changes were made 	- there is no function to_char in mysql, funtion cast needs to be used instead 	- there is no rownum in mysql, however variables or auto increments can be used instead 	- permanent id was not populated using id generator (hibernate_unique_key table) 	- alter table has a different syntax (column type has to be included)

--- trunk/Documentation/Database/MySQL/Changes/18 Location perm id add.sql
+++ trunk/Documentation/Database/MySQL/Changes/18 Location perm id add.sql
@@ -26,16 +26,18 @@ alter table non_university_location add permanent_id decimal(20,0);
 create table temp_perm_id
 (
   external_uid varchar(40) not null,
-  perm_id      decimal(20,0) not null
-);
+  perm_id      decimal(20,0) not null 
+) engine memory;
 
 -- populate temp_perm_id table
-insert into temp_perm_id tpi
-select s.eid, rownum from (select distinct r.external_uid as eid 
-from room r
+insert into temp_perm_id 
+select s.eid, @id := @id + 1 from (select distinct r.external_uid as eid 
+from room r, (select @id := next_hi from hibernate_unique_key) x
 where r.external_uid is not null
-union (select distinct to_char(r1.uniqueid) as eid from room r1 where r1.external_uid is null)
-union (select distinct to_char(nul.uniqueid) as eid from non_university_location nul)) s;
+union (select distinct cast(r1.uniqueid as char) as eid from room r1 where r1.external_uid is null)
+union (select distinct cast(nul.uniqueid as char) as eid from non_university_location nul)) s;
+
+update hibernate_unique_key set next_hi = (select max(perm_id)+1 from temp_perm_id);
 
 -- populate  permanent_id for rooms with external ids
 update room r
@@ -44,20 +46,19 @@ where r.external_uid is not null;
 
 -- populate  permanent_id for rooms with out external ids
 update room r
-set r.permanent_id = (select tpi.perm_id from temp_perm_id tpi where tpi.external_uid = to_char(r.uniqueid))
+set r.permanent_id = (select tpi.perm_id from temp_perm_id tpi where tpi.external_uid = cast(r.uniqueid as char))
 where r.external_uid is null;
 
 -- populate  permanent_id for non_university_locations
 update non_university_location nul
-set nul.permanent_id = (select tpi.perm_id from temp_perm_id tpi where tpi.external_uid = to_char(nul.uniqueid));
+set nul.permanent_id = (select tpi.perm_id from temp_perm_id tpi where tpi.external_uid = cast(nul.uniqueid as char));
 
 -- drop temp_perm_id table
 drop table temp_perm_id;
 
 -- Add/modify columns 
-alter table room modify permanent_id not null;
-alter table non_university_location modify permanent_id not null;
-
+alter table room modify permanent_id decimal(20,0) not null;
+alter table non_university_location modify permanent_id decimal(20,0) not null;
   
 /*
  * Update database version
