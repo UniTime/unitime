e5c84d245b662e022333fe555f7493aa14bc55ab
says0528 <says0528@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-04-19 18:42:30 +0000

    Java program that can be run to load any xml input file the data import page can load.

---
+++ trunk/JavaSource/org/unitime/timetable/util/ImportXmlFile.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.unitime.timetable.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import net.sf.cpsolver.ifs.util.ToolBox;
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+import org.unitime.commons.Debug;
+import org.unitime.commons.hibernate.util.HibernateUtil;
+import org.unitime.timetable.form.DataImportForm;
+
+/**
+ * @author says
+ *
+ */
+public class ImportXmlFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+            ToolBox.configureLogging();
+            HibernateUtil.configureHibernate(new Properties());
+         	String fileName = args[0];
+         	Debug.info("filename = " + fileName);
+        	FileInputStream fis = null;
+        	try {
+                fis = new FileInputStream(fileName);
+        		Document document = (new SAXReader()).read(fis);
+                DataImportForm.importDocument(document, null);           
+			} catch (IOException e) {
+			    throw e;
+			} finally {
+			    if (fis != null) {
+			        try { fis.close(); } catch (IOException e) {}
+			    }
+			}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+}
