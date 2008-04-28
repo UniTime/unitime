27a013191d06714bccff09625d5f463f446858b8
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2008-04-28 23:45:58 +0000

    Room availability interface  - added ability to distinguish between evening and final exams  - added academic session to the request  - added ability to execute a stored procedure as a request, and a stored function as a response  	default request: { call room_avail_interface.request(?) } 	default response: {? = call room_avail_interface.response()}

--- JavaSource/org/unitime/timetable/interfaces/RoomAvailabilityInterface.java
+++ JavaSource/org/unitime/timetable/interfaces/RoomAvailabilityInterface.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 
+import org.unitime.timetable.ApplicationProperties;
+import org.unitime.timetable.model.Session;
+
 public interface RoomAvailabilityInterface {
-    public static final String sExamType = "exam";
-    public static final String sClassType = "class";
+    public static final String sEveningExamType = ApplicationProperties.getProperty("tmtbl.room.availability.eventType.eveningExam","Evening Exam");
+    public static final String sFinalExamType = ApplicationProperties.getProperty("tmtbl.room.availability.eventType.finalExam","Final Exam");
+    public static final String sClassType = ApplicationProperties.getProperty("tmtbl.room.availability.eventType.class","Course Activity");
 
     public Collection<TimeBlock> getRoomAvailability(String roomExternalId, String buildingAbbv, String roomNbr, Date startTime, Date endTime, String[] excludeTypes);
-    public void activate(Date startTime, Date endTime);
+    public void activate(Session session, Date startTime, Date endTime);
     
     public void startService();
     public void stopService();
