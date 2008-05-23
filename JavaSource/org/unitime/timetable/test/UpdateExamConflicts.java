bee2cfc77187a8403fec54df7ce98214d9589fc9
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2008-05-23 21:27:08 +0000

    update (student/instructor) conflicts for all exams of the given problem

---
+++ JavaSource/org/unitime/timetable/test/UpdateExamConflicts.java
@@ -0,0 +1,98 @@
+/*
+ * UniTime 3.1 (University Timetabling Application)
+ * Copyright (C) 2007, UniTime.org, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+package org.unitime.timetable.test;
+
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.TreeSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
+import org.hibernate.Transaction;
+import org.unitime.commons.hibernate.util.HibernateUtil;
+import org.unitime.timetable.ApplicationProperties;
+import org.unitime.timetable.model.Exam;
+import org.unitime.timetable.model.Session;
+import org.unitime.timetable.model.dao._RootDAO;
+
+public class UpdateExamConflicts {
+    private static Log sLog = LogFactory.getLog(UpdateExamConflicts.class);
+    
+    public static void doUpdate(Long sessionId, Integer examType, org.hibernate.Session hibSession) {
+        for (Iterator i=new TreeSet(Exam.findAll(sessionId, examType)).iterator();i.hasNext();) {
+            Exam exam = (Exam)i.next();
+            sLog.info("Updating "+exam.getLabel());
+            try {
+                exam.updateConflicts(hibSession);
+            } catch (Exception e) {
+                sLog.error("Update of "+exam.getLabel()+" failed, reason: "+e.getMessage(),e);
+            }
+        }
+    }
+    
+    public static void main(String args[]) {
+        try {
+            Properties props = new Properties();
+            props.setProperty("log4j.rootLogger", "DEBUG, A1");
+            props.setProperty("log4j.appender.A1", "org.apache.log4j.ConsoleAppender");
+            props.setProperty("log4j.appender.A1.layout", "org.apache.log4j.PatternLayout");
+            props.setProperty("log4j.appender.A1.layout.ConversionPattern","%-5p %c{2}: %m%n");
+            props.setProperty("log4j.logger.org.hibernate","INFO");
+            props.setProperty("log4j.logger.org.hibernate.cfg","WARN");
+            props.setProperty("log4j.logger.org.hibernate.cache.EhCacheProvider","ERROR");
+            props.setProperty("log4j.logger.org.unitime.commons.hibernate","INFO");
+            props.setProperty("log4j.logger.net","INFO");
+            PropertyConfigurator.configure(props);
+            
+            HibernateUtil.configureHibernate(ApplicationProperties.getProperties());
+            
+            Session session = Session.getSessionUsingInitiativeYearTerm(
+                    ApplicationProperties.getProperty("initiative", "puWestLafayetteTrdtn"),
+                    ApplicationProperties.getProperty("year","2008"),
+                    ApplicationProperties.getProperty("term","Spr")
+                    );
+            if (session==null) {
+                sLog.error("Academic session not found, use properties initiative, year, and term to set academic session.");
+                System.exit(0);
+            } else {
+                sLog.info("Session: "+session);
+            }
+            
+            int examType = (ApplicationProperties.getProperty("type","final").equalsIgnoreCase("final")?Exam.sExamTypeFinal:Exam.sExamTypeMidterm);
+            
+            org.hibernate.Session hibSession = new _RootDAO().getSession();
+            Transaction tx = null;
+            try {
+                tx = hibSession.beginTransaction();
+                
+                doUpdate(session.getUniqueId(), examType, hibSession);
+                
+                tx.commit();
+            } catch (Exception e) {
+                if (tx!=null) tx.rollback();
+                throw e;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
