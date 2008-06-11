1c1ce2557c0cebeea7a5d0df2b7656666a92e553
zmullero <zmullero@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-06-11 03:41:30 +0000

    Added last screen for entering an event request

---
+++ trunk/JavaSource/org/unitime/timetable/action/EventAddInfoAction.java
@@ -0,0 +1,63 @@
+package org.unitime.timetable.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessages;
+import org.unitime.commons.web.Web;
+import org.unitime.timetable.form.EventAddInfoForm;
+
+public class EventAddInfoAction extends Action {
+
+	public ActionForward execute(
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+	
+		EventAddInfoForm myForm = (EventAddInfoForm) form;
+		
+//Verification of user being logged in
+		if (!Web.isLoggedIn( request.getSession() )) {
+            throw new Exception ("Access Denied.");
+        }		
+
+//Operations		
+		String iOp = myForm.getOp();
+		
+		if (iOp!=null) {
+			
+			if ("Back".equals(iOp)) {
+				myForm.save(request.getSession());
+				request.setAttribute("back", "eventAddInfo");
+				return mapping.findForward("back");
+			}
+
+			if ("Submit".equals(iOp)) {
+	        	ActionMessages errors = myForm.validate(mapping, request);
+	        	if (!errors.isEmpty()) {
+	        		saveErrors(request, errors);
+	        	} else {
+	        		myForm.submit(request.getSession());
+	        		myForm.cleanSessionAttributes(request.getSession());
+	        		System.out.println("Main Contact Email: "+myForm.getMainContactEmail());
+	        		System.out.println("Event Name: " + myForm.getEventName());
+	        		return mapping.findForward("eventList");
+	        	}
+			}
+			
+			if ("Cancel Event".equals(iOp)) {
+        		myForm.cleanSessionAttributes(request.getSession());				
+				return mapping.findForward("eventList");
+			}
+		
+		}
+		
+		return mapping.findForward("show");
+	}
+	
+}
