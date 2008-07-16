9ac87e6d60320beccb11ee4aaae07b8af15b5781
zmullero <mullerova@unitime.org>
2008-07-16 01:16:41 +0000

    -- SponsoringOrgEdit (or Add) - check if there already is an organization with the same name (if there is one, write an error message) -- Added copyright information at the beginning of java files

--- JavaSource/org/unitime/timetable/action/SponsoringOrgEditAction.java
+++ JavaSource/org/unitime/timetable/action/SponsoringOrgEditAction.java
@@ -1,3 +1,22 @@
+/*
+ * UniTime 3.1 (University Timetabling Application)
+ * Copyright (C) 2008, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 package org.unitime.timetable.action;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +27,16 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessages;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.unitime.timetable.form.SponsoringOrgEditForm;
 import org.unitime.timetable.model.SponsoringOrganization;
 import org.unitime.timetable.model.dao._RootDAO;
 
+/**
+ * @author Zuzana Mullerova
+ */
 public class SponsoringOrgEditAction extends Action {
 
 	public ActionForward execute(
@@ -31,61 +54,69 @@ public class SponsoringOrgEditAction extends Action {
 		}
 
 		if ("Save".equals(op)) {
-			Transaction tx = null;
-			try {
-				Session hibSession = new _RootDAO().getSession();
-				tx = hibSession.beginTransaction();
-
-				// create sponsoring org
-				SponsoringOrganization spor = new SponsoringOrganization();
-				if (myForm.getOrgName()!= null) spor.setName(myForm.getOrgName());
-				if (myForm.getOrgEmail()!=null) spor.setEmail(myForm.getOrgEmail());
-				hibSession.saveOrUpdate(spor); // save sponsoring org
-
-/*				ChangeLog.addChange(
-	                    hibSession,
-	                    request,
-	                    iEvent,
-	                    ChangeLog.Source.EVENT_EDIT,
-	                    ChangeLog.Operation.UPDATE,
-	                    null,null);
-*/				
-				tx.commit();
-			} catch (Exception e) {
-				if (tx!=null) tx.rollback();
-				throw e;
+			ActionMessages errors = myForm.validate(mapping, request);
+			if (!errors.isEmpty()) saveErrors(request, errors);
+			else {
+				Transaction tx = null;
+				try {
+					Session hibSession = new _RootDAO().getSession();
+					tx = hibSession.beginTransaction();
+	
+					// create sponsoring org
+					SponsoringOrganization spor = new SponsoringOrganization();
+					if (myForm.getOrgName()!= null) spor.setName(myForm.getOrgName());
+					if (myForm.getOrgEmail()!=null) spor.setEmail(myForm.getOrgEmail());
+					hibSession.saveOrUpdate(spor); // save sponsoring org
+	
+	/*				ChangeLog.addChange(
+		                    hibSession,
+		                    request,
+		                    iEvent,
+		                    ChangeLog.Source.EVENT_EDIT,
+		                    ChangeLog.Operation.UPDATE,
+		                    null,null);
+	*/				
+					tx.commit();
+				} catch (Exception e) {
+					if (tx!=null) tx.rollback();
+					throw e;
+				}
+				return mapping.findForward("list");
 			}
-			return mapping.findForward("list");
-		}
+			}
 		
 		if ("Update".equals(op)) {
-			Transaction tx = null;
-			try {
-				Session hibSession = new _RootDAO().getSession();
-				tx = hibSession.beginTransaction();
-
-				// create sponsoring org
-				SponsoringOrganization spor = myForm.getOrg();
-				spor.setName(myForm.getOrgName()==null?"":myForm.getOrgName());
-				spor.setEmail(myForm.getOrgEmail()==null?"":myForm.getOrgEmail());
-				hibSession.saveOrUpdate(spor); // save sponsoring org
-
-/*				ChangeLog.addChange(
-	                    hibSession,
-	                    request,
-	                    iEvent,
-	                    ChangeLog.Source.EVENT_EDIT,
-	                    ChangeLog.Operation.UPDATE,
-	                    null,null);
-*/				
-				tx.commit();
-			} catch (Exception e) {
-				if (tx!=null) tx.rollback();
-				e.printStackTrace();
-				throw e;
+			ActionMessages errors = myForm.validate(mapping, request);
+			if (!errors.isEmpty()) saveErrors(request, errors);
+			else {
+				Transaction tx = null;
+				try {
+					Session hibSession = new _RootDAO().getSession();
+					tx = hibSession.beginTransaction();
+	
+					// create sponsoring org
+					SponsoringOrganization spor = myForm.getOrg();
+					spor.setName(myForm.getOrgName()==null?"":myForm.getOrgName());
+					spor.setEmail(myForm.getOrgEmail()==null?"":myForm.getOrgEmail());
+					hibSession.saveOrUpdate(spor); // save sponsoring org
+	
+	/*				ChangeLog.addChange(
+		                    hibSession,
+		                    request,
+		                    iEvent,
+		                    ChangeLog.Source.EVENT_EDIT,
+		                    ChangeLog.Operation.UPDATE,
+		                    null,null);
+	*/				
+					tx.commit();
+				} catch (Exception e) {
+					if (tx!=null) tx.rollback();
+					e.printStackTrace();
+					throw e;
+				}
+				
+				return mapping.findForward("list");
 			}
-			
-			return mapping.findForward("list");
 		}
 
 		if ("Delete".equals(op)) {
@@ -104,6 +135,7 @@ public class SponsoringOrgEditAction extends Action {
             }			
 		}
 		
-		return mapping.findForward("show");
+		if ("add".equals(myForm.getScreen())) return mapping.findForward("add");
+		else return mapping.findForward("show");
 	}
 }
