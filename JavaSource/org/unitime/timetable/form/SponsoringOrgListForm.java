fd029c30e0de1b07550682263fb6a6cf9166b39e
zmullero <mullerova@unitime.org>
2008-07-16 01:16:41 +0000

    -- SponsoringOrgEdit (or Add) - check if there already is an organization with the same name (if there is one, write an error message) -- Added copyright information at the beginning of java files

--- JavaSource/org/unitime/timetable/form/SponsoringOrgListForm.java
+++ JavaSource/org/unitime/timetable/form/SponsoringOrgListForm.java
@@ -1,3 +1,22 @@
+/*
+ * UniTime 3.1 (University Timetabling Application)
+ * Copyright (C) 2008, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 package org.unitime.timetable.form;
 
 import java.util.Iterator;
@@ -10,6 +29,9 @@ import org.apache.struts.action.ActionMapping;
 import org.unitime.commons.web.WebTable;
 import org.unitime.timetable.model.SponsoringOrganization;
 
+/**
+ * @author Zuzana Mullerova
+ */
 public class SponsoringOrgListForm extends ActionForm {
 
 	private String iOp;
