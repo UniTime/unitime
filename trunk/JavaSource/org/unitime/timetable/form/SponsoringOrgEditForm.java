77da339294f85abc224e9b9df662d651824da750
zmullero <zmullero@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2008-07-16 01:16:41 +0000

    -- SponsoringOrgEdit (or Add) - check if there already is an organization with the same name (if there is one, write an error message) -- Added copyright information at the beginning of java files

--- trunk/JavaSource/org/unitime/timetable/form/SponsoringOrgEditForm.java
+++ trunk/JavaSource/org/unitime/timetable/form/SponsoringOrgEditForm.java
@@ -1,4 +1,25 @@
+/*
+ * UniTime 3.1 (University Timetabling Application)
+ * Copyright (C) 2008, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 package org.unitime.timetable.form;
+
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +30,9 @@ import org.apache.struts.action.ActionMessage;
 import org.unitime.timetable.model.SponsoringOrganization;
 import org.unitime.timetable.model.dao.SponsoringOrganizationDAO;
 
+/**
+ * @author Zuzana Mullerova
+ */
 public class SponsoringOrgEditForm extends ActionForm {
 
 	private String iScreen = "edit";
@@ -21,11 +45,26 @@ public class SponsoringOrgEditForm extends ActionForm {
 	public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
 		
 		ActionErrors errors = new ActionErrors();
-
+		
 		if (iOrgName==null || iOrgName.length()==0) {
 			errors.add("orgName", new ActionMessage("errors.generic", "Please enter the name of the organization."));
+		} else {
+			for (Iterator i=SponsoringOrganization.findAll().iterator(); i.hasNext();) {
+				SponsoringOrganization so2 = (SponsoringOrganization) i.next();
+				if (iOrgName.compareToIgnoreCase(so2.getName())==0) {
+					if (iId != null) {
+						if (iId.compareTo(so2.getUniqueId())!=0) {
+							errors.add("orgNameExists", new ActionMessage("errors.generic", "Another organization with this name already exists."));
+							break;
+						}
+					} else {
+						errors.add("orgNameExists", new ActionMessage("errors.generic", "Another organization with this name already exists."));
+						break;
+					}
+				}
+			}
 		}
-
+			
 		return errors;
 	}
 	
