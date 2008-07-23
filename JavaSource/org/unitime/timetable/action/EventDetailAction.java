58c045661bd2d4813f1d2c2fffada5a9dd9639ab
zmullero <mullerova@unitime.org>
2008-07-23 18:40:40 +0000

    Approval is a part of Event Detail (Reject still needs implementing, so does color-coding for events/meetings that have not been approved yet)

--- JavaSource/org/unitime/timetable/action/EventDetailAction.java
+++ JavaSource/org/unitime/timetable/action/EventDetailAction.java
@@ -20,8 +20,10 @@
 package org.unitime.timetable.action;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
@@ -42,6 +44,7 @@ import org.unitime.timetable.model.ClassEvent;
 import org.unitime.timetable.model.Class_;
 import org.unitime.timetable.model.CourseEvent;
 import org.unitime.timetable.model.CourseOffering;
+import org.unitime.timetable.model.Department;
 import org.unitime.timetable.model.Event;
 import org.unitime.timetable.model.EventContact;
 import org.unitime.timetable.model.EventNote;
@@ -49,12 +52,15 @@ import org.unitime.timetable.model.ExamEvent;
 import org.unitime.timetable.model.ExamOwner;
 import org.unitime.timetable.model.InstrOfferingConfig;
 import org.unitime.timetable.model.InstructionalOffering;
+import org.unitime.timetable.model.Location;
 import org.unitime.timetable.model.Meeting;
 import org.unitime.timetable.model.RelatedCourseInfo;
 import org.unitime.timetable.model.Roles;
 import org.unitime.timetable.model.StandardEventNote;
+import org.unitime.timetable.model.TimetableManager;
 import org.unitime.timetable.model.dao.CourseEventDAO;
 import org.unitime.timetable.model.dao.EventDAO;
+import org.unitime.timetable.model.dao.MeetingDAO;
 import org.unitime.timetable.util.Constants;
 import org.unitime.timetable.webutil.BackTracker;
 import org.unitime.timetable.webutil.Navigation;
@@ -85,6 +91,12 @@ public class EventDetailAction extends Action {
 		User user = Web.getUser(webSession);
 		Event event = EventDAO.getInstance().get(Long.valueOf(myForm.getId()));
 		
+		Set<Department> userDepartments = null;
+		if (user!=null && Roles.EVENT_MGR_ROLE.equals(user.getRole())) {
+			TimetableManager mgr = TimetableManager.getManager(user);
+			if (mgr!=null) userDepartments = mgr.getDepartments();
+		}
+		
 		if (!Web.isLoggedIn(webSession)) {
 			throw new Exception("Access Denied.");
 		}			
@@ -113,6 +125,27 @@ public class EventDetailAction extends Action {
 			if(iOp.equals("Add Meetings")) {
 				response.sendRedirect(response.encodeURL("eventAdd.do?op=view&id="+myForm.getId()));
 				return null;
+			}
+			
+			if(iOp.equals("Approve") && myForm.getSelectedMeetings()!=null && myForm.getSelectedMeetings().length>0) {
+				System.out.println("Meetings selected for approval:");
+				Long[] selectedMeetings = myForm.getSelectedMeetings();
+                org.hibernate.Session hibSession = new EventDAO().getSession();
+                Transaction tx = null;
+                try {
+                    tx = hibSession.beginTransaction();
+					for (int i=0; i<selectedMeetings.length; i++) {
+						Meeting approvedMeeting = MeetingDAO.getInstance().get(selectedMeetings[i]);
+						System.out.println(selectedMeetings[i]+": "+approvedMeeting);
+						approvedMeeting.setApprovedDate(new Date());
+						hibSession.saveOrUpdate(approvedMeeting);
+					}
+                    tx.commit();
+                } catch (Exception e) {
+                    if (tx!=null) tx.rollback();
+                    throw e;
+                }		                
+
 			}
 			
 			if(iOp.equals("Delete")) {
@@ -176,13 +209,10 @@ public class EventDetailAction extends Action {
 					myForm.setSponsoringOrgName(event.getSponsoringOrganization()==null?"":event.getSponsoringOrganization().getName());
 				for (Iterator i = event.getNotes().iterator(); i.hasNext();) {
 					EventNote en = (EventNote) i.next();
+					StandardEventNote sen = en.getStandardNote();
+					if (sen!=null) {myForm.addNote(sen.getNote());}
 					if (en.getTextNote()!= null) {myForm.addNote(en.getTextNote());}
 				}
-				for (Iterator i = event.getNotes().iterator(); i.hasNext();) {
-					EventNote en2 = (EventNote) i.next();
-					StandardEventNote sen = en2.getStandardNote();
-					if (sen!=null) {myForm.addNote(sen.getNote());}
-				}			
 				if (event.getMainContact()!=null)
 				    myForm.setMainContact(event.getMainContact());
 				for (Iterator i = event.getAdditionalContacts().iterator(); i.hasNext();) {
@@ -208,16 +238,30 @@ public class EventDetailAction extends Action {
 					(meeting.getStopOffset()==null?0:meeting.getStopOffset());
 					int endHour = end/60;
 					int endMin = end%60;
-					String location = (meeting.getLocation()==null?"":meeting.getLocation().getLabel());
-					String locationCapacity = meeting.getLocation().getCapacity().toString();
+					Location location = meeting.getLocation();
+					String locationLabel = (location==null?"":location.getLabel());
+					String locationCapacity = location.getCapacity().toString();
 					String approvedDate = (meeting.getApprovedDate()==null?"":iDateFormat2.format(meeting.getApprovedDate()));
+					boolean canEdit = false;
+					boolean canDelete = false;
+					if (meeting.getStartTime().after(new Date())) {
+						if (user.isAdmin()) {
+							canEdit = true;
+						} else if (user.getId().equals(event.getMainContact().getExternalUniqueId())) {
+							canEdit = true;
+							canDelete = true;
+							myForm.setCanDelete(true);
+						} else if (userDepartments!=null && location!=null && location.getControllingDepartment()!=null)
+							canEdit = userDepartments.contains(location.getControllingDepartment());
+						myForm.setCanEdit(canEdit);
+					}
 					myForm.addMeeting(meeting.getUniqueId(),
 							iDateFormat.format(meeting.getMeetingDate()),
 							(startHour>12?startHour-12:startHour)+":"+(startMin<10?"0":"")+startMin+(startHour>=12?"p":"a"),
 							(endHour>12?endHour-12:endHour)+":"+(endMin<10?"0":"")+endMin+(endHour>=12?"p":"a"), 
-							location, locationCapacity, approvedDate);
+							locationLabel, locationCapacity, approvedDate, canEdit, canDelete);
 				}
-				myForm.setCanEdit(user.isAdmin()||user.hasRole(Roles.EVENT_MGR_ROLE)||user.getId().equals(event.getMainContact().getExternalUniqueId()));
+//				myForm.setCanEdit(user.isAdmin()||user.hasRole(Roles.EVENT_MGR_ROLE)||user.getId().equals(event.getMainContact().getExternalUniqueId()));
 				if (event instanceof ClassEvent || event instanceof ExamEvent) {
 					myForm.setCanEdit(false);
 				}
