73075d6f2f902d29cf62df6fe38904fc1cf3c01e
Tomáš Müller <muller@unitime.org>
2008-08-29 09:05:01 +0000

    Event Add Meetings 	- fixed bug on Event Add Info page (while adding meeting to an existing event): related courses/classes were not displayed for modified course event

--- JavaSource/org/unitime/timetable/action/EventRoomAvailabilityAction.java
+++ JavaSource/org/unitime/timetable/action/EventRoomAvailabilityAction.java
@@ -20,6 +20,9 @@
 
 package org.unitime.timetable.action;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -32,7 +35,16 @@ import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 import org.unitime.commons.User;
 import org.unitime.commons.web.Web;
+import org.unitime.commons.web.WebTable;
 import org.unitime.timetable.form.EventRoomAvailabilityForm;
+import org.unitime.timetable.model.Class_;
+import org.unitime.timetable.model.CourseEvent;
+import org.unitime.timetable.model.CourseOffering;
+import org.unitime.timetable.model.ExamOwner;
+import org.unitime.timetable.model.InstrOfferingConfig;
+import org.unitime.timetable.model.InstructionalOffering;
+import org.unitime.timetable.model.RelatedCourseInfo;
+import org.unitime.timetable.model.dao.CourseEventDAO;
 
 /**
  * @author Zuzana Mullerova
@@ -94,8 +106,60 @@ public class EventRoomAvailabilityAction extends Action {
                     saveErrors(request, errors);
                     return  mapping.findForward("show");
                 }
-				if (myForm.getIsAddMeetings()) return mapping.findForward("eventUpdateMeetings");
-				else return mapping.findForward("eventAddInfo");
+				if (myForm.getIsAddMeetings()) {
+				    if ("Course Event".equals(myForm.getEventType())) {
+			            CourseEvent courseEvent = new CourseEventDAO().get((myForm.getEventId()));;
+			            if (!courseEvent.getRelatedCourses().isEmpty()) {
+			                WebTable table = new WebTable(5, null, new String[] {"Object", "Type", "Title","Limit","Assignment"}, new String[] {"left", "left", "left","right","left"}, new boolean[] {true, true, true, true,true});
+			                for (Iterator i=new TreeSet(courseEvent.getRelatedCourses()).iterator();i.hasNext();) {
+			                    RelatedCourseInfo rci = (RelatedCourseInfo)i.next();
+			                    String onclick = null, name = null, type = null, title = null, assignment = null;
+			                    String students = String.valueOf(rci.countStudents());
+			                    switch (rci.getOwnerType()) {
+			                        case ExamOwner.sOwnerTypeClass :
+			                            Class_ clazz = (Class_)rci.getOwnerObject();
+			                            if (user.getRole()!=null && clazz.isViewableBy(user))
+			                                onclick = "onClick=\"document.location='classDetail.do?cid="+clazz.getUniqueId()+"';\"";
+			                            name = rci.getLabel();//clazz.getClassLabel();
+			                            type = "Class";
+			                            title = clazz.getSchedulePrintNote();
+			                            if (title==null || title.length()==0) title=clazz.getSchedulingSubpart().getControllingCourseOffering().getTitle();
+			                            if (clazz.getCommittedAssignment()!=null)
+			                                assignment = clazz.getCommittedAssignment().getPlacement().getLongName();
+			                            break;
+			                        case ExamOwner.sOwnerTypeConfig :
+			                            InstrOfferingConfig config = (InstrOfferingConfig)rci.getOwnerObject();
+			                            if (user.getRole()!=null && config.isViewableBy(user))
+			                                onclick = "onClick=\"document.location='instructionalOfferingDetail.do?io="+config.getInstructionalOffering().getUniqueId()+"';\"";;
+			                            name = rci.getLabel();//config.getCourseName()+" ["+config.getName()+"]";
+			                            type = "Configuration";
+			                            title = config.getControllingCourseOffering().getTitle();
+			                            break;
+			                        case ExamOwner.sOwnerTypeOffering :
+			                            InstructionalOffering offering = (InstructionalOffering)rci.getOwnerObject();
+			                            if (user.getRole()!=null && offering.isViewableBy(user))
+			                                onclick = "onClick=\"document.location='instructionalOfferingDetail.do?io="+offering.getUniqueId()+"';\"";;
+			                            name = rci.getLabel();//offering.getCourseName();
+			                            type = "Offering";
+			                            title = offering.getControllingCourseOffering().getTitle();
+			                            break;
+			                        case ExamOwner.sOwnerTypeCourse :
+			                            CourseOffering course = (CourseOffering)rci.getOwnerObject();
+			                            if (user.getRole()!=null && course.isViewableBy(user))
+			                                onclick = "onClick=\"document.location='instructionalOfferingDetail.do?io="+course.getInstructionalOffering().getUniqueId()+"';\"";;
+			                            name = rci.getLabel();//course.getCourseName();
+			                            type = "Course";
+			                            title = course.getTitle();
+			                            break;
+			                                
+			                    }
+			                    table.addLine(onclick, new String[] { name, type, title, students, assignment}, null);
+			                }
+			                request.setAttribute("EventDetail.table",table.printTable());
+			            }
+			        }
+				    return mapping.findForward("eventUpdateMeetings");
+				} else return mapping.findForward("eventAddInfo");
 			}
 			
 			if("Change".equals(iOp)) {
