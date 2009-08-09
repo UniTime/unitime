c38774588183a3288a0af29abc8cb0cbd73e72e7
tomas13 <tomas13@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2009-08-09 10:42:41 +0000

    Added missing semicolons.

--- trunk/Documentation/Database/Oracle/Changes/46 Committed student enrls load speedup.sql
+++ trunk/Documentation/Database/Oracle/Changes/46 Committed student enrls load speedup.sql
@@ -30,12 +30,12 @@ create index idx_student_enrl_assignment on student_enrl(solution_id,class_id);
 insert into solver_parameter_def (select solver_parameter_def_seq.nextval as uniqueid,
 	'General.IgnoreCommittedStudentConflicts' as name, 'false' as default_vale, 'Do not load committed student conflicts' as description,
 	'boolean' as type, 13 as ord, 1 as visible,
-	uniqueid as solver_param_group_id from solver_parameter_group where name='General')
+	uniqueid as solver_param_group_id from solver_parameter_group where name='General');
 
 insert into solver_parameter_def (select solver_parameter_def_seq.nextval as uniqueid,
 	'General.WeightStudents' as name, 'true' as default_vale, 'Weight last-like students' as description,
 	'boolean' as type, 14 as ord, 0 as visible,
-	uniqueid as solver_param_group_id from solver_parameter_group where name='General')
+	uniqueid as solver_param_group_id from solver_parameter_group where name='General');
 
 /*
  * Update database version
