2481e8f658a58a0eec4ed337992065a4e9d030b7
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2009-10-31 10:18:10 +0000

    removed unused imports

--- JavaSource/org/unitime/timetable/model/Class_.java
+++ JavaSource/org/unitime/timetable/model/Class_.java
@@ -19,17 +19,14 @@
 */
 package org.unitime.timetable.model;
 
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
