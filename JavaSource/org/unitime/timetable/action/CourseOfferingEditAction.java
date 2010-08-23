56ed7ddf3c3b8c5ef1ad761da3d876ea2d1e9f5a
says0528 <says@unitime.org>
2010-08-23 15:21:10 +0000

    If the course number is changed for a course offering that does not have a permanent id, then give the course offering a permanent id and add the permanent id to any last like demand data associated with the course offering.

--- JavaSource/org/unitime/timetable/action/CourseOfferingEditAction.java
+++ JavaSource/org/unitime/timetable/action/CourseOfferingEditAction.java
@@ -34,6 +34,7 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessages;
 import org.apache.struts.util.MessageResources;
 import org.hibernate.Transaction;
+import org.hibernate.impl.SessionImpl;
 import org.unitime.commons.Debug;
 import org.unitime.commons.User;
 import org.unitime.commons.web.Web;
@@ -47,12 +48,14 @@ import org.unitime.timetable.model.CourseOffering;
 import org.unitime.timetable.model.CourseOfferingReservation;
 import org.unitime.timetable.model.FixedCreditUnitConfig;
 import org.unitime.timetable.model.InstructionalOffering;
+import org.unitime.timetable.model.LastLikeCourseDemand;
 import org.unitime.timetable.model.OfferingConsentType;
 import org.unitime.timetable.model.VariableFixedCreditUnitConfig;
 import org.unitime.timetable.model.VariableRangeCreditUnitConfig;
 import org.unitime.timetable.model.dao.CourseOfferingDAO;
 import org.unitime.timetable.model.dao.OfferingConsentTypeDAO;
 import org.unitime.timetable.util.Constants;
+import org.unitime.timetable.util.InstrOfferingPermIdGenerator;
 import org.unitime.timetable.util.LookupTables;
 
 
@@ -207,6 +210,18 @@ public class CourseOfferingEditAction extends Action {
 
 	        CourseOffering co = cdao.get(crsId);
 
+	        if (co.getCourseNbr() != null && !co.getCourseNbr().equals(crsNbr) && co.getPermId() == null){
+	        	LastLikeCourseDemand llcd = null;
+	        	for(Iterator it = co.getCourseOfferingDemands().iterator(); it.hasNext();){
+	        		llcd = (LastLikeCourseDemand)it.next();
+	        		co.setPermId(InstrOfferingPermIdGenerator.getGenerator().generate((SessionImpl)new CourseOfferingDAO().getSession(), co).toString());
+	        		if (llcd.getCoursePermId() == null){
+		        		llcd = (LastLikeCourseDemand) it.next();
+		        		llcd.setCoursePermId(co.getPermId());
+		        		hibSession.update(llcd);
+	        		}
+	        	}
+	        }
 	        co.setCourseNbr(crsNbr);
 	        co.setTitle(title);
 	        co.setScheduleBookNote(note);
