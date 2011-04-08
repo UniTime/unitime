2381de04aa1febe134aece487a189ba832608b73
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2011-04-08 21:58:45 +0000

    Online Student Sectioning  - update expected / held space counters when student enrollment is changed
    Online Sectioning: Data Load
     - when config has a limit of 9999 or more, assume it is unlimited
     - when course demands are not used, WaitList objects may be used to record unassigned course requests
     
    Merged from dev_sectioning branch, revisions 2214, 2222.

--- trunk/JavaSource/org/unitime/timetable/onlinesectioning/updates/EnrollStudent.java
+++ trunk/JavaSource/org/unitime/timetable/onlinesectioning/updates/EnrollStudent.java
@@ -18,6 +18,8 @@
 */
 package org.unitime.timetable.onlinesectioning.updates;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -26,7 +28,10 @@ import java.util.Map;
 import java.util.Set;
 
 import net.sf.cpsolver.studentsct.model.Course;
+import net.sf.cpsolver.studentsct.model.CourseRequest;
+import net.sf.cpsolver.studentsct.model.Enrollment;
 import net.sf.cpsolver.studentsct.model.Request;
+import net.sf.cpsolver.studentsct.model.Section;
 
 import org.unitime.timetable.gwt.shared.ClassAssignmentInterface;
 import org.unitime.timetable.gwt.shared.CourseRequestInterface;
@@ -34,6 +39,7 @@ import org.unitime.timetable.gwt.shared.SectioningException;
 import org.unitime.timetable.gwt.shared.SectioningExceptionType;
 import org.unitime.timetable.model.ClassWaitList;
 import org.unitime.timetable.model.Class_;
+import org.unitime.timetable.model.SectioningInfo;
 import org.unitime.timetable.model.Student;
 import org.unitime.timetable.model.StudentClassEnrollment;
 import org.unitime.timetable.model.dao.Class_DAO;
@@ -48,6 +54,7 @@ import org.unitime.timetable.onlinesectioning.solver.CheckAssignmentAction;
  * @author Tomas Muller
  */
 public class EnrollStudent implements OnlineSectioningAction<ClassAssignmentInterface> {
+	private static DecimalFormat sDF = new DecimalFormat("+0.000;-0.000");
 	private Long iStudentId;
 	private CourseRequestInterface iRequest;
 	private List<ClassAssignmentInterface.ClassAssignment> iAssignment;
@@ -77,8 +84,26 @@ public class EnrollStudent implements OnlineSectioningAction<ClassAssignmentInte
 				if (course != null) offeringIds.add(course.getOffering().getId());
 			}
 		
-		Set<Long> reloadOfferingIds = new HashSet<Long>();
-		Set<Long> updateEnrollmentCountOfferingIds = new HashSet<Long>();
+		OnlineSectioningServer.Callback<Boolean> enrollmentsUpdated = new OnlineSectioningServer.Callback<Boolean>() {
+			@Override
+			public void onFailure(Throwable exception) {
+				helper.error("Update enrollment counts failed: " + exception.getMessage(), exception);
+			}
+			@Override
+			public void onSuccess(Boolean result) {
+			}
+		};
+
+		OnlineSectioningServer.Callback<Boolean> offeringChecked = new OnlineSectioningServer.Callback<Boolean>() {
+			@Override
+			public void onFailure(Throwable exception) {
+				helper.error("Offering check failed: " + exception.getMessage(), exception);
+			}
+			@Override
+			public void onSuccess(Boolean result) {
+			}
+		};
+		
 		Lock lock = server.lockStudent(getStudentId(), offeringIds, true);
 		try {
 			helper.beginTransaction();
@@ -87,12 +112,6 @@ public class EnrollStudent implements OnlineSectioningAction<ClassAssignmentInte
 				
 				Student student = StudentDAO.getInstance().get(getStudentId(), helper.getHibSession());
 				if (student == null) throw new SectioningException(SectioningExceptionType.BAD_STUDENT_ID);
-
-				net.sf.cpsolver.studentsct.model.Student st = server.getStudent(getStudentId());
-				if (st != null)
-					for (Request r: st.getRequests())
-						if (r.getAssignment() != null && r.getAssignment().isCourseRequest())
-							reloadOfferingIds.add(r.getAssignment().getCourse().getOffering().getId());
 
 				Hashtable<Long, Class_> classes = new Hashtable<Long, Class_>();
 				for (ClassAssignmentInterface.ClassAssignment ca: getAssignment()) {
@@ -154,13 +173,38 @@ public class EnrollStudent implements OnlineSectioningAction<ClassAssignmentInte
 					throw new SectioningException(SectioningExceptionType.UNKNOWN, e);
 				}
 				
-				if (newStudent != null)
-					for (Request r: newStudent.getRequests())
-						if (r.getAssignment() != null && r.getAssignment().isCourseRequest())
-							if (!reloadOfferingIds.contains(r.getAssignment().getCourse().getOffering().getId()))
-								updateEnrollmentCountOfferingIds.add(r.getAssignment().getCourse().getOffering().getId());
+				if (oldStudent != null) {
+					for (Request oldRequest: oldStudent.getRequests()) {
+						Enrollment oldEnrollment = oldRequest.getInitialAssignment();
+						if (oldEnrollment == null || !oldEnrollment.isCourseRequest()) continue; // free time or not assigned
+						Enrollment newEnrollment = null;
+						if (newStudent != null)
+							for (Request newRequest: newStudent.getRequests())
+								if (newRequest.getAssignment() != null && newRequest.getAssignment().isCourseRequest() && newRequest.getAssignment().getOffering().getId() == oldEnrollment.getOffering().getId()) {
+									newEnrollment = newRequest.getAssignment(); break;
+								}
+						if (newEnrollment != null && newEnrollment.getSections().equals(oldEnrollment.getSections())) continue; // same assignment
+						
+						server.execute(new CheckOfferingAction(oldEnrollment.getOffering().getId()), offeringChecked);
+						updateSpace(helper, newEnrollment, oldEnrollment);
+					}
+				}
 				
-				server.notifyStudentChanged(getStudentId(), oldStudent.getRequests(), newStudent.getRequests());
+				if (newStudent != null) {
+					requests: for (Request newRequest: newStudent.getRequests()) {
+						Enrollment newEnrollment = newRequest.getAssignment();
+						if (newEnrollment == null || !newEnrollment.isCourseRequest()) continue; // free time or not assigned
+						if (oldStudent != null)
+							for (Request oldRequest: oldStudent.getRequests())
+								if (oldRequest.getInitialAssignment() != null && oldRequest.getInitialAssignment().isCourseRequest() && oldRequest.getInitialAssignment().getOffering().getId() == newEnrollment.getOffering().getId())
+									continue requests;
+						updateSpace(helper, newEnrollment, null);
+						server.execute(new UpdateEnrollmentCountsAction(newEnrollment.getOffering().getId()), enrollmentsUpdated);
+					}
+					
+				}
+
+				server.notifyStudentChanged(getStudentId(), (oldStudent == null ? null : oldStudent.getRequests()), (newStudent == null ? null : newStudent.getRequests()));
 				
 				helper.commitTransaction();
 			} catch (Exception e) {
@@ -173,32 +217,110 @@ public class EnrollStudent implements OnlineSectioningAction<ClassAssignmentInte
 			lock.release();
 		}
 		
-		if (!updateEnrollmentCountOfferingIds.isEmpty())
-			server.execute(new UpdateEnrollmentCountsAction(updateEnrollmentCountOfferingIds), new OnlineSectioningServer.Callback<Boolean>() {
-				@Override
-				public void onFailure(Throwable exception) {
-					helper.error("Update enrollment counts failed: " + exception.getMessage(), exception);
-				}
-				@Override
-				public void onSuccess(Boolean result) {
-					helper.info("All enrollment counts are updated.");
-				}
-			});
-		
-		if (!reloadOfferingIds.isEmpty())
-			server.execute(new CheckOfferingAction(reloadOfferingIds), new OnlineSectioningServer.Callback<Boolean>() {
-				@Override
-				public void onFailure(Throwable exception) {
-					helper.error("Offering check failed: " + exception.getMessage(), exception);
-				}
-				@Override
-				public void onSuccess(Boolean result) {
-					helper.info("All related offerings were checked.");
-				}
-			});
-		
 		return server.getAssignment(getStudentId());
 	}
+	
+    public static void updateSpace(OnlineSectioningHelper helper, Enrollment newEnrollment, Enrollment oldEnrollment) {
+    	Map<Long, Section> sections = new Hashtable<Long, Section>();
+    	if (oldEnrollment != null) {
+            for (Section section : oldEnrollment.getSections()) {
+                section.setSpaceHeld(section.getSpaceHeld() + 1.0);
+                sections.put(section.getId(), section);
+            }
+            List<Enrollment> feasibleEnrollments = new ArrayList<Enrollment>();
+            for (Enrollment enrl : oldEnrollment.getRequest().values()) {
+            	if (!enrl.getCourse().equals(oldEnrollment.getCourse())) continue;
+                boolean overlaps = false;
+                for (Request otherRequest : oldEnrollment.getRequest().getStudent().getRequests()) {
+                    if (otherRequest.equals(oldEnrollment.getRequest()) || !(otherRequest instanceof CourseRequest))
+                        continue;
+                    Enrollment otherErollment = otherRequest.getInitialAssignment();
+                    if (otherErollment == null)
+                        continue;
+                    if (enrl.isOverlapping(otherErollment)) {
+                        overlaps = true;
+                        break;
+                    }
+                }
+                if (!overlaps)
+                    feasibleEnrollments.add(enrl);
+            }
+            double increment = 1.0 / feasibleEnrollments.size();
+            for (Enrollment feasibleEnrollment : feasibleEnrollments) {
+                for (Section section : feasibleEnrollment.getSections()) {
+                    section.setSpaceExpected(section.getSpaceExpected() + increment);
+                    sections.put(section.getId(), section);
+                }
+            }
+    	}
+    	if (newEnrollment != null) {
+            for (Section section : newEnrollment.getSections()) {
+                section.setSpaceHeld(section.getSpaceHeld() - 1.0);
+                sections.put(section.getId(), section);
+            }
+            List<Enrollment> feasibleEnrollments = new ArrayList<Enrollment>();
+            for (Enrollment enrl : newEnrollment.getRequest().values()) {
+            	if (!enrl.getCourse().equals(newEnrollment.getCourse())) continue;
+                boolean overlaps = false;
+                for (Request otherRequest : newEnrollment.getRequest().getStudent().getRequests()) {
+                    if (otherRequest.equals(newEnrollment.getRequest()) || !(otherRequest instanceof CourseRequest))
+                        continue;
+                    Enrollment otherErollment = otherRequest.getAssignment();
+                    if (otherErollment == null)
+                        continue;
+                    if (enrl.isOverlapping(otherErollment)) {
+                        overlaps = true;
+                        break;
+                    }
+                }
+                if (!overlaps)
+                    feasibleEnrollments.add(enrl);
+            }
+            double decrement = 1.0 / feasibleEnrollments.size();
+            for (Enrollment feasibleEnrollment : feasibleEnrollments) {
+                for (Section section : feasibleEnrollment.getSections()) {
+                    section.setSpaceExpected(section.getSpaceExpected() - decrement);
+                    sections.put(section.getId(), section);
+                }
+            }
+    	}
+    	
+    	for (SectioningInfo info: (List<SectioningInfo>)helper.getHibSession().createQuery(
+    			"select i from SectioningInfo i where i.clazz.schedulingSubpart.instrOfferingConfig.instructionalOffering = :offeringId").
+    			setLong("offeringId", (newEnrollment == null ? oldEnrollment : newEnrollment).getCourse().getOffering().getId()).
+    			setCacheable(true).list()) {
+    		Section section = sections.remove(info.getClazz().getUniqueId());
+    		if (section == null) continue;
+    		if (info.getNbrExpectedStudents() == section.getSpaceExpected() && info.getNbrHoldingStudents() == section.getSpaceHeld()) continue;
+    		helper.debug(info.getClazz().getClassLabel(helper.getHibSession()) + ": expected " + sDF.format(section.getSpaceExpected() - info.getNbrExpectedStudents()) +
+    				", held " + sDF.format(section.getSpaceHeld() - info.getNbrHoldingStudents()));
+    		if (section.getLimit() >= 0 && section.getLimit() >= info.getNbrExpectedStudents() && section.getLimit() < section.getSpaceExpected())
+    			helper.info(info.getClazz().getClassLabel(helper.getHibSession()) + ": become over-expected");
+    		if (section.getLimit() >= 0 && section.getLimit() < info.getNbrExpectedStudents() && section.getLimit() >= section.getSpaceExpected())
+    			helper.info(info.getClazz().getClassLabel(helper.getHibSession()) + ": no longer over-expected");
+    		info.setNbrExpectedStudents(section.getSpaceExpected());
+    		info.setNbrHoldingStudents(section.getSpaceHeld());
+    		helper.getHibSession().saveOrUpdate(info);
+    	}
+    	
+    	if (!sections.isEmpty())
+        	for (Class_ clazz: (List<Class_>)helper.getHibSession().createQuery(
+        			"select c from Class_ c where c.schedulingSubpart.instrOfferingConfig.instructionalOffering = :offeringId").
+        			setLong("offeringId", (newEnrollment == null ? oldEnrollment : newEnrollment).getCourse().getOffering().getId()).
+        			setCacheable(true).list()) {
+        		Section section = sections.remove(clazz.getUniqueId());
+        		if (section == null) continue;
+                SectioningInfo info = new SectioningInfo();
+        		helper.debug(info.getClazz().getClassLabel(helper.getHibSession()) + ": expected " + sDF.format(section.getSpaceExpected()) +
+        				", held " + sDF.format(section.getSpaceHeld()) + " (new)");
+        		if (section.getLimit() >= 0 && section.getLimit() < section.getSpaceExpected())
+        			helper.info(info.getClazz().getClassLabel(helper.getHibSession()) + ": become over-expected");
+                info.setClazz(clazz);
+                info.setNbrExpectedStudents(section.getSpaceExpected());
+                info.setNbrHoldingStudents(section.getSpaceHeld());
+                helper.getHibSession().saveOrUpdate(info);
+        	}
+    }
 	
 	@Override
 	public String name() {
