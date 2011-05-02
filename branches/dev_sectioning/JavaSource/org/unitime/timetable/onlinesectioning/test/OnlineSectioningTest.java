abfc13a941457dd53e016ecba930b26756b62c9f
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2011-05-02 22:17:33 +0000

    Online Student Sectioning: Tests  - added (simple) online sectioning test    - it is just two actions find assignments & enroll

---
+++ branches/dev_sectioning/JavaSource/org/unitime/timetable/onlinesectioning/test/OnlineSectioningTest.java
@@ -0,0 +1,58 @@
+package org.unitime.timetable.onlinesectioning.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.unitime.timetable.gwt.shared.ClassAssignmentInterface;
+import org.unitime.timetable.gwt.shared.CourseRequestInterface;
+import org.unitime.timetable.model.dao._RootDAO;
+import org.unitime.timetable.onlinesectioning.OnlineSectioningServer;
+import org.unitime.timetable.onlinesectioning.OnlineSectioningTestFwk;
+import org.unitime.timetable.onlinesectioning.solver.FindAssignmentAction;
+import org.unitime.timetable.onlinesectioning.updates.EnrollStudent;
+
+public class OnlineSectioningTest extends OnlineSectioningTestFwk {
+
+	public List<Operation> operations() {
+
+		org.hibernate.Session hibSession = new _RootDAO().getSession();
+		
+		List<Operation> operations = new ArrayList<Operation>();
+		
+		for (final Long studentId: (List<Long>)hibSession.createQuery(
+				"select s.uniqueId from Student s where s.session.uniqueId = :sessionId")
+				.setLong("sessionId", getServer().getAcademicSession().getUniqueId()).list()) {
+			
+			CourseRequestInterface request = getServer().getRequest(studentId);
+			if (request == null || request.getCourses().isEmpty()) continue;
+			
+			operations.add(new Operation() {
+				@Override
+				public double execute(OnlineSectioningServer s) {
+					CourseRequestInterface request = s.getRequest(studentId);
+					if (request != null && !request.getCourses().isEmpty()) {
+						FindAssignmentAction action = new FindAssignmentAction(request, new ArrayList<ClassAssignmentInterface.ClassAssignment>()); 
+						List<ClassAssignmentInterface> assignments = s.execute(action);
+						if (assignments != null && !assignments.isEmpty()) {
+							List<ClassAssignmentInterface.ClassAssignment> assignment = new ArrayList<ClassAssignmentInterface.ClassAssignment>();
+							for (ClassAssignmentInterface.CourseAssignment course: assignments.get(0).getCourseAssignments())
+								assignment.addAll(course.getClassAssignments());
+							s.execute(new EnrollStudent(studentId, request, assignment));
+						}
+						return action.value();
+					} else {
+						return 1.0;
+					}
+				}
+			});
+		}
+		
+		hibSession.close();
+
+		return operations;
+	}
+	
+	public static void main(String args[]) {
+		new FindAssignmentsTest().test(-1, 100);
+	}
+}
