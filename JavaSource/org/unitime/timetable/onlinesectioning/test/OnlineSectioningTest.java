38cece1bb7c121192bf14ad354e13f67e48ff583
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2011-05-06 15:51:42 +0000

    Online Student Sectioning: Tests  - added (simple) online sectioning test    - it is just two actions find assignments & enroll    - check for cases when a section / config / course become unavailable (between find assignments and enroll student actions)      - try some other assignment in such a case (retry up to 5 times)
    Merged from dev_sectioning branch, revision 2317, 2319, and 2324.

---
+++ JavaSource/org/unitime/timetable/onlinesectioning/test/OnlineSectioningTest.java
@@ -0,0 +1,71 @@
+package org.unitime.timetable.onlinesectioning.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.unitime.timetable.gwt.shared.ClassAssignmentInterface;
+import org.unitime.timetable.gwt.shared.CourseRequestInterface;
+import org.unitime.timetable.gwt.shared.SectioningException;
+import org.unitime.timetable.gwt.shared.SectioningExceptionType;
+import org.unitime.timetable.model.dao._RootDAO;
+import org.unitime.timetable.onlinesectioning.OnlineSectioningServer;
+import org.unitime.timetable.onlinesectioning.OnlineSectioningTestFwk;
+import org.unitime.timetable.onlinesectioning.solver.FindAssignmentAction;
+import org.unitime.timetable.onlinesectioning.updates.EnrollStudent;
+
+public class OnlineSectioningTest extends OnlineSectioningTestFwk {
+
+	public List<Operation> operations() {
+
+		org.hibernate.Session hibSession = new _RootDAO().getSession();
+		
+		List<Operation> operations = new ArrayList<Operation>();
+		
+		for (final Long studentId: (List<Long>)hibSession.createQuery(
+				"select s.uniqueId from Student s where s.session.uniqueId = :sessionId")
+				.setLong("sessionId", getServer().getAcademicSession().getUniqueId()).list()) {
+			
+			CourseRequestInterface request = getServer().getRequest(studentId);
+			if (request == null || request.getCourses().isEmpty()) continue;
+			
+			operations.add(new Operation() {
+				@Override
+				public double execute(OnlineSectioningServer s) {
+					CourseRequestInterface request = s.getRequest(studentId);
+					if (request != null && !request.getCourses().isEmpty()) {
+						FindAssignmentAction action = null;
+						for (int i = 1; i <= 5; i++) {
+							try {
+								action = new FindAssignmentAction(request, new ArrayList<ClassAssignmentInterface.ClassAssignment>()); 
+								List<ClassAssignmentInterface> assignments = s.execute(action);
+								if (assignments != null && !assignments.isEmpty()) {
+									List<ClassAssignmentInterface.ClassAssignment> assignment = new ArrayList<ClassAssignmentInterface.ClassAssignment>();
+									for (ClassAssignmentInterface.CourseAssignment course: assignments.get(0).getCourseAssignments())
+										assignment.addAll(course.getClassAssignments());
+									s.execute(new EnrollStudent(studentId, request, assignment));
+								}
+								break;
+							} catch (SectioningException e) {
+								if (e.getType() == SectioningExceptionType.ENROLL_NOT_AVAILABLE) {
+									sLog.warn("Enrollment failed: " +e.getMessage() + " become unavailable (" + i + ". attempt)");
+									continue;
+								}
+							}
+						}
+						return action.value();
+					} else {
+						return 1.0;
+					}
+				}
+			});
+		}
+		
+		hibSession.close();
+
+		return operations;
+	}
+	
+	public static void main(String args[]) {
+		new OnlineSectioningTest().test(-1, 100);
+	}
+}
