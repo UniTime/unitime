8dc79d79d99fa01af3fb8988ceade90ac15d9005
Tomáš Müller <muller@unitime.org>
2011-05-10 07:00:17 +0000

    Ajax calls: encode the resultant XML on UTF-8

--- JavaSource/org/unitime/timetable/action/ajax/ITypesAjax.java
+++ JavaSource/org/unitime/timetable/action/ajax/ITypesAjax.java
@@ -42,7 +42,8 @@ public class ITypesAjax extends Action {
 
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         
-        response.addHeader("Content-Type", "text/xml");
+        response.addHeader("Content-Type", "text/xml; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         
         PrintWriter out = response.getWriter();
         
