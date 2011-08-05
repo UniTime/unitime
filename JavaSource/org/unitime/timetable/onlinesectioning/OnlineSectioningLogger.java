d4e5782cfc3ebfc1ae6ccd44088767aa30122915
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2011-08-05 13:19:02 +0000

    Online Student Sectioning logger  - added ability to disable logging (by setting unitime.sectioning.log to false)  - handle exceptions while saving logs, cache academic sessions

--- JavaSource/org/unitime/timetable/onlinesectioning/OnlineSectioningLogger.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/OnlineSectioningLogger.java
@@ -25,13 +25,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.Session;
 import org.unitime.timetable.ApplicationProperties;
+import org.unitime.timetable.model.Session;
 import org.unitime.timetable.model.Student;
 import org.unitime.timetable.model.dao.OnlineSectioningLogDAO;
 import org.unitime.timetable.model.dao.SessionDAO;
@@ -44,6 +45,7 @@ public class OnlineSectioningLogger extends Thread {
 	private static Log sLog = LogFactory.getLog(OnlineSectioningLogger.class);
 	private List<OnlineSectioningLog.Action> iActions = new Vector<OnlineSectioningLog.Action>();
 	private boolean iActive = false;
+	private boolean iEnabled = false;
 	private PrintWriter iOut = null;
 	
 	private static OnlineSectioningLogger sInstance = null;
@@ -70,20 +72,27 @@ public class OnlineSectioningLogger extends Thread {
 	private OnlineSectioningLogger() {
 		super("OnlineSectioningLogger");
 		setDaemon(true);
+		iEnabled = "true".equals(ApplicationProperties.getProperty("unitime.sectioning.log", "true"));
 		try {
 			iOut = new PrintWriter(new FileWriter(new File(ApplicationProperties.getDataFolder(), "sectioning.log"), true));
 		} catch (IOException e) {
 			sLog.warn("Unable to create sectioning log: " + e.getMessage(), e);
 		}
 	}
+	
+	public boolean isEnabled() { return iEnabled; }
+	public void setEnabled(boolean enabled) { iEnabled = enabled; }
+	public boolean isActive() { return iActive; }
 
 	public void record(OnlineSectioningLog.Log log) {
-		if (log == null) return;
+		if (log == null || !isEnabled() || !isActive()) return;
 		for (OnlineSectioningLog.Action action: log.getActionList()) {
 			if (action.hasStartTime() && action.hasStudent() && action.hasOperation() && action.hasSession()) {
 				synchronized (iActions) {
 					iActions.add(action);
-					if (iOut != null) {
+				}
+				if (iOut != null) {
+					synchronized (iOut) {
 						iOut.print(OnlineSectioningLog.Log.newBuilder().addAction(action).build().toString());
 						iOut.flush();
 					}
@@ -93,53 +102,69 @@ public class OnlineSectioningLogger extends Thread {
 	}
 	
 	public void run() {
-		sLog.debug("Online Sectioning Logger is up.");
-		iActive = true;
+		sLog.info("Online Sectioning Logger is up.");
 		try {
-			iOut = new PrintWriter(new FileWriter(new File(ApplicationProperties.getDataFolder(), "sectioning.log"), true));
-		} catch (IOException e) {
-			sLog.warn("Unable to create sectioning log: " + e.getMessage(), e);
-		}
-		while (true) {
+			iActive = true;
 			try {
-				sleep(60000);
-			} catch (InterruptedException e) {
+				iOut = new PrintWriter(new FileWriter(new File(ApplicationProperties.getDataFolder(), "sectioning.log"), true));
+			} catch (IOException e) {
+				sLog.warn("Unable to create sectioning log: " + e.getMessage(), e);
 			}
-			List<OnlineSectioningLog.Action> actionsToSave = null;
-			synchronized (iActions) {
-				if (!iActions.isEmpty()) {
-					actionsToSave = new ArrayList<OnlineSectioningLog.Action>(iActions);
-					iActions.clear();
-				}
-			}
-			if (actionsToSave != null) {
-				sLog.debug("Persisting " + actionsToSave.size() + " actions...");
-				Session hibSession = OnlineSectioningLogDAO.getInstance().createNewSession();
+			while (true) {
 				try {
-					for (OnlineSectioningLog.Action q: actionsToSave) {
-						org.unitime.timetable.model.OnlineSectioningLog log = new org.unitime.timetable.model.OnlineSectioningLog();
-						log.setAction(q.toByteArray());
-						log.setOperation(q.getOperation());
-						String studentExternalId = (q.getStudent().hasExternalId() ? q.getStudent().getExternalId() : null);
-						if (studentExternalId == null || studentExternalId.isEmpty()) {
-							Student student = StudentDAO.getInstance().get(q.getStudent().getUniqueId(), hibSession);
-							if (student == null) continue;
-							studentExternalId = student.getExternalUniqueId();
-						}
-						log.setStudent(studentExternalId);
-						log.setTimeStamp(new Date(q.getStartTime()));
-						log.setSession(SessionDAO.getInstance().get(q.getSession().getUniqueId()));
-						hibSession.save(log);
-					}
-					hibSession.flush();
-				} finally {
-					hibSession.close();
+					sleep(60000);
+				} catch (InterruptedException e) {
 				}
+				List<OnlineSectioningLog.Action> actionsToSave = null;
+				synchronized (iActions) {
+					if (!iActions.isEmpty()) {
+						actionsToSave = new ArrayList<OnlineSectioningLog.Action>(iActions);
+						iActions.clear();
+					}
+				}
+				try {
+					if (actionsToSave != null) {
+						sLog.debug("Persisting " + actionsToSave.size() + " actions...");
+						org.hibernate.Session hibSession = OnlineSectioningLogDAO.getInstance().createNewSession();
+						try {
+							Hashtable<Long, Session> sessions = new Hashtable<Long, Session>();
+							for (OnlineSectioningLog.Action q: actionsToSave) {
+								org.unitime.timetable.model.OnlineSectioningLog log = new org.unitime.timetable.model.OnlineSectioningLog();
+								log.setAction(q.toByteArray());
+								log.setOperation(q.getOperation());
+								String studentExternalId = (q.getStudent().hasExternalId() ? q.getStudent().getExternalId() : null);
+								if (studentExternalId == null || studentExternalId.isEmpty()) {
+									Student student = StudentDAO.getInstance().get(q.getStudent().getUniqueId(), hibSession);
+									if (student == null) continue;
+									studentExternalId = student.getExternalUniqueId();
+								}
+								log.setStudent(studentExternalId);
+								log.setTimeStamp(new Date(q.getStartTime()));
+								Long sessionId = q.getSession().getUniqueId();
+								Session session = sessions.get(sessionId);
+								if (session == null) {
+									session = SessionDAO.getInstance().get(sessionId);
+									sessions.put(sessionId, session);
+								}
+								log.setSession(session);
+								hibSession.save(log);
+							}
+							hibSession.flush();
+						} finally {
+							hibSession.close();
+						}
+					}
+				} catch (Throwable t) {
+					sLog.warn("Failed to save " + actionsToSave.size() + " log actions: " + t.getMessage(), t);
+				}
+				if (!iActive) break;
 			}
-			if (!iActive) break;
+		} catch (Throwable t) {
+			sLog.error("Online Sectioning Logger failed: " + t.getMessage(), t);
+		} finally {
+			iActive = false;
+			if (iOut != null) iOut.flush(); iOut.close();
 		}
-		sLog.debug("Online Sectioning Logger is down.");
-		if (iOut != null) iOut.flush(); iOut.close();
-	}
+		sLog.info("Online Sectioning Logger is down.");	}
 
 }
