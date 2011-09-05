54ff51b9191163db1d042dc283e5d91cb93fb9de
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2011-09-05 15:25:34 +0000

    Data Exchange  - added session backup / restore

--- JavaSource/org/unitime/timetable/form/DataImportForm.java
+++ JavaSource/org/unitime/timetable/form/DataImportForm.java
@@ -73,6 +73,7 @@ public class DataImportForm extends ActionForm {
     			"tmtbl.export.exam.type", "midterm"),
     	CURRICULA("curricula", "Curricula"),
     	STUDENT_ENRL("studentEnrollments", "Student class enrollments"),
+    	SESSION("session", "Academic Session"),
     	;
     	
     	private String iType, iLabel;
