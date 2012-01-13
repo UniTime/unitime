ded37774fd0f0078bb80ea15cfd5e0d33247206b
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2012-01-13 11:20:24 +0000

    Use case 3: simple Guice integration with Struts 1  - binding of actions in the model is not needed -- it was just a leftover from some testing

--- branches/test_guice/JavaSource/org/unitime/timetable/guice/modules/StrutsModule.java
+++ branches/test_guice/JavaSource/org/unitime/timetable/guice/modules/StrutsModule.java
@@ -1,6 +1,5 @@
 package org.unitime.timetable.guice.modules;
 
-import org.unitime.timetable.action.ClassEditAction;
 import org.unitime.timetable.guice.struts.GuiceRequestProcessor;
 
 import com.google.inject.AbstractModule;
@@ -10,7 +9,6 @@ public class StrutsModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		requestStaticInjection(GuiceRequestProcessor.class);
-		bind(ClassEditAction.class);
 	}
 
 }
