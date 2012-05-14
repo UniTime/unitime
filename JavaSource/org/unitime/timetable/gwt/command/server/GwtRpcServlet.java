6cea1ac0ede09cbd08377b7983438aa6b4379412
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2012-05-14 14:55:26 +0000

    GWT RPC command pattern  - added ability to cancel a request    - this is done by splitting the call into two, first (executeAsync) is only returing an execution id, the next (waitForResults) is waiting for the results    - while the waitForResults call is waiting, cancelExecution call can be used to cancel the execution with the given id

--- JavaSource/org/unitime/timetable/gwt/command/server/GwtRpcServlet.java
+++ JavaSource/org/unitime/timetable/gwt/command/server/GwtRpcServlet.java
@@ -20,6 +20,8 @@
 package org.unitime.timetable.gwt.command.server;
 
 import java.util.Date;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 
@@ -28,8 +30,8 @@ import net.sf.cpsolver.ifs.util.JProf;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.unitime.commons.User;
-import org.unitime.commons.web.Web;
 import org.unitime.timetable.filter.QueryLogFilter;
+import org.unitime.timetable.gwt.command.client.GwtRpcCancelledException;
 import org.unitime.timetable.gwt.command.client.GwtRpcImplementedBy;
 import org.unitime.timetable.gwt.command.client.GwtRpcRequest;
 import org.unitime.timetable.gwt.command.client.GwtRpcException;
@@ -44,6 +46,8 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 	private static final long serialVersionUID = 1L;
 	private static Log sLog = LogFactory.getLog(GwtRpcServlet.class);
 	private QueryLogFilter.Saver iSaver = null;
+	private static IdGenerator sIdGenerator = new IdGenerator();
+	private static Map<Long, Execution> sExecutions = new Hashtable<Long, Execution>();
 	
 	@Override
 	public void init() throws ServletException {
@@ -61,6 +65,8 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 	public <T extends GwtRpcResponse> T execute(GwtRpcRequest<T> request) throws GwtRpcException {
 		// start time
 		long t0 = JProf.currentTimeMillis();
+		// create helper
+		GwtRpcHelper helper = new GwtRpcHelper(getThreadLocalRequest().getSession());
 		try {
 			// retrieve implementation from given request
 			GwtRpcImplementedBy annotation = request.getClass().getAnnotation(GwtRpcImplementedBy.class);
@@ -71,16 +77,16 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 			GwtRpcImplementation<GwtRpcRequest<T>, T> implementation = (GwtRpcImplementation<GwtRpcRequest<T>, T>)Class.forName(annotation.value()).newInstance();
 			
 			// execute request
-			T response = implementation.execute(request, new GwtRpcHelper(getThreadLocalRequest().getSession()));
+			T response = implementation.execute(request, helper);
 			
 			// log request
-			log(request, response, null, JProf.currentTimeMillis() - t0);
+			log(request, response, null, JProf.currentTimeMillis() - t0, helper);
 			
 			// return response
 			return response;
 		} catch (Throwable t) {
 			// log exception
-			log(request, null, t, JProf.currentTimeMillis() - t0);
+			log(request, null, t, JProf.currentTimeMillis() - t0, helper);
 			
 			// re-throw exception as GwtRpcException or IsSerializable runtime exception
 			if (t instanceof GwtRpcException) {
@@ -96,7 +102,7 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 		}
 	}
 	
-	private <T extends GwtRpcResponse> void log(GwtRpcRequest<T> request, T response, Throwable exception, long time) {
+	private <T extends GwtRpcResponse> void log(GwtRpcRequest<T> request, T response, Throwable exception, long time, GwtRpcHelper helper) {
 		try {
 			if (iSaver == null) return;
 			QueryLog q = new QueryLog();
@@ -107,8 +113,8 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 			q.setTimeStamp(new Date());
 			q.setTimeSpent(time);
 			try {
-				q.setSessionId(getThreadLocalRequest().getSession().getId());
-				User user = Web.getUser(getThreadLocalRequest().getSession()); 
+				q.setSessionId(helper.getHttpSessionId());
+				User user = helper.getUser(); 
 				if (user != null) q.setUid(user.getId());
 			} catch (IllegalStateException e) {}
 			q.setQuery(request.toString());
@@ -132,5 +138,154 @@ public class GwtRpcServlet extends RemoteServiceServlet implements GwtRpcService
 			sLog.warn("Failed to log a request: " + t.getMessage(), t);
 		}
 	}
+
+	@Override
+	public <T extends GwtRpcResponse> Long executeAsync(GwtRpcRequest<T> request) throws GwtRpcException {
+		try {
+			Execution<GwtRpcRequest<T>, T> execution = new Execution<GwtRpcRequest<T>, T>(request);
+			synchronized (sExecutions) {
+				sExecutions.put(execution.getExecutionId(), execution);
+			}
+			execution.start();
+			return execution.getExecutionId();
+		} catch (Exception e) {
+			sLog.warn("Execute async failed: " + e.getMessage());
+			throw new GwtRpcException(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public <T extends GwtRpcResponse> T waitForResults(Long executionId) throws GwtRpcException {
+		try {
+			Execution<GwtRpcRequest<T>, T> execution = null;
+			synchronized (sExecutions) {
+				execution = sExecutions.get(executionId);
+			}
+			if (execution == null) throw new GwtRpcException("No execution with given id found.");
+			try {
+				execution.waitToFinish();
+			} catch (InterruptedException e) {
+			}
+			synchronized (sExecutions) {
+				sExecutions.remove(executionId);
+			}
+			if (execution.getException() != null) throw execution.getException();
+			return execution.getResponse();
+		} catch (GwtRpcCancelledException e) {
+			throw e;
+		} catch (GwtRpcException e) {
+			throw e;
+		} catch (Exception e) {
+			sLog.warn("Wait for results failed: " + e.getMessage());
+			throw new GwtRpcException(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public Boolean cancelExecution(Long executionId) throws GwtRpcException {
+		try {
+			Execution execution = null;
+			synchronized (sExecutions) {
+				execution = sExecutions.get(executionId);
+			}
+			if (execution == null) return false;
+			execution.cancelExecution();
+			return true;
+		} catch (Exception e) {
+			sLog.warn("Cancel execution failed: " + e.getMessage());
+			throw new GwtRpcException(e.getMessage(), e);
+		}
+	}
+		
+	private static class IdGenerator {
+		long iNextId = 0;
+		synchronized Long generatedId() {
+			return iNextId++;
+		}
+	}
+	
+	private class Execution<R extends GwtRpcRequest<T>, T extends GwtRpcResponse> extends Thread {
+		R iRequest;
+		T iResponse = null;
+		GwtRpcHelper iHelper = null;
+		GwtRpcException iException = null;
+		Thread iWaitingThread = null;
+		long iExecutionId;
+		boolean iRunning = false;
+		
+		Execution(R request) {
+			setName("RPC:" + request);
+			setDaemon(true);
+			iRequest = request;
+			iExecutionId = sIdGenerator.generatedId();
+			iHelper = new GwtRpcHelper(getThreadLocalRequest().getSession());
+		}
+
+		@Override
+		public void run() {
+			iRunning = true;
+			// start time
+			long t0 = JProf.currentTimeMillis();
+			try {
+				// retrieve implementation from given request
+				GwtRpcImplementedBy annotation = iRequest.getClass().getAnnotation(GwtRpcImplementedBy.class);
+				if (annotation == null || annotation.value() == null || annotation.value().isEmpty()) {
+					throw new GwtRpcException("Request " + iRequest.getClass().getName().substring(iRequest.getClass().getName().lastIndexOf('.') + 1) +
+							" does not have the GwtRpcImplementedBy annotation.");
+				}
+				GwtRpcImplementation<R, T> implementation = (GwtRpcImplementation<R, T>)Class.forName(annotation.value()).newInstance();
+				
+				// execute request
+				iResponse = implementation.execute(iRequest, iHelper);
+				
+				// log request
+				log(iRequest, iResponse, null, JProf.currentTimeMillis() - t0, iHelper);
+			} catch (Throwable t) {
+				// log exception
+				log(iRequest, null, t, JProf.currentTimeMillis() - t0, iHelper);
+				
+				// re-throw exception as GwtRpcException or IsSerializable runtime exception
+				if (t instanceof GwtRpcException) {
+					sLog.info("Seen server exception: " + t.getMessage());
+					iException = (GwtRpcException)t;
+				} else  if (t instanceof IsSerializable) {
+					sLog.warn("Seen server exception: " + t.getMessage());
+					iException = new GwtRpcException(t.getMessage(), t);
+				} else {
+					sLog.error("Seen exception: " + t.getMessage(), t);
+					iException = new GwtRpcException(t.getMessage());
+				}
+			}
+			synchronized (this) {
+				iWaitingThread = null;
+				iRunning = false;
+				iHelper = null;
+			}
+		}
+		
+		void waitToFinish() throws InterruptedException {
+			synchronized (this) {
+				if (iRunning) {
+					iWaitingThread = Thread.currentThread();
+					join();
+				}
+			}
+		}
+		
+		void cancelExecution() {
+			synchronized (this) {
+				iException = new GwtRpcCancelledException("Operation cancelled by the user.");
+				if (iWaitingThread != null) iWaitingThread.interrupt();
+			}
+			interrupt();
+		}
+		
+		T getResponse() { return iResponse; }
+		
+		GwtRpcException getException() { return iException; }
+		
+		Long getExecutionId() { return iExecutionId; }
+	}
+
 
 }
