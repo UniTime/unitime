faaa274ea8a453846e991f091da87da45167f4d3
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2012-06-15 20:13:51 +0000

    Instructional Offering Detail  - print a warning next to the offering limit when there is a configuration with a limit higher than the total limit set on its classes    - Configuration X has a higher limit than there is space available in its classes

--- JavaSource/org/unitime/localization/messages/CourseMessages.java
+++ JavaSource/org/unitime/localization/messages/CourseMessages.java
@@ -1700,6 +1700,12 @@ public interface CourseMessages extends Messages {
 	@DefaultMessage("Spaces reserved for course offerings ({0}) should total to at least the offering limit.")
 	String errorReservedSpacesForOfferingsTotal(String coursesTotal);
 	
+	@DefaultMessage("Configuration {0} has a higher limit than there is space available in its classes.")
+	String errorConfigWithTooHighLimit(String configuration);
+	
+	@DefaultMessage("Configurations {0} have a higher limit than there is space available in its classes.")
+	String errorConfigsWithTooHighLimit(String configurations);
+
 	@DefaultMessage("Department is required.")
 	String errorRequiredDepartment();
 	
