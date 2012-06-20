e7e08e1bf23054e406405646b26d783970dfc41f
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2012-06-20 22:58:22 +0000

    Instructional Offering Detail   - Curricula, Reservations: do not load the curriculum / reservations data with the page when the section is closed     - the data are loaded when the section is opened for the first time instead     - this is the same behavior as with the Enrollments section  - Curricula, Reservations, Enrollments: when the table data failed to load, switch the section opened toggle to false   - this means that it will not try to load the data again on the next page, unless the section is opened again
    Merged from maint_UniTime33 branch, revision 3070 and revision 3071.

--- JavaSource/org/unitime/timetable/gwt/client/reservations/ReservationTable.java
+++ JavaSource/org/unitime/timetable/gwt/client/reservations/ReservationTable.java
@@ -96,7 +96,9 @@ public class ReservationTable extends Composite {
 			@Override
 			public void onValueChange(ValueChangeEvent<Boolean> event) {
 				ReservationCookie.getInstance().setReservationCoursesDetails(event.getValue());
-				if (iReservations.getRowCount() > 2) {
+				if (iReservations.getRowCount() == 0)
+					refresh();
+				else if (iReservations.getRowCount() > 2) {
 					for (int row = 1; row < iReservations.getRowCount() - 1; row++) {
 						iReservations.getRowFormatter().setVisible(row, event.getValue());
 					}
@@ -148,6 +150,8 @@ public class ReservationTable extends Composite {
 				@Override
 				public void onFailure(Throwable caught) {
 					iHeader.setErrorMessage("Failed to load reservations (" + caught.getMessage() + ").");
+					iHeader.setCollapsible(null);
+					ReservationCookie.getInstance().setReservationCoursesDetails(false);
 				}
 				@Override
 				public void onSuccess(List<ReservationInterface> result) {
@@ -627,7 +631,13 @@ public class ReservationTable extends Composite {
 	public void insert(final RootPanel panel) {
 		initCallbacks();
 		iOfferingId = Long.valueOf(panel.getElement().getInnerText());
-		refresh();
+		if (ReservationCookie.getInstance().getReservationCoursesDetails()) {
+			refresh();
+		} else {
+			clear(false);
+			iHeader.clearMessage();
+			iHeader.setCollapsible(false);
+		}
 		panel.getElement().setInnerText(null);
 		panel.add(this);
 		panel.setVisible(true);
