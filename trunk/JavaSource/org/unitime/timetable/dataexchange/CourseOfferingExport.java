17e5c0ea003a0de70655b2d3f5c1092b3df3eb0b
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-02-04 09:15:31 +0000

    Course Offering / Course Timetable Export  - when application property tmtbl.export.defaultDatePattern is set to true, export dates for the default date pattern as well

--- trunk/JavaSource/org/unitime/timetable/dataexchange/CourseOfferingExport.java
+++ trunk/JavaSource/org/unitime/timetable/dataexchange/CourseOfferingExport.java
@@ -337,7 +337,7 @@ public class CourseOfferingExport extends BaseExport {
             timeElement.addAttribute("startTime", startSlot2startTime(time.getStartSlot()));
             timeElement.addAttribute("endTime", timeLocation2endTime(time));
             DatePattern dp = assignment.getDatePattern();
-            if (dp != null && !dp.isDefault())
+            if (dp != null && (!dp.isDefault() || "true".equals(ApplicationProperties.getProperty("tmtbl.export.defaultDatePattern", "false"))))
             	timeElement.addAttribute("datePattern", dp.getName());
         }
     }
@@ -365,7 +365,7 @@ public class CourseOfferingExport extends BaseExport {
     }
     
     protected void exportDatePattern(Element classElement, DatePattern dp, Session session) {
-        if (dp!=null && !dp.equals(session.getDefaultDatePattern())) {
+        if (dp!=null && (!dp.equals(session.getDefaultDatePattern()) || "true".equals(ApplicationProperties.getProperty("tmtbl.export.defaultDatePattern", "false")))) {
             Calendar startDate = Calendar.getInstance(Locale.US);
             startDate.setTime(dp.getStartDate());
             Calendar endDate = Calendar.getInstance(Locale.US);
