a76db50263e893bab2fcc048fd88e869c8a2f90e
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-02-22 12:04:35 +0000

    Student Scheduling Assistant: Alternatives  - show the little x on the top right corner of the dialog (to close the dialog)  - escape (or a mouse click outside of the dialog) works as well (as before)

--- trunk/JavaSource/org/unitime/timetable/gwt/client/sectioning/SuggestionsBox.java
+++ trunk/JavaSource/org/unitime/timetable/gwt/client/sectioning/SuggestionsBox.java
@@ -24,6 +24,7 @@ import java.util.Collection;
 
 import org.unitime.timetable.gwt.client.widgets.HorizontalPanelWithHint;
 import org.unitime.timetable.gwt.client.widgets.LoadingWidget;
+import org.unitime.timetable.gwt.client.widgets.UniTimeDialogBox;
 import org.unitime.timetable.gwt.client.widgets.WebTable;
 import org.unitime.timetable.gwt.client.widgets.WebTable.RowClickEvent;
 import org.unitime.timetable.gwt.resources.StudentSectioningConstants;
@@ -54,7 +55,6 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
@@ -69,7 +69,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * @author Tomas Muller
  */
-public class SuggestionsBox extends DialogBox {
+public class SuggestionsBox extends UniTimeDialogBox {
 	public static final StudentSectioningResources RESOURCES =  GWT.create(StudentSectioningResources.class);
 	public static final StudentSectioningMessages MESSAGES = GWT.create(StudentSectioningMessages.class);
 	public static final StudentSectioningConstants CONSTANTS = GWT.create(StudentSectioningConstants.class);
@@ -101,7 +101,7 @@ public class SuggestionsBox extends DialogBox {
 	private Timer iHideHint;
 	
 	public SuggestionsBox(TimeGrid.ColorProvider color, boolean online) {
-		super();
+		super(true, false);
 		
 		iOnline = online;
 		
