06f3ace97b7d6f636d69d57d14baabb3c42efe1c
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-03-11 22:14:17 +0000

    Student Scheduling Assistant  - first serious attempt to make the page accessible (and screen reader friendly)  - using WAI-ARIA standard, see http://www.w3.org/TR/wai-aria and https://developers.google.com/web-toolkit/doc/latest/DevGuideA11y  - more testing is needed (only tested using ChromeVox and Apple VoiceOver)

---
+++ trunk/JavaSource/org/unitime/timetable/gwt/client/aria/AriaHiddenLabel.java
@@ -0,0 +1,67 @@
+/*
+ * UniTime 3.4 (University Timetabling Application)
+ * Copyright (C) 2013, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+package org.unitime.timetable.gwt.client.aria;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HasHTML;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+
+public class AriaHiddenLabel extends Widget implements HasHTML, HasText {
+
+	public AriaHiddenLabel(String text, boolean asHtml) {
+		setElement(DOM.createSpan());
+		setStyleName("unitime-AriaHiddenLabel");
+		if (text != null) {
+			if (asHtml)
+				setHTML(text);
+			else
+				setText(text);
+		}
+	}
+	
+	public AriaHiddenLabel(String text) {
+		this(text, false);
+	}
+	
+	public AriaHiddenLabel() {
+		this(null, false);
+	}
+	
+	@Override
+	public String getText() {
+		return getElement().getInnerText();
+	}
+
+	@Override
+	public void setText(String text) {
+		getElement().setInnerText(text);
+	}
+
+	@Override
+	public String getHTML() {
+		return getElement().getInnerHTML();
+	}
+
+	@Override
+	public void setHTML(String html) {
+		getElement().setInnerHTML(html);
+	}
+}
