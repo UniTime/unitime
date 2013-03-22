0c1c90a38035a1246a91fec17cb4485838e26ff7
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-03-22 18:47:07 +0000

    Online Student Scheduling: Class Assignment Changed  - removed the outer read-lock as it can cause a deadlock with some other operation    - no action should request a student/offering lock (MultiLock) while already in a lock

--- trunk/JavaSource/org/unitime/timetable/onlinesectioning/updates/ClassAssignmentChanged.java
+++ trunk/JavaSource/org/unitime/timetable/onlinesectioning/updates/ClassAssignmentChanged.java
@@ -80,89 +80,84 @@ public class ClassAssignmentChanged implements OnlineSectioningAction<Boolean> {
 	@Override
 	public Boolean execute(OnlineSectioningServer server, OnlineSectioningHelper helper) {
 		helper.info(getClassIds().size() + " class assignments changed.");
-		Lock readLock = server.readLock();
+		helper.beginTransaction();
 		try {
-			helper.beginTransaction();
-			try {
-				OnlineSectioningLog.Enrollment.Builder previous = OnlineSectioningLog.Enrollment.newBuilder()
-					.setType(OnlineSectioningLog.Enrollment.EnrollmentType.PREVIOUS);
-				OnlineSectioningLog.Enrollment.Builder stored = OnlineSectioningLog.Enrollment.newBuilder()
-					.setType(OnlineSectioningLog.Enrollment.EnrollmentType.STORED);
-				for (Long classId: getClassIds()) {
-					Class_ clazz = Class_DAO.getInstance().get(classId, helper.getHibSession());
-					if (clazz == null) {
-						helper.warn("Class " + classId + " wos deleted -- unsupported operation (use reload offering instead).");
+			OnlineSectioningLog.Enrollment.Builder previous = OnlineSectioningLog.Enrollment.newBuilder()
+				.setType(OnlineSectioningLog.Enrollment.EnrollmentType.PREVIOUS);
+			OnlineSectioningLog.Enrollment.Builder stored = OnlineSectioningLog.Enrollment.newBuilder()
+				.setType(OnlineSectioningLog.Enrollment.EnrollmentType.STORED);
+			for (Long classId: getClassIds()) {
+				Class_ clazz = Class_DAO.getInstance().get(classId, helper.getHibSession());
+				if (clazz == null) {
+					helper.warn("Class " + classId + " wos deleted -- unsupported operation (use reload offering instead).");
+					continue;
+				}
+				Lock lock = server.lockClass(classId,
+						(List<Long>)helper.getHibSession().createQuery(
+								"select e.student.uniqueId from StudentClassEnrollment e where "+
+				                "e.clazz.uniqueId = :classId").setLong("classId", classId).list());
+				try {
+					Section section = server.getSection(clazz.getUniqueId());
+					if (section == null) {
+						helper.warn("Class " + clazz.getClassLabel() + " was added -- unsupported operation (use reload offering instead).");
 						continue;
 					}
-					Lock lock = server.lockClass(classId,
-							(List<Long>)helper.getHibSession().createQuery(
-									"select e.student.uniqueId from StudentClassEnrollment e where "+
-					                "e.clazz.uniqueId = :classId").setLong("classId", classId).list());
-					try {
-						Section section = server.getSection(clazz.getUniqueId());
-						if (section == null) {
-							helper.warn("Class " + clazz.getClassLabel() + " was added -- unsupported operation (use reload offering instead).");
-							continue;
-						}
-						previous.addSection(OnlineSectioningHelper.toProto(section));
-						helper.info("Reloading " + clazz.getClassLabel());
-		                org.unitime.timetable.model.Assignment a = clazz.getCommittedAssignment();
-		                Placement p = (a == null ? null : a.getPlacement());
-		                if (p != null && p.getTimeLocation() != null) {
-		                	p.getTimeLocation().setDatePattern(
-		                			p.getTimeLocation().getDatePatternId(),
-		                			ReloadAllData.datePatternName(p.getTimeLocation(), server.getAcademicSession()),
-		                			p.getTimeLocation().getWeekCode());
-		                }
-		                section.setPlacement(p);
-						helper.info("  -- placement: " + p);
+					previous.addSection(OnlineSectioningHelper.toProto(section));
+					helper.info("Reloading " + clazz.getClassLabel());
+	                org.unitime.timetable.model.Assignment a = clazz.getCommittedAssignment();
+	                Placement p = (a == null ? null : a.getPlacement());
+	                if (p != null && p.getTimeLocation() != null) {
+	                	p.getTimeLocation().setDatePattern(
+	                			p.getTimeLocation().getDatePatternId(),
+	                			ReloadAllData.datePatternName(p.getTimeLocation(), server.getAcademicSession()),
+	                			p.getTimeLocation().getWeekCode());
+	                }
+	                section.setPlacement(p);
+					helper.info("  -- placement: " + p);
 
-		                int minLimit = clazz.getExpectedCapacity();
-		            	int maxLimit = clazz.getMaxExpectedCapacity();
-		            	int limit = maxLimit;
-		            	if (minLimit < maxLimit && p != null) {
-		            		int roomLimit = (int) Math.floor(p.getRoomSize() / (clazz.getRoomRatio() == null ? 1.0f : clazz.getRoomRatio()));
-		            		// int roomLimit = Math.round((clazz.getRoomRatio() == null ? 1.0f : clazz.getRoomRatio()) * p.getRoomSize());
-		            		limit = Math.min(Math.max(minLimit, roomLimit), maxLimit);
-		            	}
-		                if (clazz.getSchedulingSubpart().getInstrOfferingConfig().isUnlimitedEnrollment() || limit >= 9999) limit = -1;
-		                section.setLimit(limit);
-						helper.info("  -- limit: " + limit);
+	                int minLimit = clazz.getExpectedCapacity();
+	            	int maxLimit = clazz.getMaxExpectedCapacity();
+	            	int limit = maxLimit;
+	            	if (minLimit < maxLimit && p != null) {
+	            		int roomLimit = (int) Math.floor(p.getRoomSize() / (clazz.getRoomRatio() == null ? 1.0f : clazz.getRoomRatio()));
+	            		// int roomLimit = Math.round((clazz.getRoomRatio() == null ? 1.0f : clazz.getRoomRatio()) * p.getRoomSize());
+	            		limit = Math.min(Math.max(minLimit, roomLimit), maxLimit);
+	            	}
+	                if (clazz.getSchedulingSubpart().getInstrOfferingConfig().isUnlimitedEnrollment() || limit >= 9999) limit = -1;
+	                section.setLimit(limit);
+					helper.info("  -- limit: " + limit);
 
-		                String instructorIds = "";
-		                String instructorNames = "";
-		                for (Iterator<ClassInstructor> k = clazz.getClassInstructors().iterator(); k.hasNext(); ) {
-		                	ClassInstructor ci = k.next();
-		                	if (!ci.isLead()) continue;
-		                	if (!instructorIds.isEmpty()) {
-		                		instructorIds += ":"; instructorNames += ":";
-		                	}
-		                	instructorIds += ci.getInstructor().getUniqueId().toString();
-		                	instructorNames += ci.getInstructor().getName(DepartmentalInstructor.sNameFormatShort) + "|"  + (ci.getInstructor().getEmail() == null ? "" : ci.getInstructor().getEmail());
-		                }
-		                section.getChoice().setInstructor(instructorIds, instructorNames);
-						helper.info("  -- instructor: " + instructorNames);
+	                String instructorIds = "";
+	                String instructorNames = "";
+	                for (Iterator<ClassInstructor> k = clazz.getClassInstructors().iterator(); k.hasNext(); ) {
+	                	ClassInstructor ci = k.next();
+	                	if (!ci.isLead()) continue;
+	                	if (!instructorIds.isEmpty()) {
+	                		instructorIds += ":"; instructorNames += ":";
+	                	}
+	                	instructorIds += ci.getInstructor().getUniqueId().toString();
+	                	instructorNames += ci.getInstructor().getName(DepartmentalInstructor.sNameFormatShort) + "|"  + (ci.getInstructor().getEmail() == null ? "" : ci.getInstructor().getEmail());
+	                }
+	                section.getChoice().setInstructor(instructorIds, instructorNames);
+					helper.info("  -- instructor: " + instructorNames);
 
-		                section.setName(clazz.getExternalUniqueId() == null ? clazz.getClassSuffix() == null ? clazz.getSectionNumberString(helper.getHibSession()) : clazz.getClassSuffix() : clazz.getExternalUniqueId());
-						stored.addSection(OnlineSectioningHelper.toProto(section));
-					} finally {
-						lock.release();
-					}
+	                section.setName(clazz.getExternalUniqueId() == null ? clazz.getClassSuffix() == null ? clazz.getSectionNumberString(helper.getHibSession()) : clazz.getClassSuffix() : clazz.getExternalUniqueId());
+					stored.addSection(OnlineSectioningHelper.toProto(section));
+				} finally {
+					lock.release();
 				}
-				
-				helper.getAction().addEnrollment(previous);
-				helper.getAction().addEnrollment(stored);
-				
-				helper.commitTransaction();
-				return true;
-			} catch (Exception e) {
-				helper.rollbackTransaction();
-				if (e instanceof SectioningException)
-					throw (SectioningException)e;
-				throw new SectioningException(MSG.exceptionUnknown(e.getMessage()), e);
 			}
-		} finally {
-			readLock.release();
+			
+			helper.getAction().addEnrollment(previous);
+			helper.getAction().addEnrollment(stored);
+			
+			helper.commitTransaction();
+			return true;
+		} catch (Exception e) {
+			helper.rollbackTransaction();
+			if (e instanceof SectioningException)
+				throw (SectioningException)e;
+			throw new SectioningException(MSG.exceptionUnknown(e.getMessage()), e);
 		}		
 	}
 
