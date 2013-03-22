5f53b6d2c22f0516ccc6cb013fd75dc3037c4fa1
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-03-22 18:44:39 +0000

    Online Student Scheduling: Reload Test  - reload test extended to contain the following actions as well    - find courses    - class assignment changed    - find assignment

--- trunk/JavaSource/org/unitime/timetable/onlinesectioning/test/ReloadTest.java
+++ trunk/JavaSource/org/unitime/timetable/onlinesectioning/test/ReloadTest.java
@@ -22,9 +22,15 @@ package org.unitime.timetable.onlinesectioning.test;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.unitime.timetable.gwt.shared.ClassAssignmentInterface;
+import org.unitime.timetable.gwt.shared.CourseRequestInterface;
 import org.unitime.timetable.model.dao._RootDAO;
+import org.unitime.timetable.onlinesectioning.CourseInfo;
 import org.unitime.timetable.onlinesectioning.OnlineSectioningServer;
 import org.unitime.timetable.onlinesectioning.OnlineSectioningTestFwk;
+import org.unitime.timetable.onlinesectioning.basic.GetRequest;
+import org.unitime.timetable.onlinesectioning.solver.FindAssignmentAction;
+import org.unitime.timetable.onlinesectioning.updates.ClassAssignmentChanged;
 import org.unitime.timetable.onlinesectioning.updates.ReloadStudent;
 
 public class ReloadTest extends OnlineSectioningTestFwk {
@@ -58,7 +64,7 @@ public class ReloadTest extends OnlineSectioningTestFwk {
 					sLog.info("Locking " + offeringId + " ...");
 					s.lockOffering(offeringId);
 					sLog.info("  -- " + offeringId + " locked");
-					long delay = Math.round(Math.random() * 300000);
+					long delay = Math.round(Math.random() * 5000);
 					sLog.info("Sleeping for " + delay + " ms  ...");
 					try {
 						Thread.sleep(delay);
@@ -70,14 +76,69 @@ public class ReloadTest extends OnlineSectioningTestFwk {
 				}
 			});
 		}
+
+		final OnlineSectioningServer.CourseInfoMatcher matcher = new OnlineSectioningServer.CourseInfoMatcher() {
+			@Override
+			public boolean match(CourseInfo course) {
+				return true;
+			}
+		};
+		for (final String name: (List<String>)hibSession.createQuery(
+				"select co.subjectAreaAbbv || ' ' || co.courseNbr from CourseOffering co where co.instructionalOffering.session.uniqueId = :sessionId and co.instructionalOffering.notOffered = false")
+				.setLong("sessionId", getServer().getAcademicSession().getUniqueId()).list()) {
+			loadRequests.add(new Operation() {
+				@Override
+				public double execute(OnlineSectioningServer s) {
+					for (int i = 1; i < name.length(); i++) {
+						sLog.info("Looking for " + name.substring(0, i) + " ...");
+						s.findCourses(name.substring(0, i), 20, matcher);
+					}
+					return 1.0;
+				}
+			});
+		}
+
+		for (final Long classId: (List<Long>)hibSession.createQuery(
+				"select c.uniqueId from Class_ c where c.schedulingSubpart.instrOfferingConfig.instructionalOffering.session.uniqueId = :sessionId")
+				.setLong("sessionId", getServer().getAcademicSession().getUniqueId()).list()) {
+			loadRequests.add(new Operation() {
+				@Override
+				public double execute(OnlineSectioningServer s) {
+					sLog.info("Assignment changed for " + classId + " ...");
+					s.execute(new ClassAssignmentChanged(classId), user());
+					return 1.0;
+				}
+			});
+		}
 		
+		for (final Long studentId: (List<Long>)hibSession.createQuery("select s.uniqueId from Student s where s.session.uniqueId = :sessionId")
+				.setLong("sessionId", getServer().getAcademicSession().getUniqueId()).list()) {
+			
+			CourseRequestInterface request = getServer().execute(new GetRequest(studentId), user());
+			if (request == null || request.getCourses().isEmpty()) continue;
+			
+			loadRequests.add(new Operation() {
+				@Override
+				public double execute(OnlineSectioningServer s) {
+					CourseRequestInterface request = s.execute(new GetRequest(studentId), user());
+					if (request != null && !request.getCourses().isEmpty()) {
+						sLog.info("Find assignments for " + studentId + " ...");
+						FindAssignmentAction action = new FindAssignmentAction(request, new ArrayList<ClassAssignmentInterface.ClassAssignment>()); 
+						s.execute(action, user());
+						return action.value();
+					} else {
+						return 1.0;
+					}
+				}
+			});
+		}
 		hibSession.close();
 		
 		return loadRequests;
 	}
 	
 	public static void main(String args[]) {
-		new ReloadTest().test(-1, 100);
+		new ReloadTest().test(-1, 20);
 	}
 
 }
