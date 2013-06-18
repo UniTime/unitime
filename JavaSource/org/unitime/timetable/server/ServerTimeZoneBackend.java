c95b5d17ac9a8689085fec608b3ea9951e2476ab
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2013-06-18 19:48:16 +0000

    GWT: ServerDateTimeFormat  - added ServerDateTimeFormat class    - it is like the DateTimeFormat but the given date is assumed to be in the server's time zone and it is formatted accordingly    - it also provides methods for conversion dates between server and local time zones    - this class is meant to be used only for dates (java.util.Date) that only store a date with no time (event meeting date, reservation expiration date, etc.)      - with such dates, the problem is that they may get shifted by a day if client's time zone is BEFORE the server's time zone

---
+++ JavaSource/org/unitime/timetable/server/ServerTimeZoneBackend.java
@@ -0,0 +1,41 @@
+/*
+ * UniTime 3.4 (University Timetabling Application)
+ * Copyright (C) 2012, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+package org.unitime.timetable.server;
+
+import java.util.Calendar;
+
+import org.unitime.localization.impl.Localization;
+import org.unitime.timetable.gwt.client.widgets.ServerDateTimeFormat.ServerTimeZoneRequest;
+import org.unitime.timetable.gwt.client.widgets.ServerDateTimeFormat.ServerTimeZoneResponse;
+import org.unitime.timetable.gwt.command.server.GwtRpcImplementation;
+import org.unitime.timetable.gwt.command.server.GwtRpcImplements;
+import org.unitime.timetable.security.SessionContext;
+
+@GwtRpcImplements(ServerTimeZoneRequest.class)
+public class ServerTimeZoneBackend implements GwtRpcImplementation<ServerTimeZoneRequest, ServerTimeZoneResponse> {
+
+	@Override
+	public ServerTimeZoneResponse execute(ServerTimeZoneRequest request, SessionContext context) {
+		Calendar cal = Calendar.getInstance(Localization.getJavaLocale());
+		int offsetInMinutes = -(cal.get(Calendar.ZONE_OFFSET) + cal.get(Calendar.DST_OFFSET)) / (60 * 1000);
+		return new ServerTimeZoneResponse(offsetInMinutes);
+	}
+
+}
