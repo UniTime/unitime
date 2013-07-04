335b1e26071f97530a956585c702dc9aa9eb16a8
Tomáš Müller <muller@unitime.org>
2013-07-04 08:12:25 +0000

    Course Timetabling: all pages where in memory solution is displayed  - generate time placement tool tip only when it is needed (using GWT's asynchronous calls)  - this greatly improves performance of many solver-related pages, especially when there are many classes displayed

---
+++ JavaSource/org/unitime/timetable/server/TimeHintBackend.java
@@ -0,0 +1,69 @@
+/*
+ * UniTime 3.4 (University Timetabling Application)
+ * Copyright (C) 2012, UniTime LLC, and individual contributors
+ * as indicated by the @authors tag.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+package org.unitime.timetable.server;
+
+import java.util.Set;
+
+import net.sf.cpsolver.coursett.model.TimeLocation;
+
+import org.unitime.timetable.gwt.client.TimeHint.TimeHintRequest;
+import org.unitime.timetable.gwt.client.TimeHint.TimeHintResponse;
+import org.unitime.timetable.gwt.command.client.GwtRpcException;
+import org.unitime.timetable.gwt.command.server.GwtRpcImplementation;
+import org.unitime.timetable.gwt.command.server.GwtRpcImplements;
+import org.unitime.timetable.model.Class_;
+import org.unitime.timetable.model.TimePattern;
+import org.unitime.timetable.model.TimePatternDays;
+import org.unitime.timetable.model.TimePatternTime;
+import org.unitime.timetable.model.TimePref;
+import org.unitime.timetable.model.dao.Class_DAO;
+import org.unitime.timetable.security.SessionContext;
+import org.unitime.timetable.webutil.RequiredTimeTable;
+
+@GwtRpcImplements(TimeHintRequest.class)
+public class TimeHintBackend implements GwtRpcImplementation<TimeHintRequest, TimeHintResponse>{
+
+	@Override
+	public TimeHintResponse execute(TimeHintRequest request, SessionContext context) {
+		String[] params = request.getParameter().split(",");
+		Long classId = Long.valueOf(params[0]);
+		int days = Integer.parseInt(params[1]);
+		int slot = Integer.parseInt(params[2]);
+		
+		Class_ clazz = Class_DAO.getInstance().get(classId);
+		for (TimePref p: (Set<TimePref>)clazz.effectivePreferences(TimePref.class)) {
+			if (p.getTimePattern().getType() == TimePattern.sTypeExactTime) continue;
+			boolean match = false;
+			for (TimePatternDays d: p.getTimePattern().getDays()) {
+				if (d.getDayCode() == days) { match = true; break; }
+			}
+			if (!match) continue;
+			match = false;
+			for (TimePatternTime t: p.getTimePattern().getTimes()) {
+				if (t.getStartSlot() == slot) { match = true; break; }
+			}
+			if (!match) continue;
+			RequiredTimeTable m = p.getRequiredTimeTable(new TimeLocation(days, slot, 0, 0, 0.0, null, null, null, 0));
+			return new TimeHintResponse("$wnd." + m.print(false, false).replace(");\n</script>", "").replace("<script language=\"javascript\">\ndocument.write(", "").replace("\n", " "));
+		}
+		throw new GwtRpcException("No matching time preference found.");
+	}
+
+}
