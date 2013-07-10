8ec5d88ce3db3a9136b1bb3c224763e0dcf2812e
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2013-07-10 18:11:45 +0000

    Examination Timetabling: all pages where in memory solution is displayed  - generate period placement tool tip only when it is needed (using GWT's asynchronous calls)  - this improves performance of many solver-related pages, especially when there are many exams displayed

--- JavaSource/org/unitime/timetable/server/TimeHintBackend.java
+++ JavaSource/org/unitime/timetable/server/TimeHintBackend.java
@@ -29,11 +29,16 @@ import org.unitime.timetable.gwt.command.client.GwtRpcException;
 import org.unitime.timetable.gwt.command.server.GwtRpcImplementation;
 import org.unitime.timetable.gwt.command.server.GwtRpcImplements;
 import org.unitime.timetable.model.Class_;
+import org.unitime.timetable.model.Exam;
+import org.unitime.timetable.model.ExamPeriod;
+import org.unitime.timetable.model.PeriodPreferenceModel;
 import org.unitime.timetable.model.TimePattern;
 import org.unitime.timetable.model.TimePatternDays;
 import org.unitime.timetable.model.TimePatternTime;
 import org.unitime.timetable.model.TimePref;
 import org.unitime.timetable.model.dao.Class_DAO;
+import org.unitime.timetable.model.dao.ExamDAO;
+import org.unitime.timetable.model.dao.ExamPeriodDAO;
 import org.unitime.timetable.security.SessionContext;
 import org.unitime.timetable.webutil.RequiredTimeTable;
 
@@ -43,25 +48,36 @@ public class TimeHintBackend implements GwtRpcImplementation<TimeHintRequest, Ti
 	@Override
 	public TimeHintResponse execute(TimeHintRequest request, SessionContext context) {
 		String[] params = request.getParameter().split(",");
-		Long classId = Long.valueOf(params[0]);
-		int days = Integer.parseInt(params[1]);
-		int slot = Integer.parseInt(params[2]);
-		
-		Class_ clazz = Class_DAO.getInstance().get(classId);
-		for (TimePref p: (Set<TimePref>)clazz.effectivePreferences(TimePref.class)) {
-			if (p.getTimePattern().getType() == TimePattern.sTypeExactTime) continue;
-			boolean match = false;
-			for (TimePatternDays d: p.getTimePattern().getDays()) {
-				if (d.getDayCode() == days) { match = true; break; }
+		if (params.length == 2) {
+			Long examId = Long.valueOf(params[0]);
+			Long periodId = Long.valueOf(params[1]);
+			Exam exam = ExamDAO.getInstance().get(examId);
+			ExamPeriod period = ExamPeriodDAO.getInstance().get(periodId);
+        	PeriodPreferenceModel px = new PeriodPreferenceModel(exam.getSession(), period, exam.getExamType().getUniqueId());
+            px.load(exam);
+            RequiredTimeTable m = new RequiredTimeTable(px);
+            return new TimeHintResponse("$wnd." + m.print(false, false).replace(");\n</script>", "").replace("<script language=\"javascript\">\ndocument.write(", "").replace("\n", " "));
+		} else if (params.length == 3) {
+			Long classId = Long.valueOf(params[0]);
+			int days = Integer.parseInt(params[1]);
+			int slot = Integer.parseInt(params[2]);
+			
+			Class_ clazz = Class_DAO.getInstance().get(classId);
+			for (TimePref p: (Set<TimePref>)clazz.effectivePreferences(TimePref.class)) {
+				if (p.getTimePattern().getType() == TimePattern.sTypeExactTime) continue;
+				boolean match = false;
+				for (TimePatternDays d: p.getTimePattern().getDays()) {
+					if (d.getDayCode() == days) { match = true; break; }
+				}
+				if (!match) continue;
+				match = false;
+				for (TimePatternTime t: p.getTimePattern().getTimes()) {
+					if (t.getStartSlot() == slot) { match = true; break; }
+				}
+				if (!match) continue;
+				RequiredTimeTable m = p.getRequiredTimeTable(new TimeLocation(days, slot, 0, 0, 0.0, null, null, null, 0));
+				return new TimeHintResponse("$wnd." + m.print(false, false).replace(");\n</script>", "").replace("<script language=\"javascript\">\ndocument.write(", "").replace("\n", " "));
 			}
-			if (!match) continue;
-			match = false;
-			for (TimePatternTime t: p.getTimePattern().getTimes()) {
-				if (t.getStartSlot() == slot) { match = true; break; }
-			}
-			if (!match) continue;
-			RequiredTimeTable m = p.getRequiredTimeTable(new TimeLocation(days, slot, 0, 0, 0.0, null, null, null, 0));
-			return new TimeHintResponse("$wnd." + m.print(false, false).replace(");\n</script>", "").replace("<script language=\"javascript\">\ndocument.write(", "").replace("\n", " "));
 		}
 		throw new GwtRpcException("No matching time preference found.");
 	}
