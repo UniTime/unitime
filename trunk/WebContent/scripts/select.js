7fb756d2e5025ffe47eed034653a5cccfe3c93c3
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2013-07-30 17:03:51 +0000

    Many Struts/JSP-based Pages  - various drop downs: removed the custom code that was used to navigate to a particular item in the list using the keyboard    - it does not work when the drop-down allows for multiple selection    - it is no longer needed as all modern browsers are doing a good (and also more consistent) job in navigating through a list using keyboard

--- trunk/WebContent/scripts/select.js
+++ trunk/WebContent/scripts/select.js
@@ -28,143 +28,23 @@
 //    onkeydown="return checkKey(event, this);"
 
 
-var matchString = "";
-var wasModified = false;
-
 function selectSearch(event, field) {
-	var code = -1;
-	
-	if (event.keyCode) code = event.keyCode;
-	else if (event.which) code = event.which;
-	
-	//displayStatus();
-
-	// KeyCodes: 9=tab, 13=Enter, 46=Delete
-	if (code == 9 || code == 13 || code == 46) {
-		matchString = "";
-		return true;
-	}
-
-	if(code > 31 && code < 127) {
-		if ( (code<=33) || (code >=38 && code<=127) ) {
-			keyVal = String.fromCharCode(code);
-			matchString = matchString + keyVal;
-			wasModified = true;
-		}
-		else {
-			wasModified = true;
-			return true;
-		}
-	}
-	
-	displayStatus();
-	
-	for (i = 0; i < field.options.length; i++) {
-		selectText = field.options[i].text.toLowerCase();
-		if (selectText.substr(0, matchString.length) == matchString.toLowerCase()) {
-			field.options[i].selected = true;
-			return false;
-		}
-	}
-	
-	return true;
 }
 
 function selectSearchTime(event, field) {
-	var code = -1;
-	
-	if (event.keyCode) code = event.keyCode;
-	else if (event.which) code = event.which;
-	
-	//displayStatus();
-
-	// KeyCodes: 9=tab, 13=Enter, 46=Delete
-	if (code == 9 || code == 13 || code == 46) {
-		matchString = "";
-		return true;
-	}
-
-	if(code > 31 && code < 127) {
-		if ( (code<=33) || (code >=38 && code<=127) ) {
-			keyVal = String.fromCharCode(code);
-			matchString = matchString + keyVal;
-			wasModified = true;
-		}
-		else {
-			wasModified = true;
-			return true;
-		}
-	}
-	
-	displayStatus();
-	
-	var pat1 = new RegExp("\\d?\\d","g");
-	var pat2 = new RegExp("[ap]","g");
-
-	var hour = pat1.exec(matchString);
-	var am = pat2.exec(matchString);
-	var min = pat1.exec(matchString);
-	
-	if (!hour) hour = 12;
-	if (!am) am = (hour<7?'p':'a'); 
-	if (!min) min = 0;
-	if (hour>23) { min = (''+hour).substr(1,1) + min; hour = (''+hour).substr(0,1); }
-	if (hour>12) { hour -= 12; am = 'p'; }
-	
-	var matchTime = hour+':'+((''+min).length<2?'0':'')+min+' '+am+'m';
-	
-	for (i = 0; i < field.options.length; i++) {
-		if (field.options[i].text == matchTime) {
-			field.options[i].selected = true;
-			return false;
-		}
-	}
-	
-	return true;
 }
 
 function checkKey(event, field) {
-
-	var code = -1;
-	
-	if (event.keyCode) code = event.keyCode;
-	else if (event.which) code = event.which;
-	
-	// KeyCodes: 8=BackSpace, 46=Delete, 9==Tab
-	if(code == 8) {
-		if(matchString.length >= 1) {
-			matchString = matchString.substr(0, (matchString.length - 1));
-			wasModified = true;
-		}
-		return selectSearch(event, field);
-	}
-	
-	if (code == 46) {
-		matchString = "";
-		wasModified = true;
-		displayStatus();
-		return false;
-	}
-
-	if (code == 9) {
-		matchString = "";
-		return true;
-	}
 }
 
 function setUp() {
-
-	matchString = "";
-	wasModified = false;
 }
 
 function isModified() {
-
-	return wasModified;
+	return false;
 }
 
 function displayStatus() {
-	window.status = 'Searching for: "' + matchString.toLowerCase() + '" ( use BACKSPACE key to modify, DELETE key to clear the search string)';
 }
 
 function changeWidth(elementId, width){
