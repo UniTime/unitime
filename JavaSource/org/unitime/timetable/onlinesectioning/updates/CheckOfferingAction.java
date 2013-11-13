dc7bfb9efe63e21fb22e066036ec22a45c90c135
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2013-11-13 17:27:58 +0000

    Online Student Scheduling  - check offering: do not re-section alternatives

--- JavaSource/org/unitime/timetable/onlinesectioning/updates/CheckOfferingAction.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/updates/CheckOfferingAction.java
@@ -291,6 +291,7 @@ public class CheckOfferingAction extends WaitlistedOnlineSectioningAction<Boolea
 	
 	public boolean check(OnlineSectioningServer server, XStudent student, XOffering offering, XCourseRequest request) {
 		if (request.getEnrollment() == null) return true;
+		if (!offering.getOfferingId().equals(request.getEnrollment().getOfferingId())) return true;
 		List<XSection> sections = offering.getSections(request.getEnrollment());
 		XConfig config = offering.getConfig(request.getEnrollment().getConfigId());
 		if (config == null || sections.size() != config.getSubparts().size()) {
