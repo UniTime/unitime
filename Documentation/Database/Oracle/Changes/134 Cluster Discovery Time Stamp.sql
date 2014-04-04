1bdfc90c2174dd618c71db17be4b58e77df2c057
Tomáš Müller <muller@unitime.org>
2014-04-04 12:50:57 +0000

    Cluster Discovery improvements  - added a set of initial_hosts (much like TCPPING)    - this should help forming the cluster, especially if there are multiple Tomcat servers  - added time stamp to better recognize active records    - this should help with merging the cluster back together if it gets split

---
+++ Documentation/Database/Oracle/Changes/134 Cluster Discovery Time Stamp.sql
@@ -0,0 +1,29 @@
+/*
+ * UniTime 3.5 (University Timetabling Application)
+ * Copyright (C) 2014, UniTime LLC
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+alter table cluster_discovery add time_stamp date;
+update cluster_discovery set time_stamp = CURRENT_TIMESTAMP;
+
+/*
+ * Update database version
+ */
+
+update application_config set value='134' where name='tmtbl.db.version';
+
+commit;
