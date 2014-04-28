ff226fa0c5df91b7e2ee3c34a4852fc87e911a0f
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2014-04-28 08:12:32 +0000

    include 2014 in the copyright message

--- JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
+++ JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
@@ -29,7 +29,7 @@ public interface GwtConstants extends Constants {
 	@DefaultStringValue("3.5")
 	String version();
 
-	@DefaultStringValue("&copy; 2008 - 2013 UniTime LLC")
+	@DefaultStringValue("&copy; 2008 - 2014 UniTime LLC")
 	String copyright();
 	
 	@DefaultBooleanValue(true)
