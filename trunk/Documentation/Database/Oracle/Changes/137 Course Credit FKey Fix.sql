7c56dda3da95301df820f9f7c0c32c7813328d0c
muller@unitime.org <muller@unitime.org@cc804148-bb53-fe5c-6b9d-7ba62d325a45>
2014-06-13 11:39:40 +0000

    Course Credit  - added the missing "on delete cascade" on the foreign key  - this fixes the issue with deleting the course while having a course credit

---
+++ trunk/Documentation/Database/Oracle/Changes/137 Course Credit FKey Fix.sql
@@ -0,0 +1,31 @@
+/*
+ * UniTime 3.5 (University Timetabling Application)
+ * Copyright (C) 2014, UniTime LLC
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+alter table course_credit_unit_config drop constraint fk_crs_crdt_unit_cfg_crs_own;
+
+alter table course_credit_unit_config add constraint fk_crs_crdt_unit_cfg_crs_own foreign key (course_id) references course_offering(uniqueid) on delete cascade;
+
+
+/*
+ * Update database version
+ */
+
+update application_config set value='137' where name='tmtbl.db.version';
+
+commit;
