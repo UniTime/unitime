f2ea0e0cc3f8ab3e5aa64066c08e380cc3fc19ab
Tomáš Müller <muller@unitime.org>
2014-09-16 16:37:03 +0000

    Student Scheduling Assistant  - a slight modification of the "you are not registered for any classes yet..." warning message

--- JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningMessages.java
+++ JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningMessages.java
@@ -1119,7 +1119,7 @@ public interface StudentSectioningMessages extends Messages {
 	@DefaultMessage("Student:")
 	String propStudent();
 	
-	@DefaultMessage("You are not registered for any classes yet. Please click the Enroll button to make the registration.")
+	@DefaultMessage("You are not registered for any classes yet. Please click the Enroll button in order to complete your registration..")
 	String warnScheduleEmpty();
 	
 	@DefaultMessage("You have made some changes in your schedule. Please click the Enroll button to update your registration.")
