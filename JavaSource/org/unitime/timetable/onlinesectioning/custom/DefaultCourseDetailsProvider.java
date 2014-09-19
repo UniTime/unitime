8f2b97a12d3730cd9926b597568d59f56676ce1d
Tomáš Müller <Tomas-Muller@users.noreply.github.com>
2014-09-19 19:02:02 +0000

    Default Course Details Provider  - default course details provider can now download course details from the given web page (using unitime.custom.default.course_url application property)    - when unitime.custom.default.course_download is set to true    - there can be additional manipulation done with the downloaded page (see the new unitime.custom.default.course_regexp and unitime.custom.default.course_modifiers properties)  - it can also act as CourseUrlProvider
    External Term Provider
     - a new interface that can be used by the course url / details (and other) providers to map academic session info to external term / campus
       - subject areas and course numbers can be manipulated as well

--- JavaSource/org/unitime/timetable/onlinesectioning/custom/DefaultCourseDetailsProvider.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/custom/DefaultCourseDetailsProvider.java
@@ -19,18 +19,31 @@
 */
 package org.unitime.timetable.onlinesectioning.custom;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.unitime.localization.impl.Localization;
+import org.unitime.timetable.ApplicationProperties;
+import org.unitime.timetable.defaults.ApplicationProperty;
 import org.unitime.timetable.gwt.resources.StudentSectioningConstants;
 import org.unitime.timetable.gwt.resources.StudentSectioningMessages;
 import org.unitime.timetable.gwt.shared.SectioningException;
 import org.unitime.timetable.model.CourseOffering;
 import org.unitime.timetable.onlinesectioning.AcademicSessionInfo;
+import org.unitime.timetable.onlinesectioning.custom.purdue.BannerTermProvider;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -39,13 +52,59 @@ import freemarker.template.TemplateException;
 /**
  * @author Tomas Muller
  */
-public class DefaultCourseDetailsProvider implements CourseDetailsProvider {
+public class DefaultCourseDetailsProvider implements CourseDetailsProvider, CourseUrlProvider {
+	private static Logger sLog = Logger.getLogger(DefaultCourseDetailsProvider.class);
 	private static StudentSectioningMessages MSG = Localization.create(StudentSectioningMessages.class);
 	private static StudentSectioningConstants CONST = Localization.create(StudentSectioningConstants.class);
 	private static final long serialVersionUID = 1L;
+	private ExternalTermProvider iExternalTermProvider = null;
+	
+	protected String replaceExternal(String url, AcademicSessionInfo session, String subject, String courseNbr) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException, UnsupportedEncodingException {
+		if (iExternalTermProvider == null) {
+			String clazz = ApplicationProperty.CustomizationExternalTerm.value();
+			if (clazz == null || clazz.isEmpty()) return url;
+			iExternalTermProvider = (ExternalTermProvider)Class.forName(clazz).getConstructor().newInstance();
+		}
+		return url
+				.replace(":xterm", URLEncoder.encode(iExternalTermProvider.getExternalTerm(session), "utf-8"))
+				.replace(":xcampus", URLEncoder.encode(iExternalTermProvider.getExternalTerm(session), "utf-8"))
+				.replace(":xsubject", URLEncoder.encode(iExternalTermProvider.getExternalSubject(session, subject, courseNbr), "utf-8"))
+				.replace(":xcourseNbr", URLEncoder.encode(iExternalTermProvider.getExternalCourseNumber(session, subject, courseNbr), "utf-8"));
+	}
+	
+	protected String getExternalSubject(AcademicSessionInfo session, String subjectArea, String courseNumber) {
+		return (iExternalTermProvider == null ? subjectArea : iExternalTermProvider.getExternalSubject(session, subjectArea, courseNumber));
+	}
+
+	protected String getExternalCourseNbr(AcademicSessionInfo session, String subjectArea, String courseNumber) {
+		return (iExternalTermProvider == null ? courseNumber : iExternalTermProvider.getExternalCourseNumber(session, subjectArea, courseNumber));
+	}
+
+	
+	@Override
+	public URL getCourseUrl(AcademicSessionInfo session, String subject, String courseNbr) throws SectioningException {
+		try {
+			String url = ApplicationProperty.CustomizationDefaultCourseUrl.value();
+			if (url == null || url.isEmpty()) return null;
+			return new URL(replaceExternal(
+					url.replace(":year", URLEncoder.encode(session.getYear(), "utf-8"))
+					   .replace(":term", URLEncoder.encode(session.getTerm(), "utf-8"))
+					   .replace(":campus", URLEncoder.encode(session.getCampus(), "utf-8"))
+					   .replace(":subject", URLEncoder.encode(subject, "utf-8"))
+					   .replace(":courseNbr", URLEncoder.encode(courseNbr, "utf-8")),
+					session, subject, courseNbr));
+		} catch (Exception e) {
+			throw new SectioningException("Failed to get course URL: " + e.getMessage(), e);
+		}
+	}
+
 
 	@Override
 	public String getDetails(AcademicSessionInfo session, String subject, String courseNbr) throws SectioningException {
+		if (ApplicationProperty.CustomizationDefaultCourseDetailsDownload.isTrue()) {
+			URL url = getCourseUrl(session, subject, courseNbr);
+			if (url != null) return downloadDetails(url);
+		}
 		CourseOffering course = CourseOffering.findBySessionSubjAreaAbbvCourseNbr(session.getUniqueId(), subject, courseNbr);
 		if (course == null)
 			return MSG.infoCourseDetailsNotAvailable(subject, courseNbr);
@@ -71,6 +130,74 @@ public class DefaultCourseDetailsProvider implements CourseDetailsProvider {
 			throw new SectioningException(MSG.failedLoadCourseDetails(e.getMessage()));
 		} catch (IOException e) {
 			throw new SectioningException(MSG.failedLoadCourseDetails(e.getMessage()));
+		}
+	}
+	
+	protected String downloadDetails(URL url) throws SectioningException {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"));
+			StringBuffer content = new StringBuffer();
+			String line;
+			while ((line = in.readLine()) != null)
+				content.append(line);
+			in.close();
+			
+			Pattern pattern = Pattern.compile(ApplicationProperty.CustomizationDefaultCourseDetailsContent.value());
+			Matcher match = pattern.matcher(content);
+			if (!match.find()) 
+				return "<div class='unitime-ErrorMessage'>" + MSG.exceptionCustomCourseDetailsFailed("unable to parse <a href='"+url+"'>course detial page</a>") + "</div>";
+			String table = match.group(1);
+			
+			String modif = ApplicationProperty.CustomizationDefaultCourseDetailsModifiers.value();
+			if (modif != null && !modif.isEmpty()) {
+				String[] modifiers = modif.split("\\r?\\n");
+				for (int i = 0; i + 1 < modifiers.length; i += 2) {
+					table = table.replaceAll(modifiers[i], modifiers[i + 1]);
+				}
+			}
+			return table;
+		} catch (IOException e) {
+			sLog.error(e.getMessage(), e);
+			throw new SectioningException(MSG.exceptionCustomCourseDetailsFailed("unable to read <a href='"+url+"'>course detail page</a>"), e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			BasicConfigurator.configure();
+
+			ApplicationProperties.getDefaultProperties().setProperty(
+					ApplicationProperty.CustomizationExternalTerm.key(),
+					BannerTermProvider.class.getName());
+			ApplicationProperties.getDefaultProperties().setProperty(
+					ApplicationProperty.CustomizationDefaultCourseUrl.key(),
+					"https://selfservice.mypurdue.purdue.edu/prod/bzwsrch.p_catalog_detail?term=:xterm&subject=:xsubject&cnbr=:xcourseNbr&enhanced=Y");
+			ApplicationProperties.getDefaultProperties().setProperty(
+					ApplicationProperty.CustomizationDefaultCourseDetailsDownload.key(),
+					"true");
+			ApplicationProperties.getDefaultProperties().setProperty(
+					ApplicationProperty.CustomizationDefaultCourseDetailsContent.key(),
+					"(?idm)(<table [ ]*class=\"[a-z]*\" summary=\"This table lists the course detail for the selected term.\" .*)<table [ ]*class=\"[a-z]*\" summary=\"This is table displays line separator at end of the page.\"");
+			ApplicationProperties.getDefaultProperties().setProperty(
+					ApplicationProperty.CustomizationDefaultCourseDetailsModifiers.key(),
+					"(?i)<a href=\"[^>]*\">\n<b>\n"+
+					"(?i)</a>\n</b>\n"+
+					"(?i)<span class=[\"]?fieldlabeltext[\"]?>\n<b>\n"+
+					"(?i)</span>\n</b>\n"+
+					"(?i) class=\"nttitle\" \n class=\"unitime-MainTableHeader\" \n"+
+					"(?i) class=\"datadisplaytable\" \n class=\"unitime-MainTable\" ");
+			
+			System.out.println(
+					"URL:" + new DefaultCourseDetailsProvider().getCourseUrl(new AcademicSessionInfo(-1l, "2010", "Spring", "PWL"), "AAE", "20300A")
+					);
+			
+			System.out.println(
+					"Details:\n" +
+					new DefaultCourseDetailsProvider().getDetails(new AcademicSessionInfo(-1l, "2010", "Spring", "PWL"), "AAE", "20300A")
+					);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
