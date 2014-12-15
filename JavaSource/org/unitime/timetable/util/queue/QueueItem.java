321be5cdf07cb4502d12a692ee1292499191f0bd
Tomáš Müller <muller@unitime.org>
2014-12-15 14:47:20 +0000

    Roll Forward Session  - log all messages using the queue item's log  - display the roll forward log on the page

--- JavaSource/org/unitime/timetable/util/queue/QueueItem.java
+++ JavaSource/org/unitime/timetable/util/queue/QueueItem.java
@@ -22,6 +22,7 @@ package org.unitime.timetable.util.queue;
 import java.io.File;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
 import org.apache.log4j.Logger;
 import org.unitime.localization.impl.Localization;
 import org.unitime.timetable.ApplicationProperties;
@@ -35,8 +36,8 @@ import org.unitime.timetable.security.UserContext;
  * @author Tomas Muller
  *
  */
-public abstract class QueueItem {
-    protected static Logger sLog = Logger.getLogger(QueueItem.class);
+public abstract class QueueItem implements Log {
+    protected static Logger iLogger;
 
 	private Long iSessionId;
 	private String iOwnerId;
@@ -52,6 +53,7 @@ public abstract class QueueItem {
 	private Long iId = null;
 	
 	public QueueItem(Long sessionId, UserContext owner) {
+		iLogger = Logger.getLogger(getClass());
 		iSessionId = sessionId;
 		iOwnerId = owner.getExternalUserId();
 		iOwnerName = owner.getName();
@@ -87,8 +89,7 @@ public abstract class QueueItem {
 		try {
 			execute();
 		} catch (Exception e) {
-			sLog.error(e.getMessage(), e);
-			setError(e);
+			fatal("Execution failed.", e);
 		} finally {
 			ApplicationProperties.setSessionId(null);
 			_RootDAO.closeCurrentThreadSessions();
@@ -138,4 +139,131 @@ public abstract class QueueItem {
 	public Date created() { return iCreated; }
 	public Date started() { return iStarted; }
 	public Date finished() { return iFinished; }
+	
+	@Override
+	public void trace(Object message) {
+		iLogger.trace(message);
+	}
+
+	@Override
+	public void trace(Object message, Throwable exception) {
+		iLogger.trace(message, exception);
+	}
+	
+	@Override
+	public void debug(Object message) {
+		debug(message, null);
+	}
+
+	@Override
+	public void debug(Object message, Throwable exception) {
+		iLogger.debug(message, exception);
+		if (exception == null) {
+			if (message != null) log(message.toString());
+		} else if (message == null) {
+			log(exception.getClass().getSimpleName() + ": " + exception.getMessage());
+		} else {
+			log(message + " (" + exception.getClass().getSimpleName() + ": " + exception.getMessage() + ")");
+		}
+	}
+	
+	@Override
+	public void warn(Object message) {
+		warn(message, null);	
+	}
+
+	@Override
+	public void warn(Object message, Throwable exception) {
+		iLogger.warn(message, exception);
+		if (exception == null) {
+			if (message != null) warn(message.toString());
+		} else if (message == null) {
+			warn(exception.getClass().getSimpleName() + ": " + exception.getMessage());
+		} else {
+			warn(message + " (" + exception.getClass().getSimpleName() + ": " + exception.getMessage() + ")");
+		}
+	}
+	
+	@Override
+	public void info(Object message) {
+		info(message, null);
+	}
+
+	@Override
+	public void info(Object message, Throwable exception) {
+		iLogger.info(message, exception);
+		if (exception == null) {
+			if (message != null) log(message.toString());
+		} else if (message == null) {
+			log(exception.getClass().getSimpleName() + ": " + exception.getMessage());
+		} else {
+			log(message + " (" + exception.getClass().getSimpleName() + ": " + exception.getMessage() + ")");
+		}
+	}
+
+	@Override
+	public void error(Object message) {
+		error(message, null);
+	}
+
+	@Override
+	public void error(Object message, Throwable exception) {
+		iLogger.error(message, exception);
+		if (exception == null) {
+			if (message != null) error(message.toString());
+		} else if (message == null) {
+			error(exception.getClass().getSimpleName() + ": " + exception.getMessage());
+		} else {
+			error(message + " (" + exception.getClass().getSimpleName() + ": " + exception.getMessage() + ")");
+		}
+	}
+
+	@Override
+	public void fatal(Object message) {
+		fatal(message, null);
+	}
+
+	@Override
+	public void fatal(Object message, Throwable exception) {
+		iLogger.fatal(message, exception);
+		if (exception == null) {
+			if (message != null) error(message.toString());
+		} else if (message == null) {
+			error(exception.getClass().getSimpleName() + ": " + exception.getMessage());
+		} else {
+			error(message + " (" + exception.getClass().getSimpleName() + ": " + exception.getMessage() + ")");
+		}
+		if (exception != null)
+			setError(exception);
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isFatalEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isTraceEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		return true;
+	}
 }
