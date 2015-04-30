15fab0710ad3a9f04e958a5d55acc5d6c79e6bff
tomas-muller <muller@unitime.org>
2015-04-30 22:27:23 +0200

    Export PDF (using the new export servlet)
    - added ability to export richer documents (font style, colors, images)

--- JavaSource/org/unitime/timetable/export/PDFPrinter.java
+++ JavaSource/org/unitime/timetable/export/PDFPrinter.java
@@ -19,6 +19,7 @@
 */
 package org.unitime.timetable.export;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -37,6 +38,8 @@ import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfContentByte;
@@ -51,7 +54,7 @@ import com.lowagie.text.pdf.PdfWriter;
 public class PDFPrinter implements Printer {
 	private static Pattern sNumber = Pattern.compile("[+-]?[0-9]*\\.?[0-9]*[a-z]?");
 	private OutputStream iOutput;
-	private String[] iLastLine = null;
+	private Object[] iLastLine = null;
 	private boolean iCheckLast = false;
 	private PdfPTable iTable = null;
 	private float[] iMaxWidth = null;
@@ -91,7 +94,9 @@ public class PDFPrinter implements Printer {
 			cell.setHorizontalAlignment(Element.ALIGN_LEFT);
 			
 			Font font = PdfFont.getFont(true);
-			cell.addElement(new Chunk(f, font));		
+			Paragraph ch = new Paragraph(f, font);
+			ch.setLeading(0f, 1f);
+			cell.addElement(ch);		
 			iTable.addCell(cell);
 			
 			float width = 0; 
@@ -144,6 +149,89 @@ public class PDFPrinter implements Printer {
 		iLastLine = fields;
 	}
 	
+	public void printLine(A... fields) {
+		PdfPCellEvent setLineDashEvent = new PdfPCellEvent() {
+			@Override
+			public void cellLayout(PdfPCell cell, Rectangle rect, PdfContentByte[] canvas) {
+				PdfContentByte cb = canvas[PdfPTable.LINECANVAS];
+				cb.setLineDash(new float[] {2, 2}, 0);
+			}
+		};
+		
+		for (int idx = 0; idx < fields.length; idx++) {
+			if (iHiddenColumns.contains(idx)) continue;
+			A f = fields[idx];
+			if (f == null || f.isEmpty() || (iCheckLast && f.equals(iLastLine == null || idx >= iLastLine.length ? null : iLastLine[idx]))) f = new A();
+			
+			PdfPCell cell = new PdfPCell();
+			float rpad = 0f;
+			cell.setBorder(iLastLine == null ? Rectangle.TOP : Rectangle.NO_BORDER);
+			cell.setVerticalAlignment(Element.ALIGN_TOP);
+			if (f.has(F.RIGHT)) {
+				cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+				cell.setPaddingRight(10f);
+				rpad = 10f;
+			} else if (f.has(F.CENTER)) {
+				cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+			} else {
+				cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+			}
+			cell.setPaddingBottom(4f);
+			cell.setCellEvent(setLineDashEvent);
+			
+			if (f.hasImage()) {
+				try {
+					Image img = Image.getInstance(f.getImage(), Color.WHITE);
+					cell.addElement(new Chunk(img, 0f, 0f));
+					iMaxWidth[idx] = Math.max(iMaxWidth[idx], img.getWidth());
+				} catch (Exception e) {}
+			}
+
+			if (f.hasText()) {
+				Font font = PdfFont.getFont(f.has(F.BOLD), f.has(F.ITALIC), f.has(F.UNDERLINE), f.getColor());
+				Paragraph ch = new Paragraph(f.getText(), font);
+				ch.setLeading(0f, 1f);
+				cell.addElement(ch);
+				float width = 0; 
+				if (f.getText().indexOf('\n')>=0) {
+					for (StringTokenizer s = new StringTokenizer(f.getText(),"\n"); s.hasMoreTokens();)
+						width = Math.max(width,font.getBaseFont().getWidthPoint(s.nextToken(), font.getSize()));
+				} else 
+					width = Math.max(width,font.getBaseFont().getWidthPoint(f.getText(), font.getSize()));
+				iMaxWidth[idx] = Math.max(iMaxWidth[idx], width + rpad);
+			}
+			
+			if (f.hasChunks()) {
+				for (A g: f.getChunks()) {
+					if (g.hasImage()) {
+						try {
+							Image img = Image.getInstance(g.getImage(), Color.WHITE);
+							cell.addElement(new Chunk(img, 0f, 0f));
+							cell.addElement(img);
+							iMaxWidth[idx] = Math.max(iMaxWidth[idx], img.getWidth());
+						} catch (Exception e) {}
+					}
+					if (g.hasText()) {
+						Font font = PdfFont.getFont(g.has(F.BOLD), g.has(F.ITALIC), g.has(F.UNDERLINE), g.getColor());
+						Paragraph ch = new Paragraph(g.getText(), font);
+						ch.setLeading(0f, 1f);
+						cell.addElement(ch);
+						float width = 0; 
+						if (g.getText().indexOf('\n')>=0) {
+							for (StringTokenizer s = new StringTokenizer(g.getText(),"\n"); s.hasMoreTokens();)
+								width = Math.max(width,font.getBaseFont().getWidthPoint(s.nextToken(), font.getSize()));
+						} else 
+							width = Math.max(width,font.getBaseFont().getWidthPoint(g.getText(), font.getSize()));
+						iMaxWidth[idx] = Math.max(iMaxWidth[idx], width + rpad);
+					}
+				}
+			}
+			
+			iTable.addCell(cell);
+		}
+		iLastLine = fields;
+	}
+	
 	@Override
 	public void flush() {
 		iLastLine = null;
@@ -165,6 +253,99 @@ public class PDFPrinter implements Printer {
 			document.close();
 		} catch (DocumentException e) {
 			throw new IOException(e.getMessage(), e);
+		}
+	}
+	
+	public static class A {
+		private List<A> iChunks = null;
+		private String iText = null;
+		private int iFlag = 0;
+		private String iColor = null;
+		private java.awt.Image iImage = null;
+		
+		public A() {}
+		
+		public A(String text, F... flags) {
+			iText = text;
+			for (F f: flags)
+				iFlag = f.set(iFlag);
+		}
+		
+		public A(A... chunks) {
+			iChunks = new ArrayList<A>();
+			for (A ch: chunks)
+				iChunks.add(ch);
+		}
+		
+		public A(java.awt.Image image) {
+			iImage = image;
+		}
+		
+		public void setColor(String color) {
+			if (color != null && color.startsWith("#")) color = color.substring(1);
+			iColor = color;
+		}
+		public boolean hasColor() { return iColor != null && !iColor.isEmpty(); }
+		public Color getColor() {
+			try {
+				return hasColor() ? new Color(Integer.parseInt(iColor,16)) : Color.BLACK; 
+			} catch (Exception e) {
+				e.printStackTrace();
+				return Color.BLACK;
+			}
+		}
+		
+		public String getText() { return iText == null ? "" : iText; }
+		public boolean hasText() { return iText != null && !iText.isEmpty(); }
+		
+		public boolean hasImage() { return iImage != null; }
+		public java.awt.Image getImage() { return iImage; }
+		
+		public A add(A chunk) {
+			if (iChunks == null) iChunks = new ArrayList<A>();
+			iChunks.add(chunk);
+			return this;
+		}
+		public List<A> getChunks() { return iChunks; }
+		public boolean hasChunks() { return iChunks != null && !iChunks.isEmpty(); }
+		public int size() { return iChunks == null ? 0 : iChunks.size(); }
+		public A getChunk(int idx) { return iChunks.get(idx); }
+		
+		public boolean has(F flag) { return flag.in(iFlag); }
+		public A set(F flag) { iFlag = flag.set(iFlag); return this; }
+		
+		public boolean isEmpty() { return !hasChunks() && !hasText() && !hasImage(); }
+		
+		public boolean equals(Object o) {
+			if (o == null || !(o instanceof A)) return false;
+			A a = (A)o;
+			if (size() != a.size() || !getText().equals(a.getText())) return false;
+			for (int i = 0; i < size(); i++)
+				if (!getChunk(i).equals(a.getChunk(i))) return false;
+			return true;
+		}
+		
+		public A bold() { set(F.BOLD); return this; }
+		public A italic() { set(F.ITALIC); return this; }
+		public A underline() { set(F.UNDERLINE); return this; }
+		public A color(String color) { setColor(color); return this; }
+		public A center() { set(F.CENTER); return this; }
+		public A right() { set(F.RIGHT); return this; }
+	} 
+	
+	public static enum F {
+		ITALIC, BOLD, UNDERLINE, RIGHT, CENTER,
+		;
+		
+		public int flag() { return 1 << ordinal(); }
+		public boolean in(int flags) {
+			return (flags & flag()) != 0;
+		}
+		public int set(int flags) {
+			return (in(flags) ? flags : flags + flag());
+		}
+		public int clear(int flags) {
+			return (in(flags) ? flags - flag() : flags);
 		}
 	}
 
