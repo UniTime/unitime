70c266c9aa86b915745c27306b5468cec0d25135
tomas-muller <muller@unitime.org>
2015-06-23 19:04:23 +0200

    API: class-info, instructor-schedule
    - added exam.type

--- JavaSource/org/unitime/timetable/api/connectors/InstructorScheduleConnector.java
+++ JavaSource/org/unitime/timetable/api/connectors/InstructorScheduleConnector.java
@@ -300,6 +300,7 @@ public class InstructorScheduleConnector extends ApiConnector{
 	protected class ExamInfo {
 		Long iExamId;
 		String iName;
+		String iType;
 		Integer iSize;
 		List<ExamOwnerInfo> iOwners = new ArrayList<ExamOwnerInfo>();
 		PeriodInfo iPeriod;
@@ -308,6 +309,7 @@ public class InstructorScheduleConnector extends ApiConnector{
 		ExamInfo(Exam exam) {
 			iExamId = exam.getUniqueId();
 			iName = exam.getName();
+			iType = exam.getExamType().getReference();
 			for (ExamOwner owner: exam.getOwners())
 				iOwners.add(new ExamOwnerInfo(owner));
 			iSize = exam.getSize();
