UniTime/unitime

commit 638ea61c0ec7b12f4ca50589bebc173f980e8998

Rooms
- Department.getRoomSharingColor(other) made a little faster

JavaSource/org/unitime/timetable/model/Department.java
@@ -203,14 +203,12 @@ public class Department extends BaseDepartment implements Comparable<Department>
 	}
 	
 	public boolean isRoomSharingColorConflicting(String color) {
-		for (Iterator i=getRoomDepts().iterator();i.hasNext();) {
-			RoomDept rd = (RoomDept)i.next();
-			for (Iterator j=rd.getRoom().getRoomDepts().iterator();j.hasNext();) {
-				BaseDepartment d = (BaseDepartment)((RoomDept)j.next()).getDepartment();
-				if (d.equals(this)) continue;
-				if (d.getRoomSharingColor()==null) continue;
-				if (distance(color, d.getRoomSharingColor())<50) return true; 
-			}
+		if (getUniqueId() == null) return false;
+		for (String other: (List<String>)DepartmentDAO.getInstance().getSession().createQuery(
+				"select distinct x.department.roomSharingColor from Department d inner join d.roomDepts rd inner join rd.room.roomDepts x " +
+				"where d.uniqueId = :uniqueId and d != x.department"
+				).setLong("uniqueId", getUniqueId()).setCacheable(true).list()) {
+			if (distance(color, other) < 50) return true;
 		}
 		return false;
 	}
@@ -256,7 +254,7 @@ public class Department extends BaseDepartment implements Comparable<Department>
 	}
 	
 	public String getRoomSharingColor(Collection otherDepartments) {
-		if (getRoomSharingColor()==null) {
+		if (getRoomSharingColor() == null) {
 			setRoomSharingColor(color2hex(RoomSharingModel.sDepartmentColors[0]));
 		}
 		fixRoomSharingColor(otherDepartments);

JavaSource/org/unitime/timetable/server/rooms/RoomDetailsBackend.java
@@ -163,7 +163,7 @@ public class RoomDetailsBackend extends RoomFilterBackend {
 		if (pref != null && !PreferenceLevel.sNeutral.equals(pref.getPrefProlog()))
 			department.setPreference(new PreferenceInterface(pref.getUniqueId(), PreferenceLevel.prolog2color(pref.getPrefProlog()), pref.getPrefProlog(), pref.getPrefName(), false));
 		if (location != null)
-			department.setColor("#" + d.getRoomSharingColor(location.getRoomDepts()));
+			department.setColor("#" + d.getRoomSharingColor(null));
 		return department;
 	}
 	

commit 3e2d00853ac3b5c6558954f30696c4dd7d392270

Rooms: a few fixes
- event manager should be able to see / change event properties even when Events are not yet enabled for the term
- faster room sharing color: fixed a null exception introduced in commit 638ea61

JavaSource/org/unitime/timetable/model/Department.java
@@ -208,7 +208,7 @@ public class Department extends BaseDepartment implements Comparable<Department>
 				"select distinct x.department.roomSharingColor from Department d inner join d.roomDepts rd inner join rd.room.roomDepts x " +
 				"where d.uniqueId = :uniqueId and d != x.department"
 				).setLong("uniqueId", getUniqueId()).setCacheable(true).list()) {
-			if (distance(color, other) < 50) return true;
+			if (other != null && distance(color, other) < 50) return true;
 		}
 		return false;
 	}

JavaSource/org/unitime/timetable/server/rooms/RoomDetailsBackend.java
@@ -120,7 +120,7 @@ public class RoomDetailsBackend extends RoomFilterBackend {
     	
     	boolean courses = context.hasPermission(Right.InstructionalOfferings) || context.hasPermission(Right.Classes);
     	boolean exams = context.hasPermission(Right.Examinations);
-    	boolean events = context.hasPermission(Right.Events);
+    	boolean events = context.hasPermission(Right.Events) || context.getUser().getCurrentAuthority().hasRight(Right.RoomEditChangeEventProperties);
     	boolean editPermissions = request.hasOption("id");
 
 		Map<Long, Double> distances = new HashMap<Long, Double>();

JavaSource/org/unitime/timetable/server/rooms/RoomPropertiesBackend.java
@@ -110,6 +110,8 @@ public class RoomPropertiesBackend implements GwtRpcImplementation<RoomPropertie
 			response.setCanChangePicture(context.getUser().getCurrentAuthority().hasRight(Right.RoomEditChangePicture));
 			response.setCanChangePreferences(context.getUser().getCurrentAuthority().hasRight(Right.RoomEditPreference));
 			response.setCanChangeDefaultGroup(context.getUser().getCurrentAuthority().hasRight(Right.GlobalRoomGroupEditSetDefault));
+			if (!response.isCanSeeEvents() && response.isCanChangeEventProperties())
+				response.setCanSeeEvents(true);
 		}
 		
 		for (RoomType type: RoomType.findAll())