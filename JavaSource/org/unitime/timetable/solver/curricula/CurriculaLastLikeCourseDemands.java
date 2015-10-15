6aeeacd137f1ff7e721badb1a837044ec01cbba7
tomas-muller <muller@unitime.org>
2015-10-15 21:09:11 +0200

    Curriculum Timetabling: Students Cache
    - changed the type of CurriculumClassifications.students to String (it is CLOB on the database side), and only parse its content (XML) when needed

--- JavaSource/org/unitime/timetable/solver/curricula/CurriculaLastLikeCourseDemands.java
+++ JavaSource/org/unitime/timetable/solver/curricula/CurriculaLastLikeCourseDemands.java
@@ -391,7 +391,8 @@ public class CurriculaLastLikeCourseDemands implements StudentCourseDemands {
 			// Load model from cache (if exists)
 			Solution<CurVariable, CurValue> cachedSolution = null;
 			iAssignment = new DefaultSingleAssignment<CurVariable, CurValue>();
-			Element cache = (iClassification.getStudents() == null ? null : iClassification.getStudents().getRootElement());
+			Document cachedXml = iClassification.getStudentsDocument();
+			Element cache = (cachedXml == null ? null : cachedXml.getRootElement());
 			if (cache != null && cache.getName().equals(getCacheName())) {
 				cachedSolution = CurModel.loadFromXml(cache);
 				if (iSetStudentCourseLimits)
@@ -451,7 +452,7 @@ public class CurriculaLastLikeCourseDemands implements StudentCourseDemands {
 				Document doc = DocumentHelper.createDocument();
 				iModel.saveAsXml(doc.addElement(getCacheName()), iAssignment);
 				// sLog.debug("Model:\n" + doc.asXML());
-				iClassification.setStudents(doc);
+				iClassification.setStudentsDocument(doc);
 
 				hibSession.update(iClassification);
 			}
