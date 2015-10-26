UniTime/unitime

commit 592152dea3d2b8489ffd7b639f416ede9488eaaf

Technology update
- updated Hibernate to 4.3.11 (was 4.3.5)
- updated Infinispan to 7.2.5 (was 6.0.1)
- updated Ehcache to 2.6.11 (was 2.6.6)
- updated JGroups to 3.6.6 (was 3.4.1)
- updated Joda to 2.8.2 (was 2.3)
- updated Protocol Buffers to 2.6.1 (was 2.5.0)
- updated Spring to 3.2.14 (was 3.1.4)
- updated Spring Security to 3.2.8 (was 3.1.4)
- updates Spring Integration to 3.0.7 (was 2.2.4)
- updated Commons FileUpload to 1.3.1 (was 1.3)
- updated Commons Configuration to 1.10 (was 1.9)
- updated Commons IO to 1.4 (was 1.3.2)
- updated Restlet to 2.3.4 (was 2.2.1)
- updated SLF4J to 1.7.12 (was 1.7.5)
- updated AspectJ to 1.8.7 (was 1.7.3)
- updated Javassist to 3.20 (was 3.18)
- updated Freemaker to 2.3.23 (was 2.3.20)
- updated JSON to 20141113 (was 20140107)
- updated GSON to 2.3.1 (was 2.2.4)
- updated Biweekly to 0.4.3 (was 0.3.3)

- UniTime Cluster Discovery was rewritten to meet the changes in Group 3.6
- A few changes have been done to ICS exports to reflect the changes in Biweekly
- Online Student Scheduling Server replication: replaced batch invocation with a simple transaction

JavaSource/org/unitime/timetable/events/EventEmail.java
@@ -69,11 +69,11 @@ import org.unitime.timetable.util.Constants;
 import org.unitime.timetable.util.NameFormat;
 
 import biweekly.Biweekly;
+import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.property.CalendarScale;
 import biweekly.property.Method;
 import biweekly.property.Status;
-import biweekly.property.Version;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -329,7 +329,7 @@ public class EventEmail {
 
 	public String icalendar() throws IOException {
 		ICalendar ical = new ICalendar();
-		ical.setVersion(Version.v2_0());
+		ical.setVersion(ICalVersion.V2_0);
 		ical.setCalendarScale(CalendarScale.gregorian());
 		ical.setMethod(new Method("PUBLISH"));
 		ical.setExperimentalProperty("X-WR-CALNAME", "UniTime Schedule");

JavaSource/org/unitime/timetable/export/events/EventsExportEventsToICal.java
@@ -41,6 +41,7 @@ import org.unitime.timetable.gwt.shared.EventInterface.MeetingInterface;
 import org.unitime.timetable.util.Constants;
 
 import biweekly.Biweekly;
+import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import biweekly.parameter.Role;
@@ -54,7 +55,6 @@ import biweekly.property.Method;
 import biweekly.property.Organizer;
 import biweekly.property.RecurrenceId;
 import biweekly.property.Status;
-import biweekly.property.Version;
 import biweekly.util.Recurrence;
 import biweekly.util.Recurrence.DayOfWeek;
 import biweekly.util.Recurrence.Frequency;
@@ -75,7 +75,7 @@ public class EventsExportEventsToICal extends EventsExporter {
 		helper.setup("text/calendar", reference(), false);
 		
 		ICalendar ical = new ICalendar();
-		ical.setVersion(Version.v2_0());
+		ical.setVersion(ICalVersion.V2_0);
 		ical.setCalendarScale(CalendarScale.gregorian());
 		ical.setMethod(new Method("PUBLISH"));
 		ical.setExperimentalProperty("X-WR-CALNAME", "UniTime Schedule");
@@ -158,7 +158,7 @@ public class EventsExportEventsToICal extends EventsExporter {
             recur.workweekStarts(DayOfWeek.MONDAY).until(until.toDate());
             master.setRecurrenceRule(recur.build());
             
-            ExceptionDates exdates = new ExceptionDates(true);
+            ExceptionDates exdates = new ExceptionDates();
             // for all dates till the last date
             dates: for (DateTime date = first.getStart(); !date.isAfter(until); date = date.plusDays(1)) {
             	// skip days of week with no meeting
@@ -177,8 +177,6 @@ public class EventsExportEventsToICal extends EventsExporter {
             		if (date.getYear() == ics.getStart().getYear() && date.getDayOfYear() == ics.getStart().getDayOfYear()) {
         				VEvent x = new VEvent();
         				RecurrenceId id = new RecurrenceId(date.toDate(), true);
-        				id.setLocalTime(false);
-        				id.setTimezoneId(TimeZone.getDefault().getID());
         				x.setRecurrenceId(id);
         				x.setDateStart(ics.getDateStart());
         				x.setDateEnd(ics.getDateEnd());
@@ -202,8 +200,6 @@ public class EventsExportEventsToICal extends EventsExporter {
     			// use exception as recurrence if there is one available
     			if (!exdates.getValues().isEmpty()) {
     				RecurrenceId id = new RecurrenceId(exdates.getValues().get(0), true);
-    				id.setLocalTime(false);
-    				id.setTimezoneId(TimeZone.getDefault().getID());
     				x.setRecurrenceId(id);
     				exdates.getValues().remove(0);
     			}
@@ -242,23 +238,19 @@ public class EventsExportEventsToICal extends EventsExporter {
             	int idx = 0;
             	for (ContactInterface instructor: event.getInstructors()) {
             		if (idx++ == 0) {
-            			Organizer organizer = new Organizer("mailto:" + (instructor.hasEmail() ? instructor.getEmail() : ""));
-            			organizer.setCommonName(instructor.getName(MESSAGES));
+            			Organizer organizer = new Organizer(instructor.getName(MESSAGES), (instructor.hasEmail() ? instructor.getEmail() : ""));
             			vevent.setOrganizer(organizer);
             		} else {
-                		Attendee attendee = new Attendee("mailto:" + (instructor.hasEmail() ? instructor.getEmail() : ""));
-                		attendee.setCommonName(instructor.getName(MESSAGES));
+                		Attendee attendee = new Attendee(instructor.getName(MESSAGES), (instructor.hasEmail() ? instructor.getEmail() : ""));
                 		attendee.setRole(Role.CHAIR);
                 		vevent.addAttendee(attendee);
             		}
             	}
             } else if (event.hasSponsor()) {
-    			Organizer organizer = new Organizer("mailto:" + (event.getSponsor().hasEmail() ? event.getSponsor().getEmail() : ""));
-    			organizer.setCommonName(event.getSponsor().getName());
+    			Organizer organizer = new Organizer(event.getSponsor().getName(), (event.getSponsor().hasEmail() ? event.getSponsor().getEmail() : ""));
     			vevent.setOrganizer(organizer);
             } else if (event.hasContact()) {
-    			Organizer organizer = new Organizer("mailto:" + (event.getContact().hasEmail() ? event.getContact().getEmail() : ""));
-    			organizer.setCommonName(event.getContact().getName(MESSAGES));
+    			Organizer organizer = new Organizer(event.getContact().getName(MESSAGES), (event.getContact().hasEmail() ? event.getContact().getEmail() : ""));
     			vevent.setOrganizer(organizer);
             }
             ical.addEvent(vevent);
@@ -299,16 +291,12 @@ public class EventsExportEventsToICal extends EventsExporter {
 		public DateTime getStart() { return iStart; }
 		public DateStart getDateStart() {
 			DateStart ds = new DateStart(iStart.toDate(), true);
-			ds.setLocalTime(false);
-			ds.setTimezoneId(TimeZone.getDefault().getID());
 			return ds;
 		}
 		
 		public DateTime getEnd() { return iEnd; }
 		public DateEnd getDateEnd() {
 			DateEnd de = new DateEnd(iEnd.toDate(), true);
-			de.setLocalTime(false);
-			de.setTimezoneId(TimeZone.getDefault().getID());
 			return de;
 		}
 

JavaSource/org/unitime/timetable/gwt/server/CalendarServlet.java
@@ -84,6 +84,7 @@ import org.unitime.timetable.util.Constants;
 import org.unitime.timetable.util.DateUtils;
 
 import biweekly.Biweekly;
+import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import biweekly.component.VFreeBusy;
@@ -96,7 +97,6 @@ import biweekly.property.ExceptionDates;
 import biweekly.property.Method;
 import biweekly.property.Organizer;
 import biweekly.property.Status;
-import biweekly.property.Version;
 import biweekly.util.Recurrence;
 import biweekly.util.Recurrence.DayOfWeek;
 import biweekly.util.Recurrence.Frequency;
@@ -171,7 +171,7 @@ public class CalendarServlet extends HttpServlet {
 		response.setHeader( "Content-Disposition", "attachment; filename=\"schedule.ics\"" );
         
 		ICalendar ical = new ICalendar();
-		ical.setVersion(Version.v2_0());
+		ical.setVersion(ICalVersion.V2_0);
 		ical.setCalendarScale(CalendarScale.gregorian());
 		ical.setMethod(new Method("PUBLISH"));
 		ical.setExperimentalProperty("X-WR-CALNAME", "UniTime Schedule");
@@ -287,11 +287,11 @@ public class CalendarServlet extends HttpServlet {
         VEvent vevent = new VEvent();
         vevent.setSequence(0);
         vevent.setUid(exam.getUniqueId().toString());
-    	DateStart dstart = new DateStart(exam.getAssignedPeriod().getStartTime(), true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+    	DateStart dstart = new DateStart(exam.getAssignedPeriod().getStartTime(), true);
     	vevent.setDateStart(dstart);
         Calendar endTime = Calendar.getInstance(); endTime.setTime(exam.getAssignedPeriod().getStartTime());
         endTime.add(Calendar.MINUTE, exam.getLength());
-    	DateEnd dend = new DateEnd(endTime.getTime(), true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+    	DateEnd dend = new DateEnd(endTime.getTime(), true);
     	vevent.setDateEnd(dend);
     	vevent.setSummary(exam.getLabel()+" ("+exam.getExamType().getLabel()+" Exam)");
         if (!exam.getAssignedRooms().isEmpty()) {
@@ -412,9 +412,9 @@ public class CalendarServlet extends HttpServlet {
     	cal.set(Calendar.SECOND, 0);
 
     	VEvent vevent = new VEvent();
-    	DateStart dstart = new DateStart(first, true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+    	DateStart dstart = new DateStart(first, true);
     	vevent.setDateStart(dstart);
-    	DateEnd dend = new DateEnd(firstEnd, true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+    	DateEnd dend = new DateEnd(firstEnd, true);
     	vevent.setDateEnd(dend);
     	
     	Recurrence.Builder recur = new Recurrence.Builder(Frequency.WEEKLY);
@@ -439,7 +439,7 @@ public class CalendarServlet extends HttpServlet {
         recur.workweekStarts(DayOfWeek.MONDAY).until(last);
         vevent.setRecurrenceRule(recur.build());
 
-        ExceptionDates exdates = new ExceptionDates(true);
+        ExceptionDates exdates = new ExceptionDates();
     	while (idx < time.getWeekCode().length()) {
     		int dow = cal.get(Calendar.DAY_OF_WEEK);
     		boolean offered = false;
@@ -506,12 +506,10 @@ public class CalendarServlet extends HttpServlet {
         if (clazz.isDisplayInstructor()) {
             for (ClassInstructor instructor: clazz.getClassInstructors()) {
 				if (vevent.getOrganizer() == null) {
-					Organizer organizer = new Organizer("mailto:" + (instructor.getInstructor().getEmail() != null ? instructor.getInstructor().getEmail() : ""));
-					organizer.setCommonName(instructor.getInstructor().getNameLastFirst());
+					Organizer organizer = new Organizer(instructor.getInstructor().getNameLastFirst(), (instructor.getInstructor().getEmail() != null ? instructor.getInstructor().getEmail() : ""));
 					vevent.setOrganizer(organizer);
 				} else {
-					Attendee attendee = new Attendee("mailto:" + (instructor.getInstructor().getEmail() != null ? instructor.getInstructor().getEmail() : ""));
-					attendee.setCommonName(instructor.getInstructor().getNameLastFirst());
+					Attendee attendee = new Attendee(instructor.getInstructor().getNameLastFirst(), (instructor.getInstructor().getEmail() != null ? instructor.getInstructor().getEmail() : ""));
 					attendee.setRole(Role.CHAIR);
 					vevent.addAttendee(attendee);
 				}
@@ -602,10 +600,10 @@ public class CalendarServlet extends HttpServlet {
     	if (last == null) return;
     	
     	VFreeBusy vfree = new VFreeBusy();
-    	DateStart dstart = new DateStart(first, true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+    	DateStart dstart = new DateStart(first, true);
     	vfree.setDateStart(dstart);
     	Calendar c = Calendar.getInstance(Locale.US); c.setTime(first); c.add(Calendar.MINUTE, Constants.SLOT_LENGTH_MIN * len);
-    	DateEnd dend = new DateEnd(c.getTime(), true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+    	DateEnd dend = new DateEnd(c.getTime(), true);
     	vfree.setDateEnd(dend);
     	vfree.addComment("Free Time");
     	ical.addFreeBusy(vfree);
@@ -646,10 +644,10 @@ public class CalendarServlet extends HttpServlet {
         	    	cal.set(Calendar.SECOND, 0);
         	    	
         	    	vfree = new VFreeBusy();
-        	    	dstart = new DateStart(cal.getTime(), true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+        	    	dstart = new DateStart(cal.getTime(), true);
         	    	vfree.setDateStart(dstart);
         	    	cal.add(Calendar.MINUTE, Constants.SLOT_LENGTH_MIN * len);
-        	    	dend = new DateEnd(cal.getTime(), true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+        	    	dend = new DateEnd(cal.getTime(), true);
         	    	vfree.setDateEnd(dend);
         	    	vfree.addComment("Free Time");
         	    	ical.addFreeBusy(vfree);
@@ -762,16 +760,12 @@ public class CalendarServlet extends HttpServlet {
 		public DateTime getStart() { return iStart; }
 		public DateStart getDateStart() {
 			DateStart ds = new DateStart(iStart.toDate(), true);
-			ds.setLocalTime(false);
-			ds.setTimezoneId(TimeZone.getDefault().getID());
 			return ds;
 		}
 		
 		public DateTime getEnd() { return iEnd; }
 		public DateEnd getDateEnd() {
 			DateEnd de = new DateEnd(iEnd.toDate(), true);
-			de.setLocalTime(false);
-			de.setTimezoneId(TimeZone.getDefault().getID());
 			return de;
 		}
 

JavaSource/org/unitime/timetable/onlinesectioning/updates/CalendarExport.java
@@ -48,6 +48,7 @@ import org.unitime.timetable.server.CourseDetailsBackend;
 import org.unitime.timetable.util.Constants;
 
 import biweekly.Biweekly;
+import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import biweekly.component.VFreeBusy;
@@ -60,7 +61,6 @@ import biweekly.property.ExceptionDates;
 import biweekly.property.Method;
 import biweekly.property.Organizer;
 import biweekly.property.Status;
-import biweekly.property.Version;
 import biweekly.util.Recurrence;
 import biweekly.util.Recurrence.DayOfWeek;
 import biweekly.util.Recurrence.Frequency;
@@ -82,7 +82,7 @@ public class CalendarExport implements OnlineSectioningAction<String>{
 	public String execute(OnlineSectioningServer server, OnlineSectioningHelper helper) {
 		try {
 			ICalendar ical = new ICalendar();
-			ical.setVersion(Version.v2_0());
+			ical.setVersion(ICalVersion.V2_0);
 			ical.setCalendarScale(CalendarScale.gregorian());
 			ical.setMethod(new Method("PUBLISH"));
 			ical.setExperimentalProperty("X-WR-CALNAME", "UniTime Schedule");
@@ -121,7 +121,7 @@ public class CalendarExport implements OnlineSectioningAction<String>{
 	public static String getCalendar(OnlineSectioningServer server, XStudent student) throws IOException {
 		if (student == null) return null;
 		ICalendar ical = new ICalendar();
-		ical.setVersion(Version.v2_0());
+		ical.setVersion(ICalVersion.V2_0);
 		ical.setCalendarScale(CalendarScale.gregorian());
 		ical.setMethod(new Method("PUBLISH"));
 		ical.setExperimentalProperty("X-WR-CALNAME", "UniTime Schedule");
@@ -245,9 +245,9 @@ public class CalendarExport implements OnlineSectioningAction<String>{
     	cal.set(Calendar.SECOND, 0);
     	
     	VEvent vevent = new VEvent();
-    	DateStart dstart = new DateStart(first, true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+    	DateStart dstart = new DateStart(first, true);
     	vevent.setDateStart(dstart);
-    	DateEnd dend = new DateEnd(firstEnd, true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+    	DateEnd dend = new DateEnd(firstEnd, true);
     	vevent.setDateEnd(dend);
     	
     	Recurrence.Builder recur = new Recurrence.Builder(Frequency.WEEKLY);
@@ -272,7 +272,7 @@ public class CalendarExport implements OnlineSectioningAction<String>{
         recur.workweekStarts(DayOfWeek.MONDAY).until(last);
         vevent.setRecurrenceRule(recur.build());
 
-        ExceptionDates exdates = new ExceptionDates(true);
+        ExceptionDates exdates = new ExceptionDates();
     	while (idx < time.getWeeks().length()) {
     		int dow = cal.get(Calendar.DAY_OF_WEEK);
     		boolean offered = false;
@@ -339,12 +339,10 @@ public class CalendarExport implements OnlineSectioningAction<String>{
         if (section.getInstructors() != null && !section.getInstructors().isEmpty()) {
 			for (XInstructor instructor: section.getInstructors()) {
 				if (vevent.getOrganizer() == null) {
-					Organizer organizer = new Organizer("mailto:" + (instructor.getEmail() != null ? instructor.getEmail() : ""));
-					organizer.setCommonName(instructor.getName());
+					Organizer organizer = new Organizer(instructor.getName(), (instructor.getEmail() != null ? instructor.getEmail() : ""));
 					vevent.setOrganizer(organizer);
 				} else {
-					Attendee attendee = new Attendee("mailto:" + (instructor.getEmail() != null ? instructor.getEmail() : ""));
-					attendee.setCommonName(instructor.getName());
+					Attendee attendee = new Attendee(instructor.getName(), (instructor.getEmail() != null ? instructor.getEmail() : ""));
 					attendee.setRole(Role.CHAIR);
 					vevent.addAttendee(attendee);
 				}
@@ -435,10 +433,10 @@ public class CalendarExport implements OnlineSectioningAction<String>{
     	if (last == null) return;
     	
     	VFreeBusy vfree = new VFreeBusy();
-    	DateStart dstart = new DateStart(first, true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+    	DateStart dstart = new DateStart(first, true);
     	vfree.setDateStart(dstart);
     	Calendar c = Calendar.getInstance(Locale.US); c.setTime(first); c.add(Calendar.MINUTE, Constants.SLOT_LENGTH_MIN * len);
-    	DateEnd dend = new DateEnd(c.getTime(), true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+    	DateEnd dend = new DateEnd(c.getTime(), true);
     	vfree.setDateEnd(dend);
     	vfree.addComment("Free Time");
     	ical.addFreeBusy(vfree);
@@ -479,10 +477,10 @@ public class CalendarExport implements OnlineSectioningAction<String>{
         	    	cal.set(Calendar.SECOND, 0);
         	    	
         	    	vfree = new VFreeBusy();
-        	    	dstart = new DateStart(cal.getTime(), true); dstart.setLocalTime(false); dstart.setTimezoneId(TimeZone.getDefault().getID());
+        	    	dstart = new DateStart(cal.getTime(), true);
         	    	vfree.setDateStart(dstart);
         	    	cal.add(Calendar.MINUTE, Constants.SLOT_LENGTH_MIN * len);
-        	    	dend = new DateEnd(cal.getTime(), true); dend.setLocalTime(false); dend.setTimezoneId(TimeZone.getDefault().getID());
+        	    	dend = new DateEnd(cal.getTime(), true);
         	    	vfree.setDateEnd(dend);
         	    	vfree.addComment("Free Time");
         	    	ical.addFreeBusy(vfree);

commit 0e882cc4944bdd6305cafd5d2c05a063c7f873d7

iCalendar Exports
- ensure that the exported dates are in the local timezone
- this fixes the issue with daylight savings that was introduced in commit 592152d
  (Biweekly 0.4.3 defaults to UTC dates)

JavaSource/org/unitime/timetable/events/EventEmail.java
@@ -68,9 +68,9 @@ import org.unitime.timetable.security.rights.Right;
 import org.unitime.timetable.util.Constants;
 import org.unitime.timetable.util.NameFormat;
 
-import biweekly.Biweekly;
 import biweekly.ICalVersion;
 import biweekly.ICalendar;
+import biweekly.io.text.ICalWriter;
 import biweekly.property.CalendarScale;
 import biweekly.property.Method;
 import biweekly.property.Status;
@@ -340,8 +340,21 @@ public class EventEmail {
         		ical,
         		response().hasEventWithId() && response().getEvent().hasMeetings() ? response().getEvent() : request().getEvent(),
         		response().hasEventWithId() && response().getEvent().hasMeetings() ? null : Status.cancelled());
-
-		return (exp ? Biweekly.write(ical).go() : null);		
+        
+        if (exp) {
+        	StringWriter ret = new StringWriter();
+	        ICalWriter writer = new ICalWriter(ret, ICalVersion.V2_0);
+	    	writer.getTimezoneInfo().setDefaultTimeZone(TimeZone.getDefault());
+	        try {
+	        	writer.write(ical);
+	        	writer.flush();
+	        } finally {
+	        	writer.close();
+	        }
+	        return ret.toString();
+        } else {
+        	return null;
+        }
 	}
 	
 	public static void eventExpired(Event cancelledEvent, Set<Meeting> cancelledMeetings) throws Exception {

JavaSource/org/unitime/timetable/export/events/EventsExportEventsToICal.java
@@ -59,10 +59,10 @@ import org.unitime.timetable.model.dao.SubjectAreaDAO;
 import org.unitime.timetable.model.dao._RootDAO;
 import org.unitime.timetable.util.Constants;
 
-import biweekly.Biweekly;
 import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
+import biweekly.io.text.ICalWriter;
 import biweekly.parameter.Role;
 import biweekly.property.Attendee;
 import biweekly.property.CalendarScale;
@@ -104,7 +104,14 @@ public class EventsExportEventsToICal extends EventsExporter {
         for (EventInterface event: events)
 			print(ical, event);
 		
-        Biweekly.write(ical).go(helper.getWriter());
+        ICalWriter writer = new ICalWriter(helper.getWriter(), ICalVersion.V2_0);
+    	writer.getTimezoneInfo().setDefaultTimeZone(TimeZone.getDefault());
+        try {
+        	writer.write(ical);
+        	writer.flush();
+        } finally {
+        	writer.close();
+        }
 	}
 	
 	public boolean print(ICalendar ical, EventInterface event) throws IOException {

JavaSource/org/unitime/timetable/gwt/server/CalendarServlet.java
@@ -83,11 +83,11 @@ import org.unitime.timetable.solver.service.SolverServerService;
 import org.unitime.timetable.util.Constants;
 import org.unitime.timetable.util.DateUtils;
 
-import biweekly.Biweekly;
 import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import biweekly.component.VFreeBusy;
+import biweekly.io.text.ICalWriter;
 import biweekly.parameter.Role;
 import biweekly.property.Attendee;
 import biweekly.property.CalendarScale;
@@ -273,11 +273,15 @@ public class CalendarServlet extends HttpServlet {
         }
 		
 		PrintWriter out = response.getWriter();
+        ICalWriter writer = new ICalWriter(out, ICalVersion.V2_0);
 		try {
-            Biweekly.write(ical).go(out);
+	    	writer.getTimezoneInfo().setDefaultTimeZone(TimeZone.getDefault());
+        	writer.write(ical);
+        	writer.flush();
         	out.flush();
 		} finally {
 			out.close();
+			writer.close();
 		}
 	}
 

JavaSource/org/unitime/timetable/onlinesectioning/updates/CalendarExport.java
@@ -20,6 +20,7 @@
 package org.unitime.timetable.onlinesectioning.updates;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.BitSet;
 import java.util.Calendar;
@@ -47,11 +48,11 @@ import org.unitime.timetable.onlinesectioning.model.XTime;
 import org.unitime.timetable.server.CourseDetailsBackend;
 import org.unitime.timetable.util.Constants;
 
-import biweekly.Biweekly;
 import biweekly.ICalVersion;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
 import biweekly.component.VFreeBusy;
+import biweekly.io.text.ICalWriter;
 import biweekly.parameter.Role;
 import biweekly.property.Attendee;
 import biweekly.property.CalendarScale;
@@ -112,7 +113,16 @@ public class CalendarExport implements OnlineSectioningAction<String>{
         		}
         	}
 			
-			return Biweekly.write(ical).go();
+			StringWriter ret = new StringWriter();
+	        ICalWriter writer = new ICalWriter(ret, ICalVersion.V2_0);
+	    	writer.getTimezoneInfo().setDefaultTimeZone(TimeZone.getDefault());
+	        try {
+	        	writer.write(ical);
+	        	writer.flush();
+	        } finally {
+	        	writer.close();
+	        }
+	        return ret.toString();
 		} catch (IOException e) {
 			throw new SectioningException(e.getMessage(), e);
 		}
@@ -144,8 +154,18 @@ public class CalendarExport implements OnlineSectioningAction<String>{
 						DayCode.toString(ft.getTime().getDays()), ft.getTime().getSlot(), ft.getTime().getLength(), ical);
 			}
 		}
+		
+		StringWriter ret = new StringWriter();
+        ICalWriter writer = new ICalWriter(ret, ICalVersion.V2_0);
+    	writer.getTimezoneInfo().setDefaultTimeZone(TimeZone.getDefault());
+        try {
+        	writer.write(ical);
+        	writer.flush();
+        } finally {
+        	writer.close();
+        }
 
-		return Biweekly.write(ical).go();		
+        return ret.toString();		
 	}
 	
 	private static void printSection(OnlineSectioningServer server, XCourse course, XSection section, ICalendar ical) throws IOException {