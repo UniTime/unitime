UniTime/unitime

commit 811badede38165c4717a6377f69e92fb9fe0655a

Student Scheduling: Exclude Department from Student Scheduling
- there is a new ability to exclude some departments completely from student scheduling
- this is controlled by a new toggle that is present on each department (named Allow Student Scheduling on the Add/Edit Department page)
  - it defaults to true (student scheduling is enabled for the department)
- when a department is excluded, all courses controlled by this department are not even loaded into the solver (both batch and online)
  - students cannot see or take courses from the excluded department(s)
- instructional offerings that have all courses excluded from student scheduling do not need locking
- if all departments of a solver group are excluded from student scheduling, the commit restriction during online student scheduling phase does not apply to the solutions of such a solver group
- the online student scheduling server does not get automatically reloaded when the Allow Student Scheduling toggle is changed on a particular department during the online student scheduling phase
  - it has to be reloaded manually, using the Manage Solvers page

JavaSource/org/unitime/timetable/action/DepartmentListAction.java
@@ -95,12 +95,12 @@ public class DepartmentListAction extends Action {
 
         if ("Export PDF".equals(request.getParameter("op"))) {
             
-            PdfWebTable webTable = new PdfWebTable((dispLastChanges ? 12 : 11), "Department List - " + sessionContext.getUser().getCurrentAuthority().getQualifiers("Session").get(0).getQualifierLabel(),
+            PdfWebTable webTable = new PdfWebTable((dispLastChanges ? 13 : 12), "Department List - " + sessionContext.getUser().getCurrentAuthority().getQualifiers("Session").get(0).getQualifierLabel(),
                     "departmentList.do?ord=%%",
                     (dispLastChanges ? new String[] { "Number", "Abbv", "Name", "External\nManager", "Subjects", "Rooms",
-                            "Status", "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Last\nChange" } 
+                            "Status", "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling", "Last\nChange" } 
                     : new String[] { "Number", "Abbreviation", "Name", "External\nManager", "Subjects", "Rooms", "Status",
-                            "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events" }),
+                            "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling" }),
                     new String[] { "left", "left", "left", "left", "right", "right", "left", "right", "left", "left", "left", "left" },
                     new boolean[] { true, true, true, true, true, true, true, true, true, false });
             for (Iterator i=departmentListForm.getDepartments().iterator();i.hasNext();) {
@@ -148,6 +148,7 @@ public class DepartmentListAction extends Action {
                                 allowReq,
                                 d.isInheritInstructorPreferences() ? "Yes" : "No",
                                 d.isAllowEvents() ? "Yes" : "No",
+                                d.isAllowStudentScheduling() ? "Yes" : "No",
                                 lastChangeStr },
                            new Comparable[] {
                             d.getDeptCode(),
@@ -161,6 +162,7 @@ public class DepartmentListAction extends Action {
                             new Integer(allowReqOrd),
                             d.isInheritInstructorPreferences(),
                             d.isAllowEvents(),
+                            d.isAllowStudentScheduling(),
                             lastChangeCmp });
                 }
             }
@@ -172,15 +174,15 @@ public class DepartmentListAction extends Action {
             return null;
         }
         
-		WebTable webTable = new WebTable((dispLastChanges ? 12 : 11), "",
+		WebTable webTable = new WebTable((dispLastChanges ? 13 : 12), "",
 				"departmentList.do?ord=%%",
 				(dispLastChanges 
 					? new String[] { "Code", "Abbv", "Name", "External<br>Manager", 
 									 "Subjects", "Rooms", "Status", "Dist&nbsp;Pref<br>Priority", 
-									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Last<br>Change" } 
+									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Student<br>Scheduling", "Last<br>Change" } 
 					: new String[] { "Code", "Abbreviation", "Name", "External Manager",
 									 "Subjects", "Rooms", "Status", "Dist&nbsp;Pref<br>Priority", 
-									 "Allow<br>Required", "Instructor<br>Preferences", "Events" }),
+									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Student<br>Scheduling" }),
 				new String[] { "left", "left", "left", "left", "right",	"right", "left", "right", "left", "left", "left", "left" },
 				new boolean[] { true, true, true, true, true, true, true, true, true, true, true, false });
 		WebTable.setOrder(sessionContext, "DepartmentList.ord", request.getParameter("ord"), 1);
@@ -256,6 +258,7 @@ public class DepartmentListAction extends Action {
     						allowReq,
     						(d.isInheritInstructorPreferences() ? "<IMG border='0' title='Instructor preferences are to be inherited.' alt='Inherit Instructor Preferences' align='absmiddle' src='images/accept.png'>" : ""),
     						(d.isAllowEvents() ? "<IMG border='0' title='This department has event management enabled.' alt='Event Management' align='absmiddle' src='images/accept.png'>" : ""),
+    						(d.isAllowStudentScheduling() ? "<IMG border='0' title='This department has student scheduling enabled.' alt='Student Scheduling' align='absmiddle' src='images/accept.png'>" : ""),
     						lastChangeStr },
     				new Comparable[] {
     						d.getDeptCode(),
@@ -272,6 +275,7 @@ public class DepartmentListAction extends Action {
     						new Integer(allowReqOrd),
     						d.isInheritInstructorPreferences(),
     						d.isAllowEvents(),
+    						d.isAllowStudentScheduling(),
     						lastChangeCmp });
     		}
         }

Documentation/Database/MySQL/Changes/155 Department Student Scheduling.sql
@@ -1,0 +1,29 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+alter table department add allow_student_schd int(1) null default 1;
+
+/*
+ * Update database version
+ */
+
+update application_config set value='155' where name='tmtbl.db.version';
+
+commit;

Documentation/Database/Oracle/Changes/155 Department Student Scheduling.sql
@@ -1,0 +1,29 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+alter table department add allow_student_schd number(1) default 1;
+
+/*
+ * Update database version
+ */
+
+update application_config set value='155' where name='tmtbl.db.version';
+
+commit;

JavaSource/PreferenceGroup.hbm.xml
@@ -486,6 +486,12 @@
                 not-null="true"/>
 
             <property
+                name="allowStudentScheduling"
+                column="allow_student_schd"
+                type="java.lang.Boolean"
+                not-null="true"/>
+
+            <property
                 name="inheritInstructorPreferences"
                 column="instructor_pref"
                 type="java.lang.Boolean"

JavaSource/dbupdate.xml
@@ -10677,4 +10677,8 @@
 				uniqueid as solver_param_group_id from %SCHEMA%.solver_parameter_group where name = 'StudentSctOnline')
 		</plsql>
 	</update>
+	<update version="155" date="12-Dec-2015" comment="Department allow student scheduling toggle">
+		<mysql>alter table %SCHEMA%.department add allow_student_schd int(1) null default 1</mysql>
+		<plsql>alter table %SCHEMA%.department add allow_student_schd number(1) default 1</plsql>
+	</update>
 </dbupdate>

JavaSource/org/unitime/timetable/action/DepartmentEditAction.java
@@ -81,6 +81,8 @@ public class DepartmentEditAction extends Action {
 	        if(rsc.getMessage("button.addDepartment").equalsIgnoreCase(op)) {
             	myForm.reset(mapping, request);
             	myForm.setSessionId(sessionContext.getUser().getCurrentAcademicSessionId());
+            	myForm.setInheritInstructorPreferences(true);
+            	myForm.setAllowStudentScheduling(true);
         		sessionContext.checkPermission(Right.DepartmentAdd);
             	return mapping.findForward("add");
 	        }

JavaSource/org/unitime/timetable/dataexchange/DepartmentImport.java
@@ -78,6 +78,7 @@ public class DepartmentImport extends BaseImport {
                     department.setAllowReqDistribution(false);
                     department.setExternalManager(false);
                     department.setAllowEvents(false);
+                    department.setAllowStudentScheduling(true);
                     department.setInheritInstructorPreferences(true);
                     department.setDistributionPrefPriority(new Integer(0));
                 } else {

JavaSource/org/unitime/timetable/form/DepartmentEditForm.java
@@ -58,6 +58,7 @@ public class DepartmentEditForm extends ActionForm {
     public boolean iAllowReqDist = false;
     public boolean iAllowEvents = false;
     public boolean iInheritInstructorPreferences = false;
+    public boolean iAllowStudentScheduling = false;
 	
 	public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
 		ActionErrors errors = new ActionErrors();
@@ -127,7 +128,7 @@ public class DepartmentEditForm extends ActionForm {
 		iId = null; iSessionId = null; iName = null; iDeptCode = null; iStatusType = null; iAbbv=null; iDistPrefPriority = 0;
 		iIsExternal = false; iExtName = null; iExtAbbv = null;
         iAllowReqTime = false; iAllowReqRoom = false; iAllowReqDist = false; iAllowEvents = false;
-        iInheritInstructorPreferences = false;
+        iInheritInstructorPreferences = false; iAllowStudentScheduling = false;
 	}
 
 	public Long getId() { return iId; }
@@ -173,6 +174,8 @@ public class DepartmentEditForm extends ActionForm {
 	public void setExtAbbv(String extAbbv) { iExtAbbv = extAbbv; }
 	public String getExtName() { return iExtName; }
 	public void setExtName(String extName) { iExtName = extName; }
+    public boolean getAllowStudentScheduling() { return iAllowStudentScheduling; }
+    public void setAllowStudentScheduling(boolean allowStudentScheduling) { iAllowStudentScheduling = allowStudentScheduling; }
 	
 	public ReferenceList getStatusOptions() { 
 		ReferenceList ref = new ReferenceList();
@@ -196,6 +199,7 @@ public class DepartmentEditForm extends ActionForm {
         setAllowReqTime(department.isAllowReqTime()!=null && department.isAllowReqTime().booleanValue());
         setAllowReqDist(department.isAllowReqDistribution()!=null && department.isAllowReqDistribution().booleanValue());
         setAllowEvents(department.isAllowEvents());
+        setAllowStudentScheduling(department.isAllowStudentScheduling());
         setInheritInstructorPreferences(department.isInheritInstructorPreferences());
 	}
 
@@ -229,6 +233,7 @@ public class DepartmentEditForm extends ActionForm {
             department.setAllowReqTime(new Boolean(getAllowReqTime()));
             department.setAllowReqDistribution(new Boolean(getAllowReqDist()));
             department.setAllowEvents(getAllowEvents());
+            department.setAllowStudentScheduling(getAllowStudentScheduling());
             department.setInheritInstructorPreferences(getInheritInstructorPreferences());
 
 			dao.saveOrUpdate(department);

commit 6ee1192b478940d5b82b7205d7954838cdb4a5f3

Departments: Allow Student Scheduling
- fixed an array index out of bounds exception thrown when last changes are to be displayed
  - this fixes a problem introduced in commit 811bade

JavaSource/org/unitime/timetable/action/DepartmentListAction.java
@@ -101,8 +101,8 @@ public class DepartmentListAction extends Action {
                             "Status", "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling", "Last\nChange" } 
                     : new String[] { "Number", "Abbreviation", "Name", "External\nManager", "Subjects", "Rooms", "Status",
                             "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling" }),
-                    new String[] { "left", "left", "left", "left", "right", "right", "left", "right", "left", "left", "left", "left" },
-                    new boolean[] { true, true, true, true, true, true, true, true, true, false });
+                    new String[] { "left", "left", "left", "left", "right", "right", "left", "right", "left", "left", "left", "left", "left" },
+                    new boolean[] { true, true, true, true, true, true, true, true, true, true, true, (dispLastChanges ? true: false), false });
             for (Iterator i=departmentListForm.getDepartments().iterator();i.hasNext();) {
                 Department d = (Department) i.next();
                 if (departmentListForm.getShowUnusedDepts() || !d.getSubjectAreas().isEmpty() || !d.getTimetableManagers().isEmpty() || d.isExternalManager().booleanValue()) {
@@ -183,8 +183,8 @@ public class DepartmentListAction extends Action {
 					: new String[] { "Code", "Abbreviation", "Name", "External Manager",
 									 "Subjects", "Rooms", "Status", "Dist&nbsp;Pref<br>Priority", 
 									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Student<br>Scheduling" }),
-				new String[] { "left", "left", "left", "left", "right",	"right", "left", "right", "left", "left", "left", "left" },
-				new boolean[] { true, true, true, true, true, true, true, true, true, true, true, false });
+				new String[] { "left", "left", "left", "left", "right", "right", "left", "right", "left", "left", "left", "left", "left" },
+                new boolean[] { true, true, true, true, true, true, true, true, true, true, true, (dispLastChanges ? true: false), false });
 		WebTable.setOrder(sessionContext, "DepartmentList.ord", request.getParameter("ord"), 1);
         webTable.enableHR("#9CB0CE");
         webTable.setRowStyle("white-space: nowrap");