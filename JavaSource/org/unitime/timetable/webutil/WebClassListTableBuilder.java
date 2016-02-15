UniTime/unitime

commit 266fa9f93952013b676054c99ef2ee0a6d20a8d7

Class Conflicts: a number of improvements
Instructional Offering Detail page
 - show a red warning message if there is a class (that is not cancelled) with a conflict
   One or more classes of this instructional offering are in a conflict.
 - show a warning icon next to each class with a conflict
 - include event conflicts in the checking (not only class conflicts)

Scheduling Subpart Detail page
 - highlight conflicting classes with a lightly red background and the warning icon (same as on the Instructional Offering Detail page)

Class Detail page
 - if there are event conflicts, list them in the (new) Conflicting Meetings table

JavaSource/org/unitime/timetable/webutil/WebClassListTableBuilder.java
@@ -144,15 +144,19 @@ public class WebClassListTableBuilder extends
 				}
 		        table = this.initTable(outputStream, context.getUser().getCurrentAcademicSessionId());
 		    }		        
-            this.buildClassRow(classAssignment,examAssignment, ++ct, table, co, c, "", context, prevLabel);
+            this.buildClassRow(classAssignment,examAssignment, ++ct, table, co, c, 0, context, prevLabel);
             prevLabel = c.getClassLabel(co);
         }  
         table.tableComplete();
     }
 	
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, String indentSpaces, boolean isEditable, String prevLabel){
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel){
     	if (prefGroup instanceof Class_) {
-    		TableCell cell = initNormalCell(indentSpaces, isEditable);
+    		TableCell cell = initNormalCell("", isEditable);
+        	if (indentSpaces > 0) {
+        		int pad = indentSpaces * indent;
+        		cell.setStyle("padding-left: " + pad + "px;");
+        	}
     		Class_ aClass = (Class_) prefGroup;
 	    	if(!isEditable){
 	    		cell.addContent("<font color='"+disabledColor+"'>");
@@ -182,8 +186,8 @@ public class WebClassListTableBuilder extends
         		cell.addContent(" (<span title='" + im.getLabel() + "'>" + im.getReference() + ")");
 	        return(cell);
         } else {
-        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, null));
-        }     
+        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, null, null));
+        }
     }
 	
     public void htmlTableForClasses(ClassAssignmentProxy classAssignment, ExamAssignmentProxy examAssignment, CourseOffering co, TreeSet classes, Long subjectAreaId, SessionContext context, JspWriter outputStream){
@@ -242,7 +246,7 @@ public class WebClassListTableBuilder extends
         int ct = 0;
         while (it.hasNext()){
             cls = (Class_) it.next();
-            this.buildClassRow(classAssignment, examAssignment, ++ct, table, co, cls, "", context, prevLabel);
+            this.buildClassRow(classAssignment, examAssignment, ++ct, table, co, cls, 0, context, prevLabel);
             prevLabel = cls.getClassLabel(co);
         }     
         table.tableComplete();

JavaSource/org/unitime/timetable/webutil/WebInstrOfferingConfigTableBuilder.java
@@ -277,8 +277,8 @@ public class WebInstrOfferingConfigTableBuilder extends
        	}
     }
     
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, String indentSpaces, boolean isEditable, String prevLabel){
-    	TableCell cell = super.buildPrefGroupLabel(co, prefGroup, indentSpaces, isEditable, prevLabel);
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel){
+    	TableCell cell = super.buildPrefGroupLabel(co, prefGroup, indentSpaces, isEditable, prevLabel, null);
     	if ("PreferenceGroup".equals(getBackType()) && prefGroup.getUniqueId().toString().equals(getBackId()))
     		cell.addContent("<A name=\"back\"></A>");
     	return cell;

JavaSource/org/unitime/timetable/webutil/WebSubpartClassListTableBuilder.java
@@ -52,9 +52,13 @@ public class WebSubpartClassListTableBuilder extends WebClassListTableBuilder {
 		return(sb.toString());
 	}
 
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, String indentSpaces, boolean isEditable){
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable){
     	if (prefGroup instanceof Class_) {
-    		TableCell cell = initNormalCell(indentSpaces, isEditable);
+    		TableCell cell = initNormalCell("", isEditable);
+    		if (indentSpaces > 0) {
+        		int pad = indentSpaces * indent;
+        		cell.setStyle("padding-left: " + pad + "px;");
+        	}
         	if ("PreferenceGroup".equals(getBackType()) && prefGroup.getUniqueId().toString().equals(getBackId()))
         		cell.addContent("<A name=\"back\"></A>");
     		Class_ aClass = (Class_) prefGroup;

JavaSource/org/unitime/localization/messages/CourseMessages.java
@@ -634,6 +634,18 @@ public interface CourseMessages extends Messages {
 	@DefaultMessage("Credit")
 	String columnCredit();
 	
+	@DefaultMessage("Name")
+	String columnEventName();
+	
+	@DefaultMessage("Type")
+	String columnEventType();
+	
+	@DefaultMessage("Date")
+	String columnEventDate();
+	
+	@DefaultMessage("Time")
+	String columnEventTime();
+	
 	@DefaultMessage("Total")
 	String rowCrossListsTotal();
 	
@@ -1932,6 +1944,9 @@ public interface CourseMessages extends Messages {
 	@DefaultMessage("Conflicting Classes")
 	String sectionTitleClassConflicts();
 	
+	@DefaultMessage("Conflicting Meetings")
+	String sectionTitleEventConflicts();
+
 	@DefaultMessage("Instructor Displayed")
 	String titleInstructorDisplayed();
 	
@@ -2126,4 +2141,7 @@ public interface CourseMessages extends Messages {
 	
 	@DefaultMessage("No matching date pattern!")
 	String warnNoMatchingDatePattern();
+	
+	@DefaultMessage("One or more classes of this instructional offering are in a conflict.")
+	String warnOfferingHasConflictingClasses();
 }

JavaSource/org/unitime/timetable/action/ClassDetailAction.java
@@ -50,6 +50,8 @@ import org.unitime.timetable.defaults.CommonValues;
 import org.unitime.timetable.defaults.UserProperty;
 import org.unitime.timetable.form.ClassEditForm;
 import org.unitime.timetable.gwt.resources.GwtConstants;
+import org.unitime.timetable.interfaces.RoomAvailabilityInterface;
+import org.unitime.timetable.interfaces.RoomAvailabilityInterface.TimeBlock;
 import org.unitime.timetable.model.Assignment;
 import org.unitime.timetable.model.ClassInstructor;
 import org.unitime.timetable.model.Class_;
@@ -60,6 +62,7 @@ import org.unitime.timetable.model.DistributionPref;
 import org.unitime.timetable.model.Location;
 import org.unitime.timetable.model.PreferenceLevel;
 import org.unitime.timetable.model.SchedulingSubpart;
+import org.unitime.timetable.model.Session;
 import org.unitime.timetable.model.StudentAccomodation;
 import org.unitime.timetable.model.TimePattern;
 import org.unitime.timetable.model.comparators.ClassComparator;
@@ -73,8 +76,11 @@ import org.unitime.timetable.solver.TimetableDatabaseLoader;
 import org.unitime.timetable.solver.service.AssignmentService;
 import org.unitime.timetable.solver.service.SolverService;
 import org.unitime.timetable.util.Constants;
+import org.unitime.timetable.util.DefaultRoomAvailabilityService;
 import org.unitime.timetable.util.Formats;
 import org.unitime.timetable.util.LookupTables;
+import org.unitime.timetable.util.RoomAvailability;
+import org.unitime.timetable.util.DefaultRoomAvailabilityService.MeetingTimeBlock;
 import org.unitime.timetable.webutil.BackTracker;
 import org.unitime.timetable.webutil.DistributionPrefsTableBuilder;
 
@@ -235,8 +241,15 @@ public class ClassDetailAction extends PreferencesAction {
 	        LookupTables.setupRoomGroups(request, c);   // Room Groups
 	        
 	        try {
-	        	ClassAssignmentProxy proxy = classAssignmentService.getAssignment();
-	        	Set<Assignment> conflicts = (proxy == null ? null : proxy.getConflicts(c));
+	            if (RoomAvailability.getInstance()!=null && !(RoomAvailability.getInstance() instanceof DefaultRoomAvailabilityService)) {
+	                Session session = c.getManagingDept().getSession();
+	                Date[] bounds = DatePattern.getBounds(session.getUniqueId());
+	                RoomAvailability.getInstance().activate(session,bounds[0],bounds[1],RoomAvailabilityInterface.sClassType, false);
+	                RoomAvailability.setAvailabilityWarning(request, session, true, true);
+	            }
+
+	            ClassAssignmentProxy proxy = classAssignmentService.getAssignment();
+	        	Set<Assignment> conflicts = (proxy == null ? null : proxy.getConflicts(c.getUniqueId()));
 	        	if (conflicts != null && !conflicts.isEmpty()) {
 	        		TreeSet<Assignment> orderedConflicts = new TreeSet<Assignment>(new Comparator<Assignment>() {
 	        			ClassComparator cc = new ClassComparator(ClassComparator.COMPARE_BY_HIERARCHY);
@@ -312,6 +325,51 @@ public class ClassDetailAction extends PreferencesAction {
 								});
 	        		}
 	        		request.setAttribute("CLASS_CONFLICTS", table.printTable());
+	        	}
+	        	
+	        	Set<TimeBlock> ec = (proxy == null ? null : proxy.getConflictingTimeBlocks(c.getUniqueId()));
+	        	if (ec != null && !ec.isEmpty()) {
+	        		WebTable table = new WebTable(4, MSG.sectionTitleEventConflicts(), new String[] {
+        					MSG.columnEventName(),
+        					MSG.columnEventType(),
+        					MSG.columnEventDate(),
+        					MSG.columnEventTime()
+	        			}, new String[] {
+	        				"left",
+	        				"left",
+	        				"left",
+	        				"left"
+	        			}, new boolean[] {
+	        				true,
+	        				true,
+	        				true,
+	        				true
+	        		});
+	        		table.setBlankWhenSame(true);
+	        		Formats.Format<Date> dateFormat = Formats.getDateFormat(Formats.Pattern.DATE_MEETING);
+	        		Formats.Format<Date> timeFormat = Formats.getDateFormat(Formats.Pattern.TIME_SHORT);
+	        		for (TimeBlock block: ec) {
+	        			String link = null;
+	        			if (block instanceof MeetingTimeBlock) {
+	        				MeetingTimeBlock mtb = (MeetingTimeBlock)block;
+	        				if (mtb.getEventId() != null && sessionContext.hasPermission(mtb.getEventId(), Right.EventDetail))
+	        					link = "onClick=\"showGwtDialog('Event Detail', 'gwt.jsp?page=events&menu=hide#event=" + mtb.getEventId() + "','900','85%');\"";
+	        			}
+	        			table.addLine(
+	        					link,
+	        					new String[] {
+	        							block.getEventName(),
+	        							block.getEventType(),
+	        							dateFormat.format(block.getStartTime()),
+	        							timeFormat.format(block.getStartTime()) + " - " + timeFormat.format(block.getEndTime())
+	        					}, new Comparable[] {
+	        							block.getEventName(),
+	        							block.getEventType(),
+	        							block.getStartTime(),
+	        							block.getEndTime()
+								});
+	        		}
+	        		request.setAttribute("EVENT_CONFLICTS", table.printTable());
 	        	}
 	        } catch (Exception e) {
 	        	Debug.error("Failed to compute conflicts: " + e.getMessage(), e);

JavaSource/org/unitime/timetable/action/InstructionalOfferingDetailAction.java
@@ -21,6 +21,7 @@ package org.unitime.timetable.action;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeSet;
@@ -48,9 +49,11 @@ import org.unitime.timetable.interfaces.ExternalInstructionalOfferingDeleteActio
 import org.unitime.timetable.interfaces.ExternalInstructionalOfferingNotOfferedAction;
 import org.unitime.timetable.interfaces.ExternalInstructionalOfferingOfferedAction;
 import org.unitime.timetable.interfaces.ExternalLinkLookup;
+import org.unitime.timetable.interfaces.RoomAvailabilityInterface;
 import org.unitime.timetable.model.ChangeLog;
 import org.unitime.timetable.model.Class_;
 import org.unitime.timetable.model.CourseOffering;
+import org.unitime.timetable.model.DatePattern;
 import org.unitime.timetable.model.DepartmentalInstructor;
 import org.unitime.timetable.model.DistributionPref;
 import org.unitime.timetable.model.Event;
@@ -59,12 +62,17 @@ import org.unitime.timetable.model.InstrOfferingConfig;
 import org.unitime.timetable.model.InstructionalOffering;
 import org.unitime.timetable.model.Reservation;
 import org.unitime.timetable.model.SchedulingSubpart;
+import org.unitime.timetable.model.Session;
 import org.unitime.timetable.model.StudentAccomodation;
 import org.unitime.timetable.model.comparators.CourseOfferingComparator;
 import org.unitime.timetable.model.comparators.InstrOfferingConfigComparator;
 import org.unitime.timetable.model.dao.InstructionalOfferingDAO;
 import org.unitime.timetable.security.SessionContext;
 import org.unitime.timetable.security.rights.Right;
+import org.unitime.timetable.solver.ClassAssignmentProxy;
+import org.unitime.timetable.solver.service.AssignmentService;
+import org.unitime.timetable.util.DefaultRoomAvailabilityService;
+import org.unitime.timetable.util.RoomAvailability;
 import org.unitime.timetable.webutil.BackTracker;
 import org.unitime.timetable.webutil.DistributionPrefsTableBuilder;
 
@@ -83,6 +91,8 @@ public class InstructionalOfferingDetailAction extends Action {
 	protected final static CourseMessages MSG = Localization.create(CourseMessages.class);
 	
 	@Autowired SessionContext sessionContext;
+	
+	@Autowired AssignmentService<ClassAssignmentProxy> classAssignmentService;
 
     // --------------------------------------------------------- Instance Variables
 
@@ -318,6 +328,22 @@ public class InstructionalOfferingDetailAction extends Action {
             throw (e);
         }
 	}
+	
+	private boolean hasConflicts(HttpServletRequest request, InstructionalOffering io) {
+        if (RoomAvailability.getInstance()!=null && !(RoomAvailability.getInstance() instanceof DefaultRoomAvailabilityService)) {
+            Session session = io.getSession();
+            Date[] bounds = DatePattern.getBounds(session.getUniqueId());
+            RoomAvailability.getInstance().activate(session,bounds[0],bounds[1],RoomAvailabilityInterface.sClassType, false);
+            RoomAvailability.setAvailabilityWarning(request, session, true, true);
+        }
+
+        ClassAssignmentProxy proxy = classAssignmentService.getAssignment();
+		try {
+			if (proxy != null) return proxy.hasConflicts(io.getUniqueId());
+		} catch (Exception e) {}
+		
+		return false;
+	}
 
 	/**
      * Loads the form initially
@@ -366,6 +392,7 @@ public class InstructionalOfferingDetailAction extends Action {
         frm.setWkChange(io.getLastWeekToChange() == null ? "" : io.getLastWeekToChange().toString());
         frm.setWkDrop(io.getLastWeekToDrop() == null ? "" : io.getLastWeekToDrop().toString());
         frm.setWeekStartDayOfWeek(Localization.getDateFormat("EEEE").format(io.getSession().getSessionBeginDateTime()));
+        frm.setHasConflict(hasConflicts(request, io));
         String coordinators = "";
         String instructorNameFormat = sessionContext.getUser().getProperty(UserProperty.NameFormat);
         for (DepartmentalInstructor instructor: new TreeSet<DepartmentalInstructor>(io.getCoordinators())) {
@@ -448,8 +475,7 @@ public class InstructionalOfferingDetailAction extends Action {
 		DistributionPrefsTableBuilder tbl = new DistributionPrefsTableBuilder();
         String html = tbl.getDistPrefsTableForInstructionalOffering(request, sessionContext, io);
         if (html!=null && html.indexOf(MSG.noPreferencesFound())<0)
-        	request.setAttribute(DistributionPref.DIST_PREF_REQUEST_ATTR, html);	    
-        
+        	request.setAttribute(DistributionPref.DIST_PREF_REQUEST_ATTR, html);
     }
 
     /**

JavaSource/org/unitime/timetable/form/InstructionalOfferingDetailForm.java
@@ -79,6 +79,7 @@ public class InstructionalOfferingDetailForm extends ActionForm {
     private String wkEnroll, wkChange, wkDrop;
     private String weekStartDayOfWeek;
     private String accommodation;
+    private boolean hasConflict;
 
 
     // --------------------------------------------------------- Classes
@@ -136,6 +137,7 @@ public class InstructionalOfferingDetailForm extends ActionForm {
         wkEnroll = null; wkChange = null; wkDrop = null;
         weekStartDayOfWeek = null;
         accommodation = null;
+        hasConflict = false;
     }
     
     public List getCourseOfferings() {
@@ -359,5 +361,8 @@ public class InstructionalOfferingDetailForm extends ActionForm {
     		if (((CourseOffering)i.next()).getReservation() != null) return true;
     	return false;
     }
+
+	public boolean isHasConflict() { return hasConflict; }
+	public void setHasConflict(boolean hasConflict) { this.hasConflict = hasConflict; }
 
 }

JavaSource/org/unitime/timetable/interfaces/RoomAvailabilityInterface.java
@@ -21,6 +21,7 @@ package org.unitime.timetable.interfaces;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
 
 import org.unitime.timetable.defaults.ApplicationProperty;
@@ -47,5 +48,17 @@ public interface RoomAvailabilityInterface {
         public String getEventType();
         public Date getStartTime();
         public Date getEndTime();
+    }
+    
+    public static class TimeBlockComparator implements Comparator<TimeBlock> {
+    	public int compare(TimeBlock t1, TimeBlock t2) {
+    		int cmp = t1.getEventName().compareToIgnoreCase(t2.getEventName());
+    		if (cmp != 0) return cmp;
+    		cmp = t1.getEventType().compareToIgnoreCase(t2.getEventType());
+    		if (cmp != 0) return cmp;
+    		cmp = t1.getStartTime().compareTo(t2.getStartTime());
+    		if (cmp != 0) return cmp;
+    		return t1.getEndTime().compareTo(t2.getEndTime());
+    	}
     }
 }

commit 2908c711ea5608e6e7e3f183a374d3470a52fbca

Classes, Class Assignment: Class Name Bugfix
- class name corrected to include subject area and course number (not just itype and class number, like, Lec 1)
 - the issue was introduced in commit 266fa9f

JavaSource/org/unitime/timetable/webutil/WebClassListTableBuilder.java
@@ -150,13 +150,16 @@ public class WebClassListTableBuilder extends
         table.tableComplete();
     }
 	
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel){
+	@Override
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel, String icon){
     	if (prefGroup instanceof Class_) {
     		TableCell cell = initNormalCell("", isEditable);
         	if (indentSpaces > 0) {
         		int pad = indentSpaces * indent;
+        		if (icon != null) pad -= indent;
         		cell.setStyle("padding-left: " + pad + "px;");
         	}
+            if (icon != null) cell.addContent(icon);
     		Class_ aClass = (Class_) prefGroup;
 	    	if(!isEditable){
 	    		cell.addContent("<font color='"+disabledColor+"'>");
@@ -186,7 +189,7 @@ public class WebClassListTableBuilder extends
         		cell.addContent(" (<span title='" + im.getLabel() + "'>" + im.getReference() + ")");
 	        return(cell);
         } else {
-        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, null, null));
+        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, prevLabel, icon));
         }
     }
 	

JavaSource/org/unitime/timetable/webutil/WebInstrOfferingConfigTableBuilder.java
@@ -277,8 +277,9 @@ public class WebInstrOfferingConfigTableBuilder extends
        	}
     }
     
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel){
-    	TableCell cell = super.buildPrefGroupLabel(co, prefGroup, indentSpaces, isEditable, prevLabel, null);
+    @Override
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel, String icon){
+    	TableCell cell = super.buildPrefGroupLabel(co, prefGroup, indentSpaces, isEditable, prevLabel, icon);
     	if ("PreferenceGroup".equals(getBackType()) && prefGroup.getUniqueId().toString().equals(getBackId()))
     		cell.addContent("<A name=\"back\"></A>");
     	return cell;

JavaSource/org/unitime/timetable/webutil/WebSubpartClassListTableBuilder.java
@@ -52,13 +52,16 @@ public class WebSubpartClassListTableBuilder extends WebClassListTableBuilder {
 		return(sb.toString());
 	}
 
-    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable){
+	@Override
+    protected TableCell buildPrefGroupLabel(CourseOffering co, PreferenceGroup prefGroup, int indentSpaces, boolean isEditable, String prevLabel, String icon){
     	if (prefGroup instanceof Class_) {
     		TableCell cell = initNormalCell("", isEditable);
-    		if (indentSpaces > 0) {
+        	if (indentSpaces > 0) {
         		int pad = indentSpaces * indent;
+        		if (icon != null) pad -= indent;
         		cell.setStyle("padding-left: " + pad + "px;");
         	}
+            if (icon != null) cell.addContent(icon);
         	if ("PreferenceGroup".equals(getBackType()) && prefGroup.getUniqueId().toString().equals(getBackId()))
         		cell.addContent("<A name=\"back\"></A>");
     		Class_ aClass = (Class_) prefGroup;
@@ -84,7 +87,7 @@ public class WebSubpartClassListTableBuilder extends WebClassListTableBuilder {
 	        }
 	        return(cell);
         } else {
-        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, null));
+        	return(super.buildPrefGroupLabel(co, prefGroup,indentSpaces, isEditable, prevLabel, icon));
         }
     	
         