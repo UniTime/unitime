UniTime/unitime

commit bc910d85e112fa0c6fac5f485f338be58117ea6b

Student Scheduling  - added ability to override the following reservation properties (for any reservation)    - reservation priority, must be used, can assign over limit, allow overlap (time conflicts)  - see the unitime.reservation.* application properties    - for instance set unitime.reservation.canOverLimit.group to true to allow students meeting a student group reservation to be enrolled over the course / configuration / class limits

JavaSource/org/unitime/timetable/onlinesectioning/model/XReservation.java
@@ -31,10 +31,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
-
 import org.cpsolver.studentsct.model.Config;
 import org.cpsolver.studentsct.model.Section;
 import org.cpsolver.studentsct.model.Subpart;
+import org.unitime.timetable.defaults.ApplicationProperty;
 import org.unitime.timetable.model.Class_;
 import org.unitime.timetable.model.InstrOfferingConfig;
 import org.unitime.timetable.model.Reservation;
@@ -50,6 +50,26 @@ public abstract class XReservation extends XReservationId implements Comparable<
     private Map<Long, Set<Long>> iSections = new HashMap<Long, Set<Long>>();
     private int iLimitCap = -1;
     private double iRestrictivity = 1.0;
+    
+    private int iPriority = 1000;
+    private int iFlags = 0;
+    
+    public static enum Flags {
+    	MustBeUsed,
+    	CanAssignOverLimit,
+    	AllowOverlap,
+    	;
+    	public int flag() { return 1 << ordinal(); }
+		public boolean in(int flags) {
+			return (flags & flag()) != 0;
+		}
+		public int set(int flags, boolean value) {
+			if (value)
+				return flags | flag();
+			else
+				return flags & ~flag();
+		}
+    }
     
     public XReservation() {
     	super();
@@ -97,6 +117,38 @@ public abstract class XReservation extends XReservationId implements Comparable<
         	iLimitCap = cap;
         }
         iRestrictivity = computeRestrictivity(offering);
+        switch (type) {
+        case Individual:
+        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	break;
+        case Group:
+        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	break;
+        case Curriculum:
+        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	break;
+        case Course:
+        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	break;
+        case Override:
+        	setPriority(ApplicationProperty.ReservationPriorityOverride.intValue());
+        	break;
+        case Dummy:
+        	setPriority(ApplicationProperty.ReservationPriorityDummy.intValue());
+        	break;
+        }
     }
     
     public XReservation(XReservationType type, org.cpsolver.studentsct.reservation.Reservation reservation) {
@@ -112,6 +164,10 @@ public abstract class XReservation extends XReservationId implements Comparable<
     			sections.add(section.getId());
     		iSections.put(entry.getKey().getId(), sections);
     	}
+    	setPriority(reservation.getPriority());
+    	setMustBeUsed(reservation.mustBeUsed());
+    	setCanAssignOverLimit(reservation.canAssignOverLimit());
+    	setAllowOverlap(reservation.isAllowOverlap());
     }
     
     /**
@@ -122,7 +178,11 @@ public abstract class XReservation extends XReservationId implements Comparable<
     
     /** Reservation priority (e.g., individual reservations first) */
     public int getPriority() {
-    	return getType().getPriority();
+    	return iPriority;
+    }
+    
+    public void setPriority(int priority) {
+    	iPriority = priority;
     }
     
     /**
@@ -152,12 +212,16 @@ public abstract class XReservation extends XReservationId implements Comparable<
     /**
      * True if can go over the course / config / section limit. Only to be used in the online sectioning. 
       */
-    public abstract boolean canAssignOverLimit();
+    public boolean canAssignOverLimit() { return Flags.CanAssignOverLimit.in(iFlags); }
+    
+    public void setCanAssignOverLimit(boolean canAssignOverLimit) { iFlags = Flags.CanAssignOverLimit.set(iFlags, canAssignOverLimit); }
     
     /**
      * If true, student must use the reservation (if applicable)
      */
-    public abstract boolean mustBeUsed();
+    public boolean mustBeUsed() { return Flags.MustBeUsed.in(iFlags); }
+    
+    public void setMustBeUsed(boolean mustBeUsed) { iFlags = Flags.MustBeUsed.set(iFlags, mustBeUsed); }
     
     /**
      * Return minimum of two limits where -1 counts as unlimited (any limit is smaller)
@@ -183,9 +247,9 @@ public abstract class XReservation extends XReservationId implements Comparable<
     /**
      * True if holding this reservation allows a student to have attend overlapping class. 
      */
-    public boolean isAllowOverlap() {
-        return false;
-    }
+    public boolean isAllowOverlap() { return Flags.AllowOverlap.in(iFlags); }
+    
+    public void setAllowOverlap(boolean allowOverlap) { iFlags = Flags.AllowOverlap.set(iFlags, allowOverlap); }
     
     /**
      * True if the reservation is expired. If a reservation is expired, it works as ordinary reservation
@@ -312,6 +376,8 @@ public abstract class XReservation extends XReservationId implements Comparable<
     	}
     	iLimitCap = in.readInt();
     	iRestrictivity = in.readDouble();
+    	iPriority = in.readInt();
+    	iFlags = in.readInt();
 	}
 
 	@Override
@@ -336,5 +402,7 @@ public abstract class XReservation extends XReservationId implements Comparable<
 		
 		out.writeInt(iLimitCap);
 		out.writeDouble(iRestrictivity);
+		out.writeInt(iPriority);
+		out.writeInt(iFlags);
 	}
 }

JavaSource/org/unitime/timetable/defaults/ApplicationProperty.java
@@ -1704,6 +1704,96 @@ public enum ApplicationProperty {
 	@Description("Scheduling Subpart: default value of the Student Overlaps toggle")
 	SchedulingSubpartStudentOverlapsDefault("unitime.defaults.subpartStudentOverlaps"),
 	
+	@Type(Integer.class)
+	@DefaultValue("100")
+	@Description("Reservations: Individual reservation priority")
+	ReservationPriorityIndividual("unitime.reservation.priority.individual"),
+
+	@Type(Integer.class)
+	@DefaultValue("200")
+	@Description("Reservations: Group reservation priority")
+	ReservationPriorityGroup("unitime.reservation.priority.group"),
+	
+	@Type(Integer.class)
+	@DefaultValue("300")
+	@Description("Reservations: Reservation override priority")
+	ReservationPriorityOverride("unitime.reservation.priority.override"),
+	
+	@Type(Integer.class)
+	@DefaultValue("400")
+	@Description("Reservations: Course reservation priority")
+	ReservationPriorityCourse("unitime.reservation.priority.course"),
+	
+	@Type(Integer.class)
+	@DefaultValue("500")
+	@Description("Reservations: Curriculum reservation priority")
+	ReservationPriorityCurriculum("unitime.reservation.priority.curriculum"),
+	
+	@Type(Integer.class)
+	@DefaultValue("600")
+	@Description("Reservations: Curriculum reservation priority")
+	ReservationPriorityDummy("unitime.reservation.priority.dummy"),
+	
+	@Type(Boolean.class)
+	@DefaultValue("true")
+	@Description("Reservations: Individual reservation must be used")
+	ReservationMustBeUsedIndividual("unitime.reservation.mustBeUsed.individual"),
+
+	@Type(Boolean.class)
+	@DefaultValue("true")
+	@Description("Reservations: Group reservation must be used")
+	ReservationMustBeUsedGroup("unitime.reservation.mustBeUsed.group"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Course reservation must be used")
+	ReservationMustBeUsedCourse("unitime.reservation.mustBeUsed.course"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Curriculum reservation must be used")
+	ReservationMustBeUsedCurriculum("unitime.reservation.mustBeUsed.curriculum"),
+
+	@Type(Boolean.class)
+	@DefaultValue("true")
+	@Description("Reservations: Individual reservation can over limit")
+	ReservationCanOverLimitIndividual("unitime.reservation.canOverLimit.individual"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Group reservation can over limit")
+	ReservationCanOverLimitGroup("unitime.reservation.canOverLimit.group"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Course reservation can over limit")
+	ReservationCanOverLimitCourse("unitime.reservation.canOverLimit.course"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Curriculum reservation can over limit")
+	ReservationCanOverLimitCurriculum("unitime.reservation.canOverLimit.curriculum"),
+
+	@Type(Boolean.class)
+	@DefaultValue("true")
+	@Description("Reservations: Individual reservation allow time conflicts")
+	ReservationAllowOverlapIndividual("unitime.reservation.allowOverlap.individual"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Group reservation allow time conflicts")
+	ReservationAllowOverlapGroup("unitime.reservation.allowOverlap.group"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Course reservation allow time conflicts")
+	ReservationAllowOverlapCourse("unitime.reservation.allowOverlap.course"),
+
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Reservations: Curriculum reservation allow time conflicts")
+	ReservationAllowOverlapCurriculum("unitime.reservation.allowOverlap.curriculum"),
+
 	;
 
 	String iKey;

JavaSource/org/unitime/timetable/onlinesectioning/model/XCourseReservation.java
@@ -70,22 +70,6 @@ public class XCourseReservation extends XReservation {
     }
 
     /**
-     * Curriculum reservation cannot go over the limit
-     */
-    @Override
-    public boolean canAssignOverLimit() {
-        return false;
-    }
-    
-    /**
-     * Course reservation do not need to be used
-     */
-    @Override
-    public boolean mustBeUsed() {
-        return false;
-    }
-
-    /**
      * Reservation limit (-1 for unlimited)
      */
     @Override

JavaSource/org/unitime/timetable/onlinesectioning/model/XCurriculumReservation.java
@@ -71,21 +71,6 @@ public class XCurriculumReservation extends XReservation {
     		iMajors.addAll(reservation.getMajors());
     }
 
-    /**
-     * Curriculum reservation cannot go over the limit
-     */
-    @Override
-    public boolean canAssignOverLimit() {
-        return false;
-    }
-    
-    /**
-     * Curriculum reservation do not need to be used
-     */
-    @Override
-    public boolean mustBeUsed() {
-        return false;
-    }
 
     /**
      * Reservation limit (-1 for unlimited)

JavaSource/org/unitime/timetable/onlinesectioning/model/XDummyReservation.java
@@ -62,23 +62,6 @@ public class XDummyReservation extends XReservation {
         return false;
     }
 
-    /**
-     * Dummy reservation cannot go over the limit
-     */
-    @Override
-    public boolean canAssignOverLimit() {
-        return false;
-    }
-
-    
-    /**
-     * Dummy reservation do not need to be used
-     */
-    @Override
-    public boolean mustBeUsed() {
-        return false;
-    }
-    
 	public static class XDummyReservationSerializer implements Externalizer<XDummyReservation> {
 		private static final long serialVersionUID = 1L;
 

JavaSource/org/unitime/timetable/onlinesectioning/model/XGroupReservation.java
@@ -56,14 +56,6 @@ public class XGroupReservation extends XReservation {
     }
     
     /**
-     * Group reservations can not be assigned over the limit.
-     */
-    @Override
-    public boolean canAssignOverLimit() {
-        return false;
-    }
-
-    /**
      * Reservation limit
      */
     @Override
@@ -71,28 +63,11 @@ public class XGroupReservation extends XReservation {
         return iLimit;
     }
     
-    /**
-     * Overlaps are allowed for individual reservations. 
-     */
-    @Override
-    public boolean isAllowOverlap() {
-        return false;
-    }
-
 	@Override
 	public boolean isApplicable(XStudent student) {
 		return student.getGroups().contains(iGroup);
 	}
 
-    /**
-     * Individual or group reservation must be used (unless it is expired)
-     * @return true if not expired, false if expired
-     */
-	@Override
-	public boolean mustBeUsed() {
-		return true;
-	}
-	
 	@Override
 	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
     	super.readExternal(in);

JavaSource/org/unitime/timetable/onlinesectioning/model/XIndividualReservation.java
@@ -41,9 +41,6 @@ public class XIndividualReservation extends XReservation {
 	private static final long serialVersionUID = 1L;
 	private Set<Long> iStudentIds = new HashSet<Long>();
 	private Integer iLimit = null;
-    private boolean iMustBeUsed = false;
-    private boolean iAllowOverlap = false;
-    private boolean iAllowOverLimit = false;
     private Boolean iExpired = null;
     
     public XIndividualReservation() {
@@ -65,9 +62,9 @@ public class XIndividualReservation extends XReservation {
         super(XReservationType.Override, offering, reservation);
         for (Student student: reservation.getStudents())
         	iStudentIds.add(student.getUniqueId());
-        iMustBeUsed = reservation.getOverrideType().isMustBeUsed();
-        iAllowOverlap = reservation.getOverrideType().isAllowTimeConflict();
-        iAllowOverLimit = reservation.getOverrideType().isAllowOverLimit();
+        setMustBeUsed(reservation.getOverrideType().isMustBeUsed());
+        setAllowOverlap(reservation.getOverrideType().isAllowTimeConflict());
+        setCanAssignOverLimit(reservation.getOverrideType().isAllowOverLimit());
         iExpired = reservation.getOverrideType().isExpired();
     }
 
@@ -79,44 +76,12 @@ public class XIndividualReservation extends XReservation {
     public XIndividualReservation(ReservationOverride reservation) {
         super(XReservationType.Override, reservation);
         iStudentIds.addAll(reservation.getStudentIds());
-        iMustBeUsed = reservation.mustBeUsed();
-        iAllowOverlap = reservation.isAllowOverlap();
-        iAllowOverLimit = reservation.canAssignOverLimit();
     }
 
     public XIndividualReservation(GroupReservation reservation) {
         super(XReservationType.Group, reservation);
         iStudentIds.addAll(reservation.getStudentIds());
         iLimit = (int)Math.round(reservation.getReservationLimit());
-    }
-
-    /**
-     * Individual reservations are the only reservations that can be assigned over the limit.
-     */
-    @Override
-    public boolean canAssignOverLimit() {
-    	switch (getType()) {
-    	case Override:
-    		return iAllowOverLimit;
-    	case Individual:
-    		return true;
-    	default:
-    		return false;
-    	}
-    }
-    
-    /**
-     * Individual or group reservation must be used (unless it is expired)
-     * @return true if not expired, false if expired
-     */
-    @Override
-    public boolean mustBeUsed() {
-    	switch (getType()) {
-    	case Override:
-    		return iMustBeUsed && !isExpired();
-    	default:
-    		return !isExpired();
-    	}
     }
 
     /**
@@ -142,21 +107,6 @@ public class XIndividualReservation extends XReservation {
         return (iLimit == null ? iStudentIds.size() : iLimit);
     }
     
-    /**
-     * Overlaps are allowed for individual reservations. 
-     */
-    @Override
-    public boolean isAllowOverlap() {
-    	switch (getType()) {
-    	case Override:
-    		return iAllowOverlap;
-    	case Individual:
-    		return true;
-    	default:
-    		return false;
-    	}
-    }
-    
     @Override
     public boolean isExpired() {
     	return (getType() == XReservationType.Override && iExpired != null ? iExpired.booleanValue() : super.isExpired());
@@ -175,9 +125,6 @@ public class XIndividualReservation extends XReservation {
     	if (iLimit == -2) iLimit = null;
     	
     	if (getType() == XReservationType.Override) {
-    		iMustBeUsed = in.readBoolean();
-    		iAllowOverlap = in.readBoolean();
-    		iAllowOverLimit = in.readBoolean();
     		switch (in.readByte()) {
     		case 0:
     			iExpired = false; break;
@@ -200,9 +147,6 @@ public class XIndividualReservation extends XReservation {
 		out.writeInt(iLimit == null ? -2 : iLimit);
 		
 		if (getType() == XReservationType.Override) {
-			out.writeBoolean(iMustBeUsed);
-			out.writeBoolean(iAllowOverlap);
-			out.writeBoolean(iAllowOverLimit);
 			out.writeByte(iExpired == null ? 2 : iExpired.booleanValue() ? 1 : 0);
 		}
 	}

JavaSource/org/unitime/timetable/onlinesectioning/model/XReservationType.java
@@ -25,16 +25,12 @@ import java.io.Serializable;
  * @author Tomas Muller
  */
 public enum XReservationType implements Serializable {
-	Individual(100),
-	Group(200),
-	Override(300),
-	Course(400),
-	Curriculum(500),
-	Dummy(600),
-	None(700),
-		;
-	
-	int iPriority;
-	XReservationType(int priority) { iPriority = priority; }
-	public int getPriority() { return iPriority; }
+	Individual,
+	Group,
+	Override,
+	Course,
+	Curriculum,
+	Dummy,
+	None,
+	;
 }

commit 6e83ecbd7afe808af29e75c5d1cc9b4b02475bc0

Online Student Scheduling: Reservations
- fixed an issue taking all the properties (reservation priority, must be used, can assign over limit, allow overlap) from individual reservation
  - this fixes an issue introduced in commit bc910d8 (with the ability to overide the reservation properties)

JavaSource/org/unitime/timetable/onlinesectioning/model/XReservation.java
@@ -125,22 +125,22 @@ public abstract class XReservation extends XReservationId implements Comparable<
         	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
         	break;
         case Group:
-        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
-        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
-        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
-        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	setPriority(ApplicationProperty.ReservationPriorityGroup.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitGroup.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedGroup.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapGroup.isTrue());
         	break;
         case Curriculum:
-        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
-        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
-        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
-        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	setPriority(ApplicationProperty.ReservationPriorityCurriculum.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitCurriculum.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedCurriculum.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapCurriculum.isTrue());
         	break;
         case Course:
-        	setPriority(ApplicationProperty.ReservationPriorityIndividual.intValue());
-        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitIndividual.isTrue());
-        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedIndividual.isTrue());
-        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapIndividual.isTrue());
+        	setPriority(ApplicationProperty.ReservationPriorityCourse.intValue());
+        	setCanAssignOverLimit(ApplicationProperty.ReservationCanOverLimitCourse.isTrue());
+        	setMustBeUsed(ApplicationProperty.ReservationMustBeUsedCourse.isTrue());
+        	setAllowOverlap(ApplicationProperty.ReservationAllowOverlapCourse.isTrue());
         	break;
         case Override:
         	setPriority(ApplicationProperty.ReservationPriorityOverride.intValue());