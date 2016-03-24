UniTime/unitime

commit 811badede38165c4717a6377f69e92fb9fe0655a

Student Scheduling: Exclude Department from Student Scheduling
- there is a new ability to exclude some departments completely from student scheduling
- this is controlled by a new toggle that is present on each department (named Allow Student Scheduling on the Add/Edit Department page)
  - it defaults to true (student scheduling is enabled for the department)
- when a department is excluded, all courses controlled by this department are not even loaded into the solver (both batch and online)
  - students cannot see or take courses from the excluded department(s)
- instructional offerings that have all courses excluded from student scheduling do not need locking
- if all departments of a solver group are excluded from student scheduling, the commit restriction during online student scheduling phase does not apply to the solutions of such a solver group
- the online student scheduling server does not get automatically reloaded when the Allow Student Scheduling toggle is changed on a particular department during the online student scheduling phase
  - it has to be reloaded manually, using the Manage Solvers page

JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
@@ -234,7 +234,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 			org.unitime.timetable.onlinesectioning.match.CourseMatcher parent = matcher.getParentCourseMatcher();
 			for (CourseOffering c: (List<CourseOffering>)hibSession.createQuery(
 					"select c from CourseOffering c where " +
-					"c.subjectArea.session.uniqueId = :sessionId and (" +
+					"c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and (" +
 					"(lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr) like :q || '%' or lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr || ' - ' || c.title) like :q || '%') " +
 					(query.length()>2 ? "or lower(c.title) like '%' || :q || '%'" : "") + ") " +
 					(matcher.isAllCourseTypes() ? "" : matcher.isNoCourseType() ? types.isEmpty() ? " and c.courseType is null " : " and (c.courseType is null or c.courseType.reference in (" + types + ")) " : " and c.courseType.reference in (" + types + ") ") +
@@ -339,7 +339,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 			CourseOffering courseOffering = null;
 			for (CourseOffering c: (List<CourseOffering>)hibSession.createQuery(
 					"select c from CourseOffering c where " +
-					"c.subjectArea.session.uniqueId = :sessionId and " +
+					"c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and " +
 					"(lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr) = :course or lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr || ' - ' || c.title) = :course)")
 					.setString("course", course.toLowerCase())
 					.setLong("sessionId", sessionId)
@@ -458,7 +458,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 		setLastSessionId(sessionId);
 		OnlineSectioningServer server = getServerInstance(sessionId, false); 
 		if (server == null) {
-			CourseOffering courseOffering = SaveStudentRequests.getCourse(CourseOfferingDAO.getInstance().getSession(), sessionId, course);
+			CourseOffering courseOffering = lookupCourse(CourseOfferingDAO.getInstance().getSession(), sessionId, null, course, null);
 			if (courseOffering == null) throw new SectioningException(MSG.exceptionCourseDoesNotExist(course));
 			return getCourseDetailsProvider().getDetails(
 					new AcademicSessionInfo(courseOffering.getSubjectArea().getSession()),
@@ -474,7 +474,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 		setLastSessionId(sessionId);
 		OnlineSectioningServer server = getServerInstance(sessionId, false); 
 		if (server == null) {
-			CourseOffering courseOffering = SaveStudentRequests.getCourse(CourseOfferingDAO.getInstance().getSession(), sessionId, course);
+			CourseOffering courseOffering = lookupCourse(CourseOfferingDAO.getInstance().getSession(), sessionId, null, course, null);
 			if (courseOffering == null) throw new SectioningException(MSG.exceptionCourseDoesNotExist(course));
 			return courseOffering.getUniqueId();
 		} else {
@@ -587,7 +587,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 		}
 		for (CourseOffering co: (List<CourseOffering>)hibSession.createQuery(
 				"select c from CourseOffering c where " +
-				"c.subjectArea.session.uniqueId = :sessionId and " +
+				"c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and " +
 				"(lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr) = :course or lower(c.subjectArea.subjectAreaAbbreviation || ' ' || c.courseNbr || ' - ' || c.title) = :course)")
 				.setString("course", courseName.toLowerCase())
 				.setLong("sessionId", sessionId)
@@ -974,7 +974,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 						false
 						);
 				
-				if (server == null) {
+				if (server == null || !offering.isAllowStudentScheduling()) {
 					NameFormat nameFormat = NameFormat.fromReference(ApplicationProperty.OnlineSchedulingStudentNameFormat.value());
 					Map<String, String> approvedBy2name = new Hashtable<String, String>();
 					Hashtable<Long, ClassAssignmentInterface.Enrollment> student2enrollment = new Hashtable<Long, ClassAssignmentInterface.Enrollment>();
@@ -1446,7 +1446,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 		
 		HashSet<Long> courseIds = new HashSet<Long>(CourseOfferingDAO.getInstance().getSession().createQuery(
 				"select distinct c.uniqueId from CourseOffering c inner join c.instructionalOffering.coordinators i where " +
-				"c.subjectArea.session.uniqueId = :sessionId and i.externalUniqueId = :extId")
+				"c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and i.externalUniqueId = :extId")
 				.setLong("sessionId", sessionId).setString("extId", user.getExternalUserId()).setCacheable(true).list());
 		
 		return courseIds;
@@ -1459,7 +1459,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 
 		HashSet<Long> courseIds = new HashSet<Long>(CourseOfferingDAO.getInstance().getSession().createQuery(
 				"select distinct c.uniqueId from CourseOffering c inner join c.instructionalOffering.coordinators i where " +
-				"c.subjectArea.session.uniqueId = :sessionId and c.consentType.reference = :reference and " +
+				"c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and c.consentType.reference = :reference and " +
 				"i.externalUniqueId = :extId"
 				).setLong("sessionId", sessionId).setString("reference", "IN").setString("extId", user.getExternalUserId()).setCacheable(true).list());
 		
@@ -1467,13 +1467,13 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 		
 		if (user.getCurrentAuthority().hasRight(Right.SessionIndependent))
 			return new HashSet<Long>(CourseOfferingDAO.getInstance().getSession().createQuery(
-					"select c.uniqueId from CourseOffering c where c.subjectArea.session.uniqueId = :sessionId and c.consentType is not null"
+					"select c.uniqueId from CourseOffering c where c.subjectArea.session.uniqueId = :sessionId and c.subjectArea.department.allowStudentScheduling = true and c.consentType is not null"
 					).setLong("sessionId", sessionId).setCacheable(true).list());
 		
 		for (Department d: Department.getUserDepartments(user)) {
 			courseIds.addAll(CourseOfferingDAO.getInstance().getSession().createQuery(
 					"select distinct c.uniqueId from CourseOffering c where " +
-					"c.subjectArea.department.uniqueId = :departmentId and c.consentType is not null"
+					"c.subjectArea.department.uniqueId = :departmentId and c.subjectArea.department.allowStudentScheduling = true and c.consentType is not null"
 					).setLong("departmentId", d.getUniqueId()).setCacheable(true).list());
 		}
 		

WebContent/help/Release-Notes.xml
@@ -325,6 +325,23 @@
 				</line>
 			</description>
 		</item>
+		<item>
+			<name>Exclude Some Departments from Student Scheduling</name>
+			<description>
+				<line>There is a new ability to exclude some departments completely from student scheduling.</line>
+				<line>This is controlled by a new toggle that is present on each department (named Allow Student Scheduling on the Add/Edit Department page).
+					<line>It defaults to true (student scheduling is enabled for the department).</line>
+				</line>
+				<line>When a department is excluded, all courses controlled by this department are not even loaded into the solver (both batch and online).
+					<line>Students cannot see or take courses from the excluded department(s).</line>
+				</line>
+				<line>Instructional offerings that have all courses excluded from student scheduling do not need locking.</line>
+				<line>If all departments of a solver group are excluded from student scheduling, the commit restriction during online student scheduling phase does not apply to the solutions of such a solver group.</line>
+				<line>The online student scheduling server does not get automatically reloaded when the Allow Student Scheduling toggle is changed on a particular department during the online student scheduling phase.
+					<line>It has to be reloaded manually, using the Manage Solvers page.</line>
+				</line>
+			</description>
+		</item>
 	</category>
 	<category>
 		<title>Other Improvements</title>

Documentation/Database/MySQL/Changes/155 Department Student Scheduling.sql
@@ -1,0 +1,29 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+alter table department add allow_student_schd int(1) null default 1;
+
+/*
+ * Update database version
+ */
+
+update application_config set value='155' where name='tmtbl.db.version';
+
+commit;

Documentation/Database/Oracle/Changes/155 Department Student Scheduling.sql
@@ -1,0 +1,29 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+alter table department add allow_student_schd number(1) default 1;
+
+/*
+ * Update database version
+ */
+
+update application_config set value='155' where name='tmtbl.db.version';
+
+commit;

JavaSource/PreferenceGroup.hbm.xml
@@ -486,6 +486,12 @@
                 not-null="true"/>
 
             <property
+                name="allowStudentScheduling"
+                column="allow_student_schd"
+                type="java.lang.Boolean"
+                not-null="true"/>
+
+            <property
                 name="inheritInstructorPreferences"
                 column="instructor_pref"
                 type="java.lang.Boolean"

JavaSource/dbupdate.xml
@@ -10677,4 +10677,8 @@
 				uniqueid as solver_param_group_id from %SCHEMA%.solver_parameter_group where name = 'StudentSctOnline')
 		</plsql>
 	</update>
+	<update version="155" date="12-Dec-2015" comment="Department allow student scheduling toggle">
+		<mysql>alter table %SCHEMA%.department add allow_student_schd int(1) null default 1</mysql>
+		<plsql>alter table %SCHEMA%.department add allow_student_schd number(1) default 1</plsql>
+	</update>
 </dbupdate>

JavaSource/org/unitime/timetable/action/DepartmentEditAction.java
@@ -81,6 +81,8 @@ public class DepartmentEditAction extends Action {
 	        if(rsc.getMessage("button.addDepartment").equalsIgnoreCase(op)) {
             	myForm.reset(mapping, request);
             	myForm.setSessionId(sessionContext.getUser().getCurrentAcademicSessionId());
+            	myForm.setInheritInstructorPreferences(true);
+            	myForm.setAllowStudentScheduling(true);
         		sessionContext.checkPermission(Right.DepartmentAdd);
             	return mapping.findForward("add");
 	        }

JavaSource/org/unitime/timetable/action/DepartmentListAction.java
@@ -95,12 +95,12 @@ public class DepartmentListAction extends Action {
 
         if ("Export PDF".equals(request.getParameter("op"))) {
             
-            PdfWebTable webTable = new PdfWebTable((dispLastChanges ? 12 : 11), "Department List - " + sessionContext.getUser().getCurrentAuthority().getQualifiers("Session").get(0).getQualifierLabel(),
+            PdfWebTable webTable = new PdfWebTable((dispLastChanges ? 13 : 12), "Department List - " + sessionContext.getUser().getCurrentAuthority().getQualifiers("Session").get(0).getQualifierLabel(),
                     "departmentList.do?ord=%%",
                     (dispLastChanges ? new String[] { "Number", "Abbv", "Name", "External\nManager", "Subjects", "Rooms",
-                            "Status", "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Last\nChange" } 
+                            "Status", "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling", "Last\nChange" } 
                     : new String[] { "Number", "Abbreviation", "Name", "External\nManager", "Subjects", "Rooms", "Status",
-                            "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events" }),
+                            "Dist Pref\nPriority", "Allow\nRequired", "Instructor\nPref", "Events", "Student\nScheduling" }),
                     new String[] { "left", "left", "left", "left", "right", "right", "left", "right", "left", "left", "left", "left" },
                     new boolean[] { true, true, true, true, true, true, true, true, true, false });
             for (Iterator i=departmentListForm.getDepartments().iterator();i.hasNext();) {
@@ -148,6 +148,7 @@ public class DepartmentListAction extends Action {
                                 allowReq,
                                 d.isInheritInstructorPreferences() ? "Yes" : "No",
                                 d.isAllowEvents() ? "Yes" : "No",
+                                d.isAllowStudentScheduling() ? "Yes" : "No",
                                 lastChangeStr },
                            new Comparable[] {
                             d.getDeptCode(),
@@ -161,6 +162,7 @@ public class DepartmentListAction extends Action {
                             new Integer(allowReqOrd),
                             d.isInheritInstructorPreferences(),
                             d.isAllowEvents(),
+                            d.isAllowStudentScheduling(),
                             lastChangeCmp });
                 }
             }
@@ -172,15 +174,15 @@ public class DepartmentListAction extends Action {
             return null;
         }
         
-		WebTable webTable = new WebTable((dispLastChanges ? 12 : 11), "",
+		WebTable webTable = new WebTable((dispLastChanges ? 13 : 12), "",
 				"departmentList.do?ord=%%",
 				(dispLastChanges 
 					? new String[] { "Code", "Abbv", "Name", "External<br>Manager", 
 									 "Subjects", "Rooms", "Status", "Dist&nbsp;Pref<br>Priority", 
-									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Last<br>Change" } 
+									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Student<br>Scheduling", "Last<br>Change" } 
 					: new String[] { "Code", "Abbreviation", "Name", "External Manager",
 									 "Subjects", "Rooms", "Status", "Dist&nbsp;Pref<br>Priority", 
-									 "Allow<br>Required", "Instructor<br>Preferences", "Events" }),
+									 "Allow<br>Required", "Instructor<br>Preferences", "Events", "Student<br>Scheduling" }),
 				new String[] { "left", "left", "left", "left", "right",	"right", "left", "right", "left", "left", "left", "left" },
 				new boolean[] { true, true, true, true, true, true, true, true, true, true, true, false });
 		WebTable.setOrder(sessionContext, "DepartmentList.ord", request.getParameter("ord"), 1);
@@ -256,6 +258,7 @@ public class DepartmentListAction extends Action {
     						allowReq,
     						(d.isInheritInstructorPreferences() ? "<IMG border='0' title='Instructor preferences are to be inherited.' alt='Inherit Instructor Preferences' align='absmiddle' src='images/accept.png'>" : ""),
     						(d.isAllowEvents() ? "<IMG border='0' title='This department has event management enabled.' alt='Event Management' align='absmiddle' src='images/accept.png'>" : ""),
+    						(d.isAllowStudentScheduling() ? "<IMG border='0' title='This department has student scheduling enabled.' alt='Student Scheduling' align='absmiddle' src='images/accept.png'>" : ""),
     						lastChangeStr },
     				new Comparable[] {
     						d.getDeptCode(),
@@ -272,6 +275,7 @@ public class DepartmentListAction extends Action {
     						new Integer(allowReqOrd),
     						d.isInheritInstructorPreferences(),
     						d.isAllowEvents(),
+    						d.isAllowStudentScheduling(),
     						lastChangeCmp });
     		}
         }

commit 2d4269ba7490240e1cceaf6388e37cbf773d9729

Class Detail: Enrollments
- fixed a Null exception preventing the Enrollment table from showing on a class
- the issue was introduced by commit 811bade, with the ability to exclude certain departments from student scheduling

JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
@@ -980,16 +980,13 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 				Class_ clazz = (classOrOfferingId < 0 ? Class_DAO.getInstance().get(-classOrOfferingId, hibSession) : null);
 				if (offering == null && clazz == null) 
 					throw new SectioningException(MSG.exceptionBadClassOrOffering());
-				Long offeringId = (clazz == null ? offering.getUniqueId() : clazz.getSchedulingSubpart().getInstrOfferingConfig().getInstructionalOffering().getUniqueId());
+				if (offering == null)
+					offering = clazz.getSchedulingSubpart().getInstrOfferingConfig().getInstructionalOffering();
+				Long offeringId = offering.getUniqueId();
 				
-				getSessionContext().checkPermission(offering != null ? offering : clazz.getSchedulingSubpart().getInstrOfferingConfig().getInstructionalOffering(), Right.OfferingEnrollments);
+				getSessionContext().checkPermission(offering, Right.OfferingEnrollments);
 
-				OnlineSectioningServer server = getServerInstance(
-						clazz == null ?
-						offering.getControllingCourseOffering().getSubjectArea().getSessionId() :
-						clazz.getSchedulingSubpart().getInstrOfferingConfig().getInstructionalOffering().getControllingCourseOffering().getSubjectArea().getSessionId(),
-						false
-						);
+				OnlineSectioningServer server = getServerInstance(offering.getControllingCourseOffering().getSubjectArea().getSessionId(), false);
 				
 				if (server == null || !offering.isAllowStudentScheduling()) {
 					NameFormat nameFormat = NameFormat.fromReference(ApplicationProperty.OnlineSchedulingStudentNameFormat.value());

WebContent/help/Release-Notes.xml
@@ -48,6 +48,14 @@
 				</line>
 			</description>
 		</item>
+		<item>
+			<name>Class Detail: Enrollments</name>
+			<description>
+				<line>Fixed a Null exception preventing the Enrollment table from showing on a class.
+					<line>The issue was introduced by commit 811bade, with the ability to exclude certain departments from student scheduling.</line>
+				</line>
+			</description>
+		</item>
 	</category>
 	<category>
 		<title>Rooms</title>