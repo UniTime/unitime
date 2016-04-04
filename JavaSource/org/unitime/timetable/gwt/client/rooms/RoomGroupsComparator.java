1835465609cc5cd32a3a3e551935b9426233fa34
tomas-muller <muller@unitime.org>
2016-04-04 16:54:32 +0200

    Rooms: Sort by
    - corrected sort by a number when there are Nulls in the table

--- JavaSource/org/unitime/timetable/gwt/client/rooms/RoomGroupsComparator.java
+++ JavaSource/org/unitime/timetable/gwt/client/rooms/RoomGroupsComparator.java
@@ -131,10 +131,10 @@ public class RoomGroupsComparator implements Comparator<GroupInterface> {
 	}
 	
 	protected int compare(Number n1, Number n2) {
-		return (n1 == null ? n2 == null ? 0 : -1 : n2 == null ? -1 : Double.compare(n1.doubleValue(), n2.doubleValue())); 
+		return (n1 == null ? n2 == null ? 0 : -1 : n2 == null ? 1 : Double.compare(n1.doubleValue(), n2.doubleValue())); 
 	}
 	
 	protected int compare(Boolean b1, Boolean b2) {
-		return (b1 == null ? b2 == null ? 0 : -1 : b2 == null ? -1 : (b1.booleanValue() == b2.booleanValue()) ? 0 : (b1.booleanValue() ? 1 : -1));
+		return (b1 == null ? b2 == null ? 0 : -1 : b2 == null ? 1 : (b1.booleanValue() == b2.booleanValue()) ? 0 : (b1.booleanValue() ? 1 : -1));
 	}
 }
--- JavaSource/org/unitime/timetable/gwt/client/rooms/RoomsComparator.java
+++ JavaSource/org/unitime/timetable/gwt/client/rooms/RoomsComparator.java
@@ -139,11 +139,11 @@ public class RoomsComparator implements Comparator<RoomDetailInterface> {
 	}
 	
 	protected int compare(Number n1, Number n2) {
-		return (n1 == null ? n2 == null ? 0 : -1 : n2 == null ? -1 : Double.compare(n1.doubleValue(), n2.doubleValue())); 
+		return (n1 == null ? n2 == null ? 0 : -1 : n2 == null ? 1 : Double.compare(n1.doubleValue(), n2.doubleValue())); 
 	}
 	
 	protected int compare(Boolean b1, Boolean b2) {
-		return (b1 == null ? b2 == null ? 0 : -1 : b2 == null ? -1 : (b1.booleanValue() == b2.booleanValue()) ? 0 : (b1.booleanValue() ? 1 : -1));
+		return (b1 == null ? b2 == null ? 0 : -1 : b2 == null ? 1 : (b1.booleanValue() == b2.booleanValue()) ? 0 : (b1.booleanValue() ? 1 : -1));
 	}
 
 	public static boolean isApplicable(RoomsColumn column) {
