9904a797779df49d2cd21d454b18f737bf76295f
tomas-muller <muller@unitime.org>
2017-08-03 16:48:27 +0200

    Course Timetabling: Last-Like Student Course Demands
    - added ability to consider last-like student course demand priorities
    - enable using solver parameter LastLikeStudentCourseDemands.UsePriorities = true (defaults to false)
    - enrollment importance is computed as base ^ priority
      - where base is defined using solver parameter LastLikeStudentCourseDemands.BasePriorityWeight, defaults to 0.9
      - priority 0 has importance of 1.0, priority 1 of 0.9, priority 2 of 0.9^2 = 0.81, etc.

--- JavaSource/org/unitime/timetable/solver/curricula/LastLikeStudentCourseDemands.java
+++ JavaSource/org/unitime/timetable/solver/curricula/LastLikeStudentCourseDemands.java
@@ -32,6 +32,7 @@ import org.unitime.timetable.model.InstructionalOffering;
 import org.unitime.timetable.model.Session;
 import org.unitime.timetable.model.Student;
 import org.unitime.timetable.model.SubjectArea;
+import org.unitime.timetable.model.dao.CourseOfferingDAO;
 import org.unitime.timetable.solver.curricula.StudentCourseDemands.ProjectionsProvider;
 
 
@@ -44,8 +45,13 @@ public class LastLikeStudentCourseDemands implements StudentCourseDemands, Proje
 	protected Hashtable<Long, Hashtable<String, Set<WeightedStudentId>>> iDemandsForSubjectCourseNbr = new Hashtable<Long, Hashtable<String,Set<WeightedStudentId>>>();
 	protected Hashtable<Long, Set<WeightedCourseOffering>> iStudentRequests = null;
 	protected Long iSessionId = null;
+	protected double iBasePriorityWeight = 0.9;
+	protected boolean iUsePriorities = false;
+	private Hashtable<Long, Hashtable<String, Double>> iEnrollmentPriorities = new Hashtable<Long, Hashtable<String, Double>>();
 	
 	public LastLikeStudentCourseDemands(DataProperties properties) {
+		iUsePriorities = properties.getPropertyBoolean("LastLikeStudentCourseDemands.UsePriorities", iUsePriorities);
+		iBasePriorityWeight = properties.getPropertyDouble("LastLikeStudentCourseDemands.BasePriorityWeight", iBasePriorityWeight);
 	}
 	
 	@Override
@@ -72,13 +78,14 @@ public class LastLikeStudentCourseDemands implements StudentCourseDemands, Proje
 		Hashtable<String, Set<WeightedStudentId>> demandsForCourseNbr = new Hashtable<String, Set<WeightedStudentId>>();
 		iDemandsForSubjectCourseNbr.put(subject.getUniqueId(), demandsForCourseNbr);
 		for (Object[] d: (List<Object[]>)iHibSession.createQuery(
-				"select distinct d.courseNbr, d.coursePermId, s " +
+				"select distinct d.courseNbr, d.coursePermId, s, d.priority " +
 				"from LastLikeCourseDemand d inner join d.student s left join fetch s.areaClasfMajors where " +
 				"d.subjectArea.uniqueId = :subjectAreaId")
 				.setLong("subjectAreaId", subject.getUniqueId()).setCacheable(true).list()) {
 			String courseNbr = (String)d[0];
 			String coursePermId = (String)d[1];
 			Student student = (Student)d[2];
+			Integer priority = (Integer)d[3];
 			WeightedStudentId studentId = new WeightedStudentId(student, this);
 			Set<WeightedStudentId> studentIds = demandsForCourseNbr.get(courseNbr);
 			if (studentIds == null) {
@@ -94,6 +101,23 @@ public class LastLikeStudentCourseDemands implements StudentCourseDemands, Proje
                     iDemandsForPemId.put(coursePermId, studentIds);
                 }
                 studentIds.add(studentId);
+			}
+			
+			if (priority != null) {
+				Hashtable<String, Double> priorities = iEnrollmentPriorities.get(student.getUniqueId());
+				if (priorities == null) {
+					priorities = new Hashtable<String, Double>();
+					iEnrollmentPriorities.put(student.getUniqueId(), priorities);
+				}
+				priorities.put(subject.getUniqueId() + "|" + courseNbr, Math.pow(iBasePriorityWeight, priority));
+			}
+			if (priority != null && coursePermId != null) {
+				Hashtable<String, Double> priorities = iEnrollmentPriorities.get(student.getUniqueId());
+				if (priorities == null) {
+					priorities = new Hashtable<String, Double>();
+					iEnrollmentPriorities.put(student.getUniqueId(), priorities);
+				}
+				priorities.put(coursePermId, Math.pow(iBasePriorityWeight, priority));
 			}
 		}
 		return demandsForCourseNbr;
@@ -155,9 +179,22 @@ public class LastLikeStudentCourseDemands implements StudentCourseDemands, Proje
 		}
 		return iStudentRequests.get(studentId);
 	}
+	
+
 
 	@Override
 	public Double getEnrollmentPriority(Long studentId, Long courseId) {
-		return null;
+		if (!iUsePriorities) return null;
+		CourseOffering course = CourseOfferingDAO.getInstance().get(courseId);
+		if (course == null) return null;
+		if (iDemandsForSubjectCourseNbr.get(course.getSubjectArea().getUniqueId()) == null)
+			loadSubject(course.getSubjectArea());
+		Hashtable<String, Double> priorities = iEnrollmentPriorities.get(studentId);
+		if (priorities == null) return null;
+		if (course.getPermId() != null) {
+			Double priority = priorities.get(course.getPermId());
+			if (priority != null) return priority;
+		}
+		return priorities.get(course.getSubjectArea().getUniqueId() + "|" + course.getCourseNbr());
 	}
 }
