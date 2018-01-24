6dc8e453311a5211da044bb09bcccba493ce1116
says0528 <says@unitime.org>
2018-01-24 09:39:27 -0500

    Point In Time Data Increase Column Lenths
    - Increased the length of the name for the PitInstrOffreingConfig from
    10 to 20.
    - Increased the length of the class suffix PitClass from 10 to 20.

---
+++ Documentation/Database/MySQL/Changes/191 Longer configuration name and class suffix in Point In Time Data.sql
@@ -0,0 +1,31 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+alter table pit_instr_offer_config modify name varchar(20);
+alter table pit_class modify class_suffix varchar(20);
+
+
+/*
+ * Update database version
+ */
+
+update application_config set value='191' where name='tmtbl.db.version';
+
+commit;
