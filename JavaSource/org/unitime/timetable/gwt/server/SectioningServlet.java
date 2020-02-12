912abc0bff376830db101ca0cf3c1393fb436796
tomas-muller <muller@unitime.org>
2020-02-12 09:25:45 +0100

    Online Student Scheduling: XE Interface
    - get assigned (section) credit from the Banner's enrollment data and display it in the Scheduling Assistant
      - keep the assigned credits while doing schedule changes

--- JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
+++ JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
@@ -773,6 +773,14 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 						ret.setCanEnroll(false);
 				}
 			}
+			EligibilityCheck last = (EligibilityCheck)getSessionContext().getAttribute(SessionAttribute.OnlineSchedulingEligibility);
+			if (ret != null && last != null && last.hasCreditHours())
+				for (CourseAssignment ca: ret.getCourseAssignments())
+					for (ClassAssignment a: ca.getClassAssignments()) {
+						Float credit = last.getCreditHour(a);
+						a.setCreditHour(credit);
+						if (credit != null) a.setCredit(FixedCreditUnitConfig.formatCredit(credit));
+					}
 			return ret;
 		} catch (PageAccessException e) {
 			throw e;
@@ -902,6 +910,15 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 					for (ClassAssignmentInterface ca: ret)
 						ca.setCanEnroll(canEnroll);
 				}
+				EligibilityCheck last = (EligibilityCheck)getSessionContext().getAttribute(SessionAttribute.OnlineSchedulingEligibility);
+				if (ret != null && last != null && last.hasCreditHours())
+					for (ClassAssignmentInterface suggestion: ret)
+						for (CourseAssignment ca: suggestion.getCourseAssignments())
+							for (ClassAssignment a: ca.getClassAssignments()) {
+								Float credit = last.getCreditHour(a);
+								a.setCreditHour(credit);
+								if (credit != null) a.setCredit(FixedCreditUnitConfig.formatCredit(credit));
+							}
 				return ret;
 			}
 			
