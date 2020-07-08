2f3336d760a72fa116559dc73e41c6f8c9a2e78b
tomas-muller <muller@unitime.org>
2020-07-08 19:17:56 +0200

    Course Timetabling: Enrolled Student Course Demands
    - when using student class enrollments as initial sectioning solution
      (Global.LoadStudentEnrlsFromSolution is true)
      - fixed a Null error when there is a class loaded in without configuration information (committed class loaded from a different problem)

--- JavaSource/org/unitime/timetable/solver/TimetableDatabaseLoader.java
+++ JavaSource/org/unitime/timetable/solver/TimetableDatabaseLoader.java
@@ -3206,7 +3206,7 @@ public class TimetableDatabaseLoader extends TimetableLoader {
                     if (student==null) continue;
                     
                     Lecture lecture = (Lecture)iLectures.get(clazzId);
-                    if (lecture!=null) {
+                    if (lecture != null && lecture.getConfiguration() != null) {
 
                 		Set<Lecture> preEnrollments = iPreEnrollments.get(student);
                 		if (preEnrollments == null) {
