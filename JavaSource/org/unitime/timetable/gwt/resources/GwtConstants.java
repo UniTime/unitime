20cdbd3d321197bada95c68790a501cc2cd7aefa
tomas-muller <muller@unitime.org>
2021-04-30 09:32:58 +0200

    UniTime 4.6
    - UniTime version on the master branch changed to 4.6
    - for the latest version of UniTime 4.5, see maint_UniTime45 branch

--- JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
+++ JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
@@ -24,7 +24,7 @@ package org.unitime.timetable.gwt.resources;
  */
 public interface GwtConstants extends Constants {
 	
-	@DefaultStringValue("4.5")
+	@DefaultStringValue("4.6")
 	@DoNotTranslate
 	String version();
 
