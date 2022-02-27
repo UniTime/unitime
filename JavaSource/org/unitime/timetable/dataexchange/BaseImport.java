UniTime/unitime

commit e91747fe690331b352d3f72c1400e87b470fbc6e

BaseImport
Added getRequiredLongAttribute() and getOptionalLongAttribute().

JavaSource/org/unitime/timetable/dataexchange/BaseImport.java
@@ -94,27 +94,41 @@ public abstract class BaseImport extends DataExchangeHelper {
 	
 	protected Integer getRequiredIntegerAttribute(Element element, String attributeName, String elementName) throws Exception {
 		String attributeStr = getRequiredStringAttribute(element, attributeName, elementName);
-		return(new Integer(attributeStr));
+		return(Integer.parseInt(attributeStr));
 	}
 	
 	protected Integer getOptionalIntegerAttribute(Element element, String attributeName) {
 		String attributeStr = getOptionalStringAttribute(element, attributeName);
 		if (attributeStr != null){
-			return(new Integer(attributeStr));
+			return(Integer.parseInt(attributeStr));
 		} else {
 			return(null);
 		}
 	}
 	
+	protected Long getRequiredLongAttribute(Element element, String attributeName, String elementName) throws Exception {
+		String attributeStr = getRequiredStringAttribute(element, attributeName, elementName);
+		return(Long.parseLong(attributeStr));
+	}
+	
+	protected Long getOptionalLongAttribute(Element element, String attributeName) {
+		String attributeStr = getOptionalStringAttribute(element, attributeName);
+		if (attributeStr != null){
+			return(Long.parseLong(attributeStr));
+		} else {
+			return(null);
+		}
+	}
+
 	protected Boolean getRequiredBooleanAttribute(Element element, String attributeName, String elementName) throws Exception {
 		String attributeStr = getRequiredStringAttribute(element, attributeName, elementName);
-		return(new Boolean(attributeStr));
+		return(Boolean.getBoolean(attributeStr));
 	}
 	
 	protected Boolean getOptionalBooleanAttribute(Element element, String attributeName) {
 		String attributeStr = getOptionalStringAttribute(element, attributeName);
 		if (attributeStr != null) {
-			return(new Boolean(attributeStr));
+			return(Boolean.getBoolean(attributeStr));
 		} else {
 			return(null);
 		}

commit 36473c8d5dd3f1687a870c99917e021ac9a162c2

Data Exchange: XML Import
- fixed parsing of boolean values in some of the imports (including Course Offering XML import)
- this fixes a bug introduced in commit e91747f
  - unlike new Boolean("true"), the Boolean.getBoolean("true") does not return true, but false

JavaSource/org/unitime/timetable/dataexchange/BaseImport.java
@@ -122,13 +122,13 @@ public abstract class BaseImport extends DataExchangeHelper {
 
 	protected Boolean getRequiredBooleanAttribute(Element element, String attributeName, String elementName) throws Exception {
 		String attributeStr = getRequiredStringAttribute(element, attributeName, elementName);
-		return(Boolean.getBoolean(attributeStr));
+		return(Boolean.parseBoolean(attributeStr));
 	}
 	
 	protected Boolean getOptionalBooleanAttribute(Element element, String attributeName) {
 		String attributeStr = getOptionalStringAttribute(element, attributeName);
 		if (attributeStr != null) {
-			return(Boolean.getBoolean(attributeStr));
+			return(Boolean.parseBoolean(attributeStr));
 		} else {
 			return(null);
 		}

WebContent/help/Release-Notes.xml
@@ -28,6 +28,25 @@
 	<version>4.6.${build.number}</version>
 	<release-date>${build.date}</release-date>
 	<category>
+		<title>Administration</title>
+		<item>
+			<name>Data Exchange: Course Offering XML Import</name>
+			<description>
+				<line>Fixed parsing of boolean values in some of the imports (including Course Offering XML import).</line>
+			</description>
+		</item>
+		<item>
+			<name>People Lookup: Source Mapping</name>
+			<description>
+				<line>Corrected source mapping when unitime.lookup.source.defaults property is used.</line>
+			</description>
+		</item>
+	</category>
+</release>
+<release>
+	<version>4.6.77</version>
+	<release-date>Wed, 29 Jan 2022</release-date>
+	<category>
 		<title>Online Student Scheduling: Wait-Listing</title>
 		<item>
 			<name>Configuration</name>