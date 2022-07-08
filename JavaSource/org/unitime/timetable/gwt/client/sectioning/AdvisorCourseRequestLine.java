e01a46d6675b5ed5521e6f3bf3a5233c0651de76
tomas-muller <muller@unitime.org>
2022-07-08 14:58:16 +0200

    Advisor Course Recommendations: Layout
    - page made a little more responsive
    - adjust width of the Advisor Course Recommendations tables based on the size of the Student Course Requests table instead of cropping the student requests or leaving space in the recommendations tables

--- JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestLine.java
+++ JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestLine.java
@@ -227,6 +227,7 @@ public class AdvisorCourseRequestLine implements HasValue<Request> {
 	public void insert(FlexTable table, int row) {
 		table.setWidget(row, 0, iP);
 		table.getFlexCellFormatter().setColSpan(row, 0, 2);
+		table.getFlexCellFormatter().getElement(row, 0).getStyle().setProperty("max-width", 400, Unit.PX);
 		table.setWidget(row, 1, iCredit);
 		table.getFlexCellFormatter().getElement(row, 1).getStyle().setWidth(45, Unit.PX);
 		table.setWidget(row, 2, iNotes);
@@ -236,6 +237,8 @@ public class AdvisorCourseRequestLine implements HasValue<Request> {
 			table.setWidget(row, 3, iCritical);
 			table.setWidget(row, 4, iWaitList);
 			table.setWidget(row, 5, iButtons);
+			table.getFlexCellFormatter().getElement(row, 3).getStyle().setProperty("max-width", 25, Unit.PX);
+			table.getFlexCellFormatter().getElement(row, 4).getStyle().setProperty("max-width", 25, Unit.PX);
 			table.getFlexCellFormatter().getElement(row, 5).getStyle().setWidth(75, Unit.PX);
 		} else {
 			table.setWidget(row, 3, iButtons);
