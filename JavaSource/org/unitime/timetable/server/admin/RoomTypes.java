75f5b44af7de39cdcf2b18c0b35858177392ab16
tomas-muller <muller@unitime.org>
2022-10-13 16:53:34 +0200

    GWT: Room Types
    - Room Types page updated to set the order of room types using the up and down arrows

--- JavaSource/org/unitime/timetable/server/admin/RoomTypes.java
+++ JavaSource/org/unitime/timetable/server/admin/RoomTypes.java
@@ -20,7 +20,6 @@
 package org.unitime.timetable.server.admin;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -68,10 +67,11 @@ public class RoomTypes implements AdminTable {
 				new Field(MESSAGES.fieldReference(), FieldType.text, 140, 20, Flag.UNIQUE, Flag.NOT_EMPTY),
 				new Field(MESSAGES.fieldLabel(), FieldType.text, 400, 60, Flag.UNIQUE, Flag.NOT_EMPTY),
 				new Field(MESSAGES.fieldRoomClassType(), FieldType.list, 120, types, Flag.NOT_EMPTY),
-				new Field(MESSAGES.fieldSortOrder(), FieldType.number, 80, 10),
 				new Field(MESSAGES.fieldNbrRooms(), FieldType.number, 80, 10, Flag.READ_ONLY, Flag.NO_DETAIL)
 				);
-		data.setSortBy(3, 1);
+		data.setSaveOrder(false);
+		data.setCanMoveUpAndDown(true);
+		data.setAllowSort(false);
 		
 		for (RoomType type: RoomType.findAll()) {
 			Record r = data.addRecord(type.getUniqueId());
@@ -80,8 +80,7 @@ public class RoomTypes implements AdminTable {
 			r.setField(1, type.getLabel(), canEdit);
 			int nbrRooms = type.countRooms();
 			r.setField(2, type.isRoom() ? "room" : "other", canEdit && (nbrRooms == 0));
-			r.setField(3, type.getOrd() == null ? "" : String.valueOf(type.getOrd()), canEdit);
-			r.setField(4, String.valueOf(nbrRooms), false);
+			r.setField(3, String.valueOf(nbrRooms), false);
 			r.setDeletable(canEdit && nbrRooms == 0);
 		}
 		data.setAddable(context.hasPermission(Right.RoomTypes));
@@ -89,65 +88,51 @@ public class RoomTypes implements AdminTable {
 		return data;
 	}
 	
-	protected int nextOrd(Set<Integer> ords) {
-		for (int i = 0; i < ords.size() + 1; i++) {
-			if (!ords.contains(i)) {
-				ords.add(i);
-				return i;
-			}
-		}
-		return ords.size();
-	}
-	
 	protected int nextOrd() {
 		Set<RoomType> types = RoomType.findAll();
 		int idx = 0;
-		while (true) {
+		w: while (true) {
 			for (RoomType t: types) {
-				if (idx == t.getOrd()) { idx++; continue; }
+				if (idx == t.getOrd()) { idx++; continue w; }
 			}
 			return idx;
 		}
 	}
 	
 	@Override
+	@PreAuthorize("checkPermission('RoomTypes')")
 	public void save(SimpleEditInterface data, SessionContext context, Session hibSession) {
-		Set<Integer> ords = new HashSet<Integer>();
-		List<Record> missing = new ArrayList<Record>();
+		int ord = 0;
 		for (Record r: data.getRecords()) {
-			try {
-				ords.add(Integer.parseInt(r.getField(3)));
-			} catch (Exception e) {
-				if (!r.isEmpty()) missing.add(r);
-			}
-		}
-		for (Record r: missing) {
-			r.setField(3, String.valueOf(nextOrd(ords)));
+			if (r.isEmpty()) continue;
+			r.setOrder(ord++);
 		}
 		for (RoomType type: RoomType.findAll()) {
 			Record r = data.getRecord(type.getUniqueId());
 			if (r == null)
 				delete(type, context, hibSession);
 			else
-				update(type, r, context, hibSession);
+				update(type, r, r.getOrder(), context, hibSession);
 		}
 		for (Record r: data.getNewRecords())
-			save(r, context, hibSession);		
+			save(r, r.getOrder(), context, hibSession);		
 	}
 	
 	@Override
+	@PreAuthorize("checkPermission('RoomTypes')")
 	public void save(Record record, SessionContext context, Session hibSession) {
+		save(record, null, context, hibSession);
+	}
+	
+	public void save(Record record, Integer order, SessionContext context, Session hibSession) {
+		if (order == null) order = nextOrd();
 		context.checkPermission(Right.RoomTypes);
 		RoomType type = new RoomType();
 		type.setReference(record.getField(0));
 		type.setLabel(record.getField(1));
 		type.setRoom("room".equals(record.getField(2)));
-		try {
-			type.setOrd(Integer.valueOf(record.getField(3)));
-		} catch (Exception e) {
-			type.setOrd(nextOrd());
-			record.setField(3, type.getOrd().toString());
-		}
+		type.setOrd(order);
+		record.setField(3, "0");
 		record.setUniqueId((Long)hibSession.save(type));
 		ChangeLog.addChange(hibSession,
 				context,
@@ -159,36 +144,34 @@ public class RoomTypes implements AdminTable {
 				null);
 	}
 	
-	protected void update(RoomType type, Record record, SessionContext context, Session hibSession) {
+	protected void update(RoomType type, Record record, Integer order, SessionContext context, Session hibSession) {
 		if (type == null) return;
+		if (order == null) order = record.getOrder();
 		context.checkPermission(Right.RoomTypes);
-		if (ToolBox.equals(type.getOrd(), Integer.valueOf(record.getField(3))) &&
-			ToolBox.equals(type.getReference(), record.getField(0)) &&
-			ToolBox.equals(type.getLabel(), record.getField(1)) &&
-			ToolBox.equals(type.isRoom() ? "room" : "other", record.getField(2))) return;
+		boolean changed = !ToolBox.equals(type.getReference(), record.getField(0)) ||
+				!ToolBox.equals(type.getLabel(), record.getField(1)) ||
+				!ToolBox.equals(type.isRoom() ? "room" : "other", record.getField(2));
+		if (ToolBox.equals(type.getOrd(), order) && !changed) return;
 		type.setReference(record.getField(0));
 		type.setLabel(record.getField(1));
 		type.setRoom("room".equals(record.getField(2)));
-		try {
-			type.setOrd(Integer.valueOf(record.getField(3)));
-		} catch (Exception e) {
-			type.setOrd(nextOrd());
-			record.setField(3, type.getOrd().toString());
-		}
+		type.setOrd(order);
 		hibSession.saveOrUpdate(type);
-		ChangeLog.addChange(hibSession,
-				context,
-				type,
-				type.getReference(),
-				Source.SIMPLE_EDIT, 
-				Operation.UPDATE,
-				null,
-				null);
+		if (changed)
+			ChangeLog.addChange(hibSession,
+					context,
+					type,
+					type.getReference(),
+					Source.SIMPLE_EDIT, 
+					Operation.UPDATE,
+					null,
+					null);
 	}
 
 	@Override
+	@PreAuthorize("checkPermission('RoomTypes')")
 	public void update(Record record, SessionContext context, Session hibSession) {
-		update(RoomTypeDAO.getInstance().get(record.getUniqueId(), hibSession), record, context, hibSession);
+		update(RoomTypeDAO.getInstance().get(record.getUniqueId(), hibSession), record, null, context, hibSession);
 	}
 
 	protected void delete(RoomType type, SessionContext context, Session hibSession) {
@@ -205,6 +188,7 @@ public class RoomTypes implements AdminTable {
 	}
 	
 	@Override
+	@PreAuthorize("checkPermission('RoomTypes')")
 	public void delete(Record record, SessionContext context, Session hibSession) {
 		delete(RoomTypeDAO.getInstance().get(record.getUniqueId(), hibSession), context, hibSession);
 	}
