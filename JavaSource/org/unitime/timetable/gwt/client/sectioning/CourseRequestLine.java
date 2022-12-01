UniTime/unitime

commit 0d0d739145fd724c23d3247a9c88b6cd34e875eb

Online Student Scheduling: Course Finder
- Course Requests, Scheduling Assistant: when the student is not allowed to wait-list, do not show wait-listing in the Course Finder (only show overrides in this case, column is renamed to Override in this case)

- Advisor Course Recommendation: still always show wait-listing, regardles of the student/wait-listing status

JavaSource/org/unitime/timetable/gwt/client/sectioning/CourseRequestLine.java
@@ -183,6 +183,8 @@ public class CourseRequestLine extends P implements HasValue<Request> {
 			iWaitList.setVisible(iWaitListMode == WaitListMode.WaitList || iWaitListMode == WaitListMode.NoSubs);
 			changeVisibleStyle();
 		}
+		for (CourseSelectionBox box: iCourses)
+			box.iCourseFinderMultipleCourses.setShowWaitLists(iWaitListMode == WaitListMode.WaitList);
 	}
 	
 	public boolean getWaitList() {
@@ -527,7 +529,8 @@ public class CourseRequestLine extends P implements HasValue<Request> {
 				public CourseFinder createCourseFinder() {
 					CourseFinder finder = (alternative ? new CourseFinderDialog() : new SelectAllCourseFinderDialog());
 					
-					iCourseFinderMultipleCourses = new CourseFinderMultipleCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecReg);
+					iCourseFinderMultipleCourses = new CourseFinderMultipleCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecReg,
+							iWaitListMode == WaitListMode.WaitList);
 					iCourseFinderMultipleCourses.setDataProvider(new DataProvider<String, Collection<CourseAssignment>>() {
 						@Override
 						public void getData(String source, AsyncCallback<Collection<CourseAssignment>> callback) {

JavaSource/org/unitime/timetable/gwt/client/widgets/CourseFinderMultipleCourses.java
@@ -31,6 +31,7 @@ import org.unitime.timetable.gwt.client.ToolBox;
 import org.unitime.timetable.gwt.client.aria.AriaHiddenLabel;
 import org.unitime.timetable.gwt.client.aria.AriaStatus;
 import org.unitime.timetable.gwt.client.aria.AriaTabBar;
+import org.unitime.timetable.gwt.client.page.UniTimeNotifications;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.CourseFinderCourseDetails;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.ResponseEvent;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.ResponseHandler;
@@ -89,17 +90,20 @@ public class CourseFinderMultipleCourses extends P implements CourseFinder.Cours
 	private boolean iAllowMultiSelection = true;
 	
 	private boolean iShowCourseTitles = false, iShowDefaultSuggestions = false;
+	private boolean iShowWaitLists = true;
 	
 	public CourseFinderMultipleCourses() {
-		this(false, false, false, null);
+		this(false, false, false, null, true);
 	}
 	
-	public CourseFinderMultipleCourses(boolean showCourseTitles, boolean showDefaultSuggestions, boolean showRequired, SpecialRegistrationContext specReg) {
+	public CourseFinderMultipleCourses(boolean showCourseTitles, boolean showDefaultSuggestions, boolean showRequired, SpecialRegistrationContext specReg, boolean showWaitLists) {
 		super("courses");
 		
 		iShowCourseTitles = showCourseTitles;
 		iShowDefaultSuggestions = showDefaultSuggestions;
 		iSpecReg = specReg;
+		iShowWaitLists = showWaitLists;
+		UniTimeNotifications.info("Show wait-lists: " + showWaitLists);
 		
 		iCourses = new UniTimeTable<CourseAssignment>();
 		iCourses.setAllowMultiSelect(false);
@@ -112,7 +116,7 @@ public class CourseFinderMultipleCourses extends P implements CourseFinder.Cours
 		head.add(new UniTimeTableHeader(MESSAGES.colTitle()));
 		head.add(new UniTimeTableHeader(MESSAGES.colCredit()));
 		head.add(new UniTimeTableHeader(MESSAGES.colNote()));
-		head.add(new UniTimeTableHeader(MESSAGES.colWaitListAndAllowedOverrides()));
+		head.add(new UniTimeTableHeader(iShowWaitLists ? MESSAGES.colWaitListAndAllowedOverrides() : MESSAGES.colAllowedOverrides()));
 		iCourses.addRow(null, head);
 		iCourses.setColumnVisible(0, iAllowMultiSelection);
 		iCourses.addMouseDoubleClickListener(new UniTimeTable.MouseDoubleClickListener<CourseAssignment>() {
@@ -179,6 +183,11 @@ public class CourseFinderMultipleCourses extends P implements CourseFinder.Cours
 		add(iCourseDetailsTabBar);
 		add(iCourseDetailsPanel);
 		add(iCoursesTip);
+	}
+	
+	public void setShowWaitLists(boolean showWaitLists) {
+		iShowWaitLists = showWaitLists;
+		((UniTimeTableHeader)iCourses.getWidget(0, 7)).setText(iShowWaitLists ? MESSAGES.colWaitListAndAllowedOverrides() : MESSAGES.colAllowedOverrides());
 	}
 
 	@Override
@@ -329,7 +338,7 @@ public class CourseFinderMultipleCourses extends P implements CourseFinder.Cours
 						line.add(new Label(record.getNote() == null ? "" : record.getNote()));
 						if (record.hasNote()) hasNote = true;
 						P wl = new P("courses-wl");
-						if (record.isCanWaitList()) {
+						if (record.isCanWaitList() && iShowWaitLists) {
 							Label l = new Label(MESSAGES.courseAllowsForWaitListing());
 							l.setTitle(MESSAGES.courseAllowsForWaitListingTitle(record.getCourseName())); 
 							wl.add(l);

JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestLine.java
@@ -691,7 +691,7 @@ public class AdvisorCourseRequestLine implements HasValue<Request> {
 				public CourseFinder createCourseFinder() {
 					CourseFinder finder = (alternative ? new CourseFinderDialog() : new SelectAllCourseFinderDialog());
 					
-					iCourseFinderMultipleCourses = new CourseFinderMultipleCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecReg);
+					iCourseFinderMultipleCourses = new CourseFinderMultipleCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecReg, true);
 					iCourseFinderMultipleCourses.setDataProvider(new DataProvider<String, Collection<CourseAssignment>>() {
 						@Override
 						public void getData(String source, AsyncCallback<Collection<CourseAssignment>> callback) {

JavaSource/org/unitime/timetable/gwt/client/sectioning/StudentSectioningWidget.java
@@ -2510,6 +2510,7 @@ public class StudentSectioningWidget extends Composite implements HasResizeHandl
 	
 	public void checkEligibility(final AsyncCallback<OnlineSectioningInterface.EligibilityCheck> ret, String pin) {
 		LoadingWidget.getInstance().show(MESSAGES.courseRequestsLoading());
+		iQuickAddFinder = null;
 		iStartOver.setVisible(false); iStartOver.setEnabled(false);
 		iSpecRegCx.reset();
 		iContext.setPin(pin);
@@ -3309,7 +3310,8 @@ public class StudentSectioningWidget extends Composite implements HasResizeHandl
 					});
 				}
 			});
-			CourseFinderCourses courses = new CourseFinderCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecRegCx);
+			CourseFinderCourses courses = new CourseFinderCourses(CONSTANTS.showCourseTitle(), CONSTANTS.courseFinderSuggestWhenEmpty(), CONSTANTS.courseFinderShowRequired(), iSpecRegCx,
+					iEligibilityCheck != null && iEligibilityCheck.hasFlag(EligibilityFlag.CAN_WAITLIST));
 			courses.setDataProvider(new DataProvider<String, Collection<CourseAssignment>>() {
 				@Override
 				public void getData(String source, AsyncCallback<Collection<CourseAssignment>> callback) {

JavaSource/org/unitime/timetable/gwt/client/widgets/CourseFinderCourses.java
@@ -87,17 +87,19 @@ public class CourseFinderCourses extends P implements CourseFinder.CourseFinderT
 	private SpecialRegistrationContext iSpecReg;
 	
 	private boolean iShowCourseTitles = false, iShowDefaultSuggestions = false;
+	private boolean iShowWaitLists = true;
 	
 	public CourseFinderCourses() {
-		this(false, false, false, null);
+		this(false, false, false, null, true);
 	}
 	
-	public CourseFinderCourses(boolean showCourseTitles, boolean showDefaultSuggestions, boolean showRequired, SpecialRegistrationContext specReg) {
+	public CourseFinderCourses(boolean showCourseTitles, boolean showDefaultSuggestions, boolean showRequired, SpecialRegistrationContext specReg, boolean showWaitLists) {
 		super("courses");
 		
 		iShowCourseTitles = showCourseTitles;
 		iShowDefaultSuggestions = showDefaultSuggestions;
 		iSpecReg = specReg;
+		iShowWaitLists = showWaitLists;
 		
 		iCourses = new UniTimeTable<CourseAssignment>();
 		iCourses.setAllowMultiSelect(false);
@@ -109,7 +111,7 @@ public class CourseFinderCourses extends P implements CourseFinder.CourseFinderT
 		head.add(new UniTimeTableHeader(MESSAGES.colTitle()));
 		head.add(new UniTimeTableHeader(MESSAGES.colCredit()));
 		head.add(new UniTimeTableHeader(MESSAGES.colNote()));
-		head.add(new UniTimeTableHeader(MESSAGES.colWaitListAndAllowedOverrides()));
+		head.add(new UniTimeTableHeader(iShowWaitLists ? MESSAGES.colWaitListAndAllowedOverrides() : MESSAGES.colAllowedOverrides()));
 		iCourses.addRow(null, head);
 		iCourses.addMouseDoubleClickListener(new UniTimeTable.MouseDoubleClickListener<CourseAssignment>() {
 			@Override
@@ -175,6 +177,11 @@ public class CourseFinderCourses extends P implements CourseFinder.CourseFinderT
 		add(iCourseDetailsTabBar);
 		add(iCourseDetailsPanel);
 		add(iCoursesTip);
+	}
+	
+	public void setShowWaitLists(boolean showWaitLists) {
+		iShowWaitLists = showWaitLists;
+		((UniTimeTableHeader)iCourses.getWidget(0, 6)).setText(iShowWaitLists ? MESSAGES.colWaitListAndAllowedOverrides() : MESSAGES.colAllowedOverrides());
 	}
 
 	@Override
@@ -285,7 +292,7 @@ public class CourseFinderCourses extends P implements CourseFinder.CourseFinderT
 						line.add(new HTML(record.getNote() == null ? "" : record.getNote()));
 						if (record.hasNote()) hasNote = true;
 						P wl = new P("courses-wl");
-						if (record.isCanWaitList()) {
+						if (record.isCanWaitList() && iShowWaitLists) {
 							Label l = new Label(MESSAGES.courseAllowsForWaitListing());
 							l.setTitle(MESSAGES.courseAllowsForWaitListingTitle(record.getCourseName())); 
 							wl.add(l);

JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningMessages.java
@@ -208,6 +208,9 @@ public interface StudentSectioningMessages extends Messages {
 	@DefaultMessage("WL/Override")
 	String colWaitListAndAllowedOverrides();
 	
+	@DefaultMessage("Override")
+	String colAllowedOverrides();
+	
 	@DefaultMessage("Preference")
 	String colSchedulingPreference();
 	

commit 605da468c40ea570f6980f00074e54dacf79ebad

Student Scheduling Dashboard: Fix
- fixed a null issue when Scheduling Assistant is opened from the dashboard (caused by commit 0d0d739)

JavaSource/org/unitime/timetable/gwt/client/sectioning/CourseRequestLine.java
@@ -184,7 +184,8 @@ public class CourseRequestLine extends P implements HasValue<Request> {
 			changeVisibleStyle();
 		}
 		for (CourseSelectionBox box: iCourses)
-			box.iCourseFinderMultipleCourses.setShowWaitLists(iWaitListMode == WaitListMode.WaitList);
+			if (box.iCourseFinderMultipleCourses != null)
+				box.iCourseFinderMultipleCourses.setShowWaitLists(iWaitListMode == WaitListMode.WaitList);
 	}
 	
 	public boolean getWaitList() {

JavaSource/org/unitime/timetable/gwt/client/widgets/CourseFinderMultipleCourses.java
@@ -31,7 +31,6 @@ import org.unitime.timetable.gwt.client.ToolBox;
 import org.unitime.timetable.gwt.client.aria.AriaHiddenLabel;
 import org.unitime.timetable.gwt.client.aria.AriaStatus;
 import org.unitime.timetable.gwt.client.aria.AriaTabBar;
-import org.unitime.timetable.gwt.client.page.UniTimeNotifications;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.CourseFinderCourseDetails;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.ResponseEvent;
 import org.unitime.timetable.gwt.client.widgets.CourseFinder.ResponseHandler;
@@ -102,9 +101,7 @@ public class CourseFinderMultipleCourses extends P implements CourseFinder.Cours
 		iShowCourseTitles = showCourseTitles;
 		iShowDefaultSuggestions = showDefaultSuggestions;
 		iSpecReg = specReg;
-		iShowWaitLists = showWaitLists;
-		UniTimeNotifications.info("Show wait-lists: " + showWaitLists);
-		
+		iShowWaitLists = showWaitLists;		
 		iCourses = new UniTimeTable<CourseAssignment>();
 		iCourses.setAllowMultiSelect(false);
 		iCourses.setAllowSelection(true);