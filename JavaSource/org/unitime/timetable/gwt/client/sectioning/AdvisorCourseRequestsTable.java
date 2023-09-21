UniTime/unitime

commit c865c22c1195b6bcb7b2876f750877a3d126f53d

Student Scheduling: Learning Communities
- added a new LC priority level (as the very first one)
  - to be used for courses with LC reservations (by setting Load.LCRequestPriority=LC)
  - this will ensure that LC courses are assigned first

JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestsTable.java
@@ -125,7 +125,6 @@ public class AdvisorCourseRequestsTable extends WebTable implements TakesValue<C
 								new WebTable.Cell(ToolBox.toString(prefs), true),
 								request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), "") :
 								request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), "") :
-								request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") : new WebTable.Cell(""),
 								(iMode == WaitListMode.WaitList
 								? (request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), "") : new WebTable.Cell(""))
 								: (request.isNoSub() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestNoSubs(), "") : new WebTable.Cell(""))),

JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestsPage.java
@@ -1241,7 +1241,7 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 	protected void fillInStudentRequests() {
 		ArrayList<WebTable.Row> rows = new ArrayList<WebTable.Row>();
 		boolean hasPref = false, hasWarn = false, hasWait = false;
-		boolean hasCrit = false, hasImp = false, hasVital = false;
+		boolean hasCrit = false, hasImp = false, hasVital = false, hasLC = false;
 		NumberFormat df = NumberFormat.getFormat("0.#");
 		if (iDetails != null && iDetails.hasStudentRequest()) {
 			switch (iDetails.getStudentRequest().getWaitListMode()) {
@@ -1262,6 +1262,7 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 				if (request.isCritical()) hasCrit = true;
 				if (request.isImportant()) hasImp = true;
 				if (request.isVital()) hasVital = true;
+				if (request.isLC()) hasLC = true;
 				for (RequestedCourse rc: request.getRequestedCourse()) {
 					if (rc.isCourse()) {
 						ImageResource icon = null; String iconText = null;
@@ -1342,7 +1343,8 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 								(icon == null ? new WebTable.Cell(status) : new WebTable.IconCell(icon, iconText, status)),
 								(first && request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), "") :
 									first && request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), "") :
-									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") : new WebTable.Cell("")),
+									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") :
+									first && request.isLC() ? new WebTable.Cell(MESSAGES.opSetLC(), MESSAGES.descriptionRequestLC()) : new WebTable.Cell("")),
 								(iDetails.getStudentRequest().getWaitListMode() == WaitListMode.WaitList
 									 ? (first && request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), (request.hasWaitListedTimeStamp() ? sDF.format(request.getWaitListedTimeStamp()) : "")) : new WebTable.Cell(""))
 									 : (first && request.isNoSub() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestNoSubs(), "") : new WebTable.Cell(""))),
@@ -1387,6 +1389,7 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 				if (request.isCritical()) hasCrit = true;
 				if (request.isImportant()) hasImp = true;
 				if (request.isVital()) hasVital = true;
+				if (request.isLC()) hasLC = true;
 				for (RequestedCourse rc: request.getRequestedCourse()) {
 					if (rc.isCourse()) {
 						ImageResource icon = null; String iconText = null;
@@ -1467,7 +1470,8 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 								(icon == null ? new WebTable.Cell(status) : new WebTable.IconCell(icon, iconText, status)),
 								(first && request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), "") :
 									first && request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), "") :
-									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") : new WebTable.Cell("")),
+									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") :
+									first && request.isLC() ? new WebTable.Cell(MESSAGES.opSetLC(), MESSAGES.descriptionRequestLC()) : new WebTable.Cell("")),
 								(first && request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), (request.hasWaitListedTimeStamp() ? sDF.format(request.getWaitListedTimeStamp()) : "")) : new WebTable.Cell("")),
 								new WebTable.Cell(first && request.hasTimeStamp() ? sDF.format(request.getTimeStamp()) : "")
 								);
@@ -1592,12 +1596,12 @@ public class AdvisorCourseRequestsPage extends SimpleForm implements TakesValue<
 		iRequests.setData(rowArray);
 		iRequests.setColumnVisible(4, hasPref);
 		iRequests.setColumnVisible(5, hasWarn);
-		iRequests.setColumnVisible(7, hasCrit || hasImp || hasVital);
-		if (hasCrit && !hasImp && !hasVital)
+		iRequests.setColumnVisible(7, hasCrit || hasImp || hasVital || hasLC);
+		if (hasCrit && !hasImp && !hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetCritical());
-		else if (!hasCrit && hasImp && !hasVital)
+		else if (!hasCrit && hasImp && !hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetImportant());
-		else if (!hasCrit && !hasImp && hasVital)
+		else if (!hasCrit && !hasImp && hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetVital());
 		else
 			iRequests.getTable().setHTML(0, 7, MESSAGES.colCritical());

JavaSource/org/unitime/timetable/gwt/client/sectioning/PublishedSectioningSolutionsTable.java
@@ -363,6 +363,7 @@ public class PublishedSectioningSolutionsTable extends UniTimeTable<PublishedSec
 		case COMPLETE: return MESSAGES.colStudentsWithCompleteSchedule();
 		case DISBALANCED: return MESSAGES.colDisbalancedSections();
 		case NO_TIME: return MESSAGES.colClassesWithoutTime();
+		case LC: return MESSAGES.colAssignedLCCourseRequests();
 		default: return column.name();
 		}
 	}

JavaSource/org/unitime/timetable/gwt/client/sectioning/StudentSchedule.java
@@ -654,7 +654,7 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 	protected void fillInRequests() {
 		ArrayList<WebTable.Row> rows = new ArrayList<WebTable.Row>();
 		boolean hasPref = false, hasWarn = false, hasWait = false;
-		boolean hasCrit = false, hasImp = false, hasVital = false;
+		boolean hasCrit = false, hasImp = false, hasVital = false, hasLC = false;
 		NumberFormat df = NumberFormat.getFormat("0.#");
 		if (iAssignment.hasRequest()) {
 			setWaitListMode(iAssignment.getRequest().getWaitListMode(), false, true);
@@ -672,6 +672,7 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 				if (request.isCritical()) hasCrit = true;
 				if (request.isImportant()) hasImp = true;
 				if (request.isVital()) hasVital = true;
+				if (request.isLC()) hasLC = true;
 				for (RequestedCourse rc: request.getRequestedCourse()) {
 					if (rc.isCourse()) {
 						ImageResource icon = null; String iconText = null;
@@ -753,7 +754,8 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 								(icon == null ? new WebTable.Cell(status) : new WebTable.IconCell(icon, iconText, status)),
 								(first && iAssignment.isCanSetCriticalOverrides() ? new CriticalCell(request) : first && request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), MESSAGES.opSetCritical()) :
 									first && request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), MESSAGES.opSetImportant()) :
-									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), MESSAGES.opSetVital()) : new WebTable.Cell("")),
+									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), MESSAGES.opSetVital()) :
+									first && request.isLC() ? new WebTable.IconCell(RESOURCES.requestsLC(), MESSAGES.opSetLC(), MESSAGES.descriptionRequestLC()) : new WebTable.Cell("")),
 								(iAssignment.getRequest().getWaitListMode() == WaitListMode.WaitList
 									? (first && request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), (request.hasWaitListedTimeStamp() ? sWLF.format(request.getWaitListedTimeStamp()) : "")) : new WebTable.Cell(""))
 									: (first && request.isNoSub() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestNoSubs(), "") : new WebTable.Cell(""))),
@@ -798,6 +800,7 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 				if (request.isCritical()) hasCrit = true;
 				if (request.isImportant()) hasImp = true;
 				if (request.isVital()) hasVital = true;
+				if (request.isLC()) hasLC = true;
 				for (RequestedCourse rc: request.getRequestedCourse()) {
 					if (rc.isCourse()) {
 						ImageResource icon = null; String iconText = null;
@@ -879,7 +882,8 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 								(icon == null ? new WebTable.Cell(status) : new WebTable.IconCell(icon, iconText, status)),
 								(first && iAssignment.isCanSetCriticalOverrides() ? new CriticalCell(request) : first && request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), MESSAGES.opSetCritical()) :
 									first && request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), MESSAGES.opSetImportant()) :
-									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), MESSAGES.opSetVital()) : new WebTable.Cell("")),
+									first && request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), MESSAGES.opSetVital()) :
+									first && request.isLC() ? new WebTable.IconCell(RESOURCES.requestsLC(), MESSAGES.opSetLC(), MESSAGES.descriptionRequestLC()) : new WebTable.Cell("")),
 								(first && request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), (request.hasWaitListedTimeStamp() ? sWLF.format(request.getWaitListedTimeStamp()) : "")) : new WebTable.Cell("")),
 								new WebTable.Cell(first && request.hasTimeStamp() ? sDF.format(request.getTimeStamp()) : "")
 								);
@@ -1002,12 +1006,12 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 		iRequests.setData(rowArray);
 		iRequests.setColumnVisible(4, hasPref);
 		iRequests.setColumnVisible(5, hasWarn);
-		iRequests.setColumnVisible(7, hasCrit || hasImp || hasVital || iAssignment.isCanSetCriticalOverrides());
-		if (hasCrit && !hasImp && !hasVital)
+		iRequests.setColumnVisible(7, hasCrit || hasImp || hasVital || hasLC || iAssignment.isCanSetCriticalOverrides());
+		if (hasCrit && !hasImp && !hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetCritical());
-		else if (!hasCrit && hasImp && !hasVital)
+		else if (!hasCrit && hasImp && !hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetImportant());
-		else if (!hasCrit && !hasImp && hasVital)
+		else if (!hasCrit && !hasImp && hasVital && !hasLC)
 			iRequests.getTable().setHTML(0, 7, MESSAGES.opSetVital());
 		else
 			iRequests.getTable().setHTML(0, 7, MESSAGES.colCritical());
@@ -1597,12 +1601,12 @@ public class StudentSchedule extends Composite implements TakesValue<ClassAssign
 		
 		CriticalCell(Request request) {
 			super(
-				request.isCritical() ? RESOURCES.requestsCritical() : request.isImportant() ? RESOURCES.requestsImportant() : request.isVital() ? RESOURCES.requestsVital() : RESOURCES.requestsNotCritical(),
+				request.isCritical() ? RESOURCES.requestsCritical() : request.isImportant() ? RESOURCES.requestsImportant() : request.isVital() ? RESOURCES.requestsVital() : request.isLC() ? RESOURCES.requestsLC() : RESOURCES.requestsNotCritical(),
 				null,
-				request.isCritical() ? MESSAGES.opSetCritical() : request.isImportant() ? MESSAGES.opSetImportant() : request.isVital() ? MESSAGES.opSetVital() : MESSAGES.opSetNotCritical()
+				request.isCritical() ? MESSAGES.opSetCritical() : request.isImportant() ? MESSAGES.opSetImportant() : request.isVital() ? MESSAGES.opSetVital() : request.isLC() ? MESSAGES.opSetLC() : MESSAGES.opSetNotCritical()
 				);
-			getIcon().setAltText(request.isCritical() ? MESSAGES.descriptionRequestCritical() : request.isImportant() ? MESSAGES.descriptionRequestImportant() : request.isVital() ? MESSAGES.descriptionRequestVital() : MESSAGES.descriptionRequestNotCritical());
-			getIcon().setTitle(request.isCritical() ? MESSAGES.descriptionRequestCritical() : request.isImportant() ? MESSAGES.descriptionRequestImportant() : request.isVital() ? MESSAGES.descriptionRequestVital() : MESSAGES.descriptionRequestNotCritical());
+			getIcon().setAltText(request.isCritical() ? MESSAGES.descriptionRequestCritical() : request.isImportant() ? MESSAGES.descriptionRequestImportant() : request.isVital() ? MESSAGES.descriptionRequestVital() : request.isLC() ? MESSAGES.descriptionRequestLC() : MESSAGES.descriptionRequestNotCritical());
+			getIcon().setTitle(request.isCritical() ? MESSAGES.descriptionRequestCritical() : request.isImportant() ? MESSAGES.descriptionRequestImportant() : request.isVital() ? MESSAGES.descriptionRequestVital() : request.isLC() ? MESSAGES.descriptionRequestLC() : MESSAGES.descriptionRequestNotCritical());
 			iRequest = request;
 			getIcon().getElement().getStyle().setCursor(Cursor.POINTER);
 			ClickHandler ch = new ClickHandler() {

JavaSource/org/unitime/timetable/gwt/client/widgets/WebTable.java
@@ -531,6 +531,10 @@ public class WebTable extends Composite implements HasMobileScroll {
 		public Cell(String value) {
 			iValue = value;
 		}
+		public Cell(String value, String title) {
+			iValue = value;
+			iTitle = title;
+		}
 		public Cell(String value, int colSpan, String width) {
 			iValue = value;
 			iColSpan = colSpan;

JavaSource/org/unitime/timetable/gwt/resources/GwtMessages.java
@@ -6709,6 +6709,9 @@ public interface GwtMessages extends Messages {
 	@DefaultMessage("Vital<br>Assignments")
 	String colAssignedVitalCourseRequests();
 	
+	@DefaultMessage("LC<br>Assignments")
+	String colAssignedLCCourseRequests();
+	
 	@DefaultMessage("Complete<br>Schedule")
 	String colStudentsWithCompleteSchedule();
 	

JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningConstants.java
@@ -183,6 +183,9 @@ public interface StudentSectioningConstants extends Constants {
 		"Vital",
 		"Assigned Vital",
 		"Not Assigned Vital",
+		"LC",
+		"Assigned LC",
+		"Not Assigned LC",
 	})
 	String[] assignmentType();
 	

commit 1faaddff564bb18644cf326f75cdd10942b4b3eb

Scheduling Assistant, Course Requests: Advisor Recommendations
- fixed the display of the Notes column in the Advisor Course Recommendations dialog
- it looks like this got accidentally broken in commit c865c22

JavaSource/org/unitime/timetable/gwt/client/sectioning/AdvisorCourseRequestsTable.java
@@ -125,6 +125,7 @@ public class AdvisorCourseRequestsTable extends WebTable implements TakesValue<C
 								new WebTable.Cell(ToolBox.toString(prefs), true),
 								request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), "") :
 								request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), "") :
+								request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") : new WebTable.Cell(""),
 								(iMode == WaitListMode.WaitList
 								? (request.isWaitList() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestWaitListed(), "") : new WebTable.Cell(""))
 								: (request.isNoSub() ? new WebTable.IconCell(RESOURCES.requestsWaitList(), MESSAGES.descriptionRequestNoSubs(), "") : new WebTable.Cell(""))),
@@ -233,10 +234,10 @@ public class AdvisorCourseRequestsTable extends WebTable implements TakesValue<C
 								new WebTable.Cell(rc.hasCourseTitle() ? rc.getCourseTitle() : ""),
 								credit,
 								new WebTable.Cell(ToolBox.toString(prefs), true),
-								new WebTable.Cell(""),
 								request.isCritical() ? new WebTable.IconCell(RESOURCES.requestsCritical(), MESSAGES.descriptionRequestCritical(), "") :
 								request.isImportant() ? new WebTable.IconCell(RESOURCES.requestsImportant(), MESSAGES.descriptionRequestImportant(), "") :
 								request.isVital() ? new WebTable.IconCell(RESOURCES.requestsVital(), MESSAGES.descriptionRequestVital(), "") : new WebTable.Cell(""),
+								new WebTable.Cell(""),
 								note
 								);
 						} else {