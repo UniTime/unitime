8aa1ee58535de822e472807ccf7845bf21234845
tomas-muller <muller@unitime.org>
2023-11-28 19:01:51 +0100

    Student Scheduling Rules: Serialization
    - provide default constructor for XSchedulingRule
    - this is to avoid InvalidClassException: no valid constructor

--- JavaSource/org/unitime/timetable/onlinesectioning/model/XSchedulingRule.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/model/XSchedulingRule.java
@@ -54,6 +54,8 @@ public class XSchedulingRule implements Serializable, Externalizable {
 	private Boolean iAppliesToBatch;
 	private Boolean iAdminOverride;
 	private Boolean iAdvisorOverride;
+	
+	public XSchedulingRule() {}
 
 	public XSchedulingRule(StudentSchedulingRule rule) {
 		iUniqueId = rule.getUniqueId();
