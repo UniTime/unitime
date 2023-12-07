989ce34fc020168e65d2c5aba371c4d6f7e0f89b
tomas-muller <muller@unitime.org>
2023-12-07 14:16:09 +0100

    Online Student Scheduling: Context Check
    - decreased logging level for the "different student id" message

--- JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
+++ JavaSource/org/unitime/timetable/gwt/server/SectioningServlet.java
@@ -3099,7 +3099,7 @@ public class SectioningServlet implements SectioningService, DisposableBean {
 			if (check)
 				sLog.debug("ContextCheck: different student id, permission check: " + check);
 			else
-				sLog.warn("ContextCheck: different student id, permission check: " + check);
+				sLog.info("ContextCheck: different student id, permission check: " + check);
 		}
 	}
 	
