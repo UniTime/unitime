ea7e08f5d862b3bc3284c729caff5007bff1ca23
tomas-muller <muller@unitime.org>
2024-04-19 15:57:04 +0200

    Code Cleanup: removed unused imports

--- JavaSource/org/unitime/timetable/gwt/shared/ClassSetupInterface.java
+++ JavaSource/org/unitime/timetable/gwt/shared/ClassSetupInterface.java
@@ -25,7 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.unitime.timetable.gwt.client.page.UniTimeNotifications;
 import org.unitime.timetable.gwt.command.client.GwtRpcRequest;
 import org.unitime.timetable.gwt.command.client.GwtRpcResponse;
 
