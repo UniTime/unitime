UniTime/unitime

commit 1c58aabb9dd1cd359821b98258fa56f609bb9ace

Student Scheduling Dashboards: Reservations Tooltip
- do not show limit (null) for unlimited classes
- show flags for curriculum override reservations
- individual, curriculum: limit the number of restrictions to 5 lines

JavaSource/org/unitime/timetable/gwt/client/reservations/ReservationTable.java
@@ -297,6 +297,13 @@ public class ReservationTable extends Composite {
 				for (IdName student: ((IndividualReservation) reservation).getStudents()) {
 					students.add(new Label(student.getName(), false));
 				}
+				if (students.getWidgetCount() > 6) {
+					int more = students.getWidgetCount();
+					while (students.getWidgetCount() > 5)
+						students.remove(5);
+					Label l = new Label(MESSAGES.moreItems(more), false); l.getElement().getStyle().setMarginLeft(10, Unit.PX);
+					students.add(l);
+				}
 				line.add(students);
 			} else if (reservation instanceof GroupReservation) {
 				if (reservation.isOverride()) {
@@ -320,7 +327,13 @@ public class ReservationTable extends Composite {
 				IdName group = ((LCReservation) reservation).getGroup();
 				line.add(new Label(group.getAbbv() + " - " + group.getName() + " (" + group.getLimit() + ")", false));
 			} else if (reservation instanceof CurriculumReservation) {
-				line.add(new Label(MESSAGES.reservationCurriculumAbbv()));
+				if (reservation.isOverride()) {
+					Label label = new Label(MESSAGES.reservationCurriculumOverride() + flags);
+					label.getElement().getStyle().setWhiteSpace(WhiteSpace.PRE);
+					line.add(label);
+				} else {
+					line.add(new Label(MESSAGES.reservationCurriculumAbbv()));
+				}
 				Areas curriculum = ((CurriculumReservation) reservation).getCurriculum();
 				VerticalPanel owner = new VerticalPanel();
 				for (IdName area: curriculum.getAreas()) {
@@ -374,6 +387,13 @@ public class ReservationTable extends Composite {
 					l.getElement().getStyle().setMarginLeft(10, Unit.PX);
 					owner.add(l);
 				}
+				if (owner.getWidgetCount() > 6) {
+					int more = owner.getWidgetCount() - 5;
+					while (owner.getWidgetCount() > 5)
+						owner.remove(5);
+					Label l = new Label(MESSAGES.moreItems(more), false); l.getElement().getStyle().setMarginLeft(10, Unit.PX);
+					owner.add(l);
+				}
 				line.add(owner);
 			} else if (reservation instanceof UniversalReservation) {
 				Label label = new Label(MESSAGES.reservationUniversalOverrideAbbv() + flags);
@@ -390,7 +410,7 @@ public class ReservationTable extends Composite {
 				restrictions.add(new Label(MESSAGES.selectionConfiguration(config.getName(), config.getLimit() == null ? MESSAGES.configUnlimited() : config.getLimit().toString())));
 			}
 			for (Clazz clazz: reservation.getClasses()) {
-				restrictions.add(new Label(clazz.getName() + " (" + clazz.getLimit() + ")", false));
+				restrictions.add(new Label(clazz.getName() + (clazz.getLimit() == null ? "" : " (" + clazz.getLimit() + ")"), false));
 			}
 			line.add(restrictions);
 			if (reservation.hasInclusive() && reservation.isInclusive()) {

JavaSource/org/unitime/timetable/gwt/resources/GwtMessages.java
@@ -7949,4 +7949,7 @@ public interface GwtMessages extends Messages {
 	
 	@DefaultMessage("(Link)")
 	String roomExternalLink();
+	
+	@DefaultMessage("\u2026 {0} more")
+	String moreItems(int count);
 }

WebContent/help/Release-Notes.xml
@@ -67,6 +67,16 @@
 				<line>Added an ability to filter reports using a Filter with similar capabilities as on the dashboard pages.</line>
 			</description>
 		</item>
+		<item>
+			<name>Batch Student Solver Dashboard, Online Student Scheduling Dashboard</name>
+			<description>
+				<line>Reservation Tooltip:
+					<line>Do not show limit (null) for unlimited classes.</line>
+					<line>Show flags for curriculum override reservations.</line>
+					<line>Individual, Curriculum: Limit the number of restrictions to 5 lines.</line>
+				</line>
+			</description>
+		</item>
 	</category>
 	<category>
 		<title>Administration</title>

commit 956d38dff94931dc762c676bdb607c3539762ef3

Reservations Table
- limit the number of restrictions and owners to 5 lines
  - but only when showing as tooltip in the dashboard
- show all restrictions/owners on the Reservations page and on the Instructional Offering Detail page
  - this fixes a problem introduced in commit 1c58aab (limiting the number of owners everywhere)

JavaSource/org/unitime/timetable/gwt/client/reservations/ReservationTable.java
@@ -201,9 +201,13 @@ public class ReservationTable extends Composite {
 		else
 			iHeader.clearMessage();
 	}
-
-
+	
 	public void populate(List<ReservationInterface> reservations) {
+		populate(reservations, -1);
+	}
+
+
+	public void populate(List<ReservationInterface> reservations, int owLimit) {
 		List<UniTimeTableHeader> header = new ArrayList<UniTimeTableHeader>();
 		
 		for (final ReservationColumn column: ReservationColumn.values()) {
@@ -297,10 +301,10 @@ public class ReservationTable extends Composite {
 				for (IdName student: ((IndividualReservation) reservation).getStudents()) {
 					students.add(new Label(student.getName(), false));
 				}
-				if (students.getWidgetCount() > 6) {
-					int more = students.getWidgetCount();
-					while (students.getWidgetCount() > 5)
-						students.remove(5);
+				if (owLimit > 0 && students.getWidgetCount() > owLimit + 1) {
+					int more = students.getWidgetCount() - owLimit;
+					while (students.getWidgetCount() > owLimit)
+						students.remove(owLimit);
 					Label l = new Label(MESSAGES.moreItems(more), false); l.getElement().getStyle().setMarginLeft(10, Unit.PX);
 					students.add(l);
 				}
@@ -387,10 +391,10 @@ public class ReservationTable extends Composite {
 					l.getElement().getStyle().setMarginLeft(10, Unit.PX);
 					owner.add(l);
 				}
-				if (owner.getWidgetCount() > 6) {
-					int more = owner.getWidgetCount() - 5;
-					while (owner.getWidgetCount() > 5)
-						owner.remove(5);
+				if (owLimit > 0 && owner.getWidgetCount() > owLimit + 1) {
+					int more = owner.getWidgetCount() - owLimit;
+					while (owner.getWidgetCount() > owLimit)
+						owner.remove(owLimit);
 					Label l = new Label(MESSAGES.moreItems(more), false); l.getElement().getStyle().setMarginLeft(10, Unit.PX);
 					owner.add(l);
 				}
@@ -411,6 +415,13 @@ public class ReservationTable extends Composite {
 			}
 			for (Clazz clazz: reservation.getClasses()) {
 				restrictions.add(new Label(clazz.getName() + (clazz.getLimit() == null ? "" : " (" + clazz.getLimit() + ")"), false));
+			}
+			if (owLimit > 0 && restrictions.getWidgetCount() > owLimit + 1) {
+				int more = restrictions.getWidgetCount() - owLimit;
+				while (restrictions.getWidgetCount() > owLimit)
+					restrictions.remove(owLimit);
+				Label l = new Label(MESSAGES.moreItems(more), false); l.getElement().getStyle().setMarginLeft(10, Unit.PX);
+				restrictions.add(l);
 			}
 			line.add(restrictions);
 			if (reservation.hasInclusive() && reservation.isInclusive()) {

JavaSource/org/unitime/timetable/gwt/client/sectioning/SectioningStatusPage.java
@@ -3154,7 +3154,7 @@ public class SectioningStatusPage extends Composite {
 									}
 								if (result != null && !result.isEmpty() && iMouseOver) {
 									ReservationTable rt = new ReservationTable(false, false);
-									rt.populate(result);
+									rt.populate(result, 5);
 									rt.getTable().setColumnVisible(ReservationColumn.LAST_LIKE.ordinal(), false);
 									rt.getTable().setColumnVisible(ReservationColumn.PROJECTED_BY_RULE.ordinal(), false);
 									rt.getTable().setColumnVisible(ReservationColumn.EXPIRATION_DATE.ordinal(), iOnline);