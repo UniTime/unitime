UniTime/unitime

commit cb8d1d9d3249b7f3dab069e6eab0ddffe07b4d1b

Events: Filters
- decrease the number of backend calls populating the room and event filters by
  - only load each filter once when the page is opened
  - only create event add/edit event widgets first time they are needed (also include their own copy of room filter)
- on the personal schedule page, create a dummy version of the filters as they are not being shown/used

JavaSource/org/unitime/timetable/gwt/client/events/RoomFilterBox.java
@@ -70,7 +70,11 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 	private FilterBox.CustomFilter iDepartmentFilter;
 	
 	public RoomFilterBox(AcademicSessionProvider session) {
-		super(session);
+		this(session, true);
+	}
+	
+	public RoomFilterBox(AcademicSessionProvider session, boolean init) {
+		super(session, init);
 		
 		iDepartments = new ListBox();
 		iDepartments.setMultipleSelect(false);
@@ -434,7 +438,7 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 						}
 					}
 				}
-				if (getAcademicSessionId() != null)
+				if (getAcademicSessionId() != null) {
 					init(false, getAcademicSessionId(), new Command() {
 						@Override
 						public void execute() {
@@ -442,6 +446,7 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 								showFilterPopup();
 						}
 					});
+				}
 				setAriaLabel(ARIA.roomFilter(toAriaString()));
 			}
 		});
@@ -452,6 +457,11 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 				setAriaLabel(ARIA.roomFilter(toAriaString()));
 			}
 		});
+	}
+	
+	@Override
+	public void setValue(String value, boolean fireEvents) {
+		iFilter.getWidget().setValue(value, fireEvents);
 	}
 	
 	@Override

JavaSource/org/unitime/timetable/gwt/client/events/AddMeetingsDialog.java
@@ -225,7 +225,7 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		iTimes = new StartEndTimeSelector();
 		form.addRow(MESSAGES.propTimes(), iTimes);
 		
-		iRooms = new RoomFilterBox(session);
+		iRooms = new RoomFilterBox(session, false);
 		form.addRow(MESSAGES.propLocations(), iRooms);
 		
 		iScrollDates = new ScrollPanel(form);
@@ -719,7 +719,6 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		iMatchingRooms = null;
 		iDates.setValue(selectedDates);
 		iTimes.setValue(selectedTimes, true); iTimes.setDiff(12);
-		iRooms.setValue(roomFilterValue == null || roomFilterValue.isEmpty() ? "flag:Event" : roomFilterValue.contains("flag:All") || roomFilterValue.contains("flag:Event") ? roomFilterValue : "flag:Event " + roomFilterValue, true);
 		iSelected.clear();
 		if (meetings != null && !meetings.isEmpty()) {
 			MeetingInterface first = meetings.get(0);
@@ -740,6 +739,8 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 				roomFilter += room;
 			}
 			iRooms.setValue((roomFilterValue == null || roomFilterValue.isEmpty() ? "flag:Event" : roomFilterValue.contains("flag:All") || roomFilterValue.contains("flag:Event") ? roomFilterValue : "flag:Event " + roomFilterValue) + (roomFilter.isEmpty() ? "" : " " + roomFilter), true);
+		} else {
+			iRooms.setValue(roomFilterValue == null || roomFilterValue.isEmpty() ? "flag:Event" : roomFilterValue.contains("flag:All") || roomFilterValue.contains("flag:Event") ? roomFilterValue : "flag:Event " + roomFilterValue, true);
 		}
 	}
 

JavaSource/org/unitime/timetable/gwt/client/events/DummyFilter.java
@@ -1,0 +1,40 @@
+package org.unitime.timetable.gwt.client.events;
+
+import org.unitime.timetable.gwt.client.widgets.FilterBox.Chip;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.TakesValue;
+import com.google.gwt.user.client.ui.Widget;
+
+public abstract class DummyFilter<T> extends Widget implements FilterBoxInterface<T> {
+	private String iValue = "";
+
+	@Override
+	public String getValue() { return iValue; }
+
+	@Override
+	public void setValue(String value) { iValue = value; }
+
+	@Override
+	public void setValue(String value, boolean fireEvents) {
+		iValue = value;
+		if (fireEvents)
+			ValueChangeEvent.fire(DummyFilter.this, getValue());
+	}
+
+	@Override
+	public HandlerRegistration addValueChangeHandler(ValueChangeHandler<String> handler) {
+		return addHandler(handler, ValueChangeEvent.getType());
+	}
+
+	@Override
+	public Chip getChip(String command) { return null; }
+
+	@Override
+	public boolean hasChip(Chip chip) { return false; }
+
+	@Override
+	public void setDefaultValueProvider(TakesValue<String> defaultValue) {}
+}

JavaSource/org/unitime/timetable/gwt/client/events/EventAdd.java
@@ -151,7 +151,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 	
 	private CourseRelatedObjectsTable iCourses;
 	
-	private AddMeetingsDialog iEventAddMeetings, iEventModifyMeetings;
+	private AddMeetingsDialog iEventAddMeetings = null, iEventModifyMeetings = null;
 	private AcademicSessionSelectionBox iSession;
 	private Lookup iLookup, iAdditionalLookup;
 	private UniTimeTable<ContactInterface> iContacts;
@@ -755,127 +755,11 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			}
 		});
 		
-		iEventAddMeetings = new AddMeetingsDialog(session, iProperties, new AsyncCallback<List<MeetingInterface>>() {
-			@Override
-			public void onFailure(Throwable caught) {
-				UniTimeNotifications.error(MESSAGES.failedAddMeetings(caught.getMessage()), caught);
-			}
-
-			@Override
-			public void onSuccess(List<MeetingInterface> result) {
-				LoadingWidget.getInstance().show(MESSAGES.waitCheckingRoomAvailability());
-				RPC.execute(EventRoomAvailabilityRpcRequest.checkAvailability(result, getEventId(), getEventType(), iSession.getAcademicSessionId()), new AsyncCallback<EventRoomAvailabilityRpcResponse>() {
-					@Override
-					public void onFailure(Throwable caught) {
-						LoadingWidget.getInstance().hide();
-						UniTimeNotifications.error(MESSAGES.failedRoomAvailability(caught.getMessage()), caught);
-					}
-
-					@Override
-					public void onSuccess(EventRoomAvailabilityRpcResponse result) {
-						LoadingWidget.getInstance().hide();
-						addMeetings(result.getMeetings());
-						iEventAddMeetings.reset(iProperties == null ? null : iProperties.getRoomFilter(), iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
-					}
-				});
-			}
-		});
-		iEventModifyMeetings = new AddMeetingsDialog(session, iProperties, new AsyncCallback<List<MeetingInterface>>() {
-			@Override
-			public void onFailure(Throwable caught) {
-				UniTimeNotifications.error(MESSAGES.failedChangeMeetings(caught.getMessage()), caught);
-			}
-
-			@Override
-			public void onSuccess(List<MeetingInterface> result) {
-				final List<MeetingInterface> meetings = iMeetings.getMeetings();
-				if (!iEventType.isReadOnly())
-					iEvent.setType(getEventType());
-				RPC.execute(EventRoomAvailabilityRpcRequest.checkAvailability(result, getEventId(), getEventType(), iSession.getAcademicSessionId()), new AsyncCallback<EventRoomAvailabilityRpcResponse>() {
-					@Override
-					public void onFailure(Throwable caught) {
-						LoadingWidget.getInstance().hide();
-						UniTimeNotifications.error(MESSAGES.failedRoomAvailability(caught.getMessage()), caught);
-					}
-
-					@Override
-					public void onSuccess(EventRoomAvailabilityRpcResponse result) {
-						LoadingWidget.getInstance().hide();
-						List<MeetingInterface> added = new ArrayList<EventInterface.MeetingInterface>(result.getMeetings());
-						current: for (Iterator<MeetingInterface> i = meetings.iterator(); i.hasNext(); ) {
-							MeetingInterface meeting = i.next();
-							if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue;
-							if (!iSelection.contains(meeting)) continue;
-							for (Iterator<MeetingInterface> j = added.iterator(); j.hasNext();) {
-								MeetingInterface m = j.next();
-								if (m.getDayOfYear() == meeting.getDayOfYear() && EventInterface.equals(meeting.getLocation(), m.getLocation()) && meeting.getStartSlot() == m.getStartSlot() && meeting.getEndSlot() == m.getEndSlot()) {
-									j.remove();
-									continue current;
-								}
-							}
-							if (meeting.getId() == null) {
-								i.remove();
-							} else if (meeting.getApprovalStatus() == ApprovalStatus.Cancelled || meeting.getApprovalStatus() == ApprovalStatus.Deleted) {
-								// already cancelled or deleted
-							} else if (meeting.isCanDelete()) {
-								meeting.setApprovalStatus(ApprovalStatus.Deleted);
-								meeting.setCanApprove(false); meeting.setCanCancel(false); meeting.setCanInquire(false); meeting.setCanEdit(false); meeting.setCanDelete(false);
-							} else if (meeting.isCanCancel()) {
-								meeting.setApprovalStatus(ApprovalStatus.Cancelled);
-								meeting.setCanApprove(false); meeting.setCanCancel(false); meeting.setCanInquire(false); meeting.setCanEdit(false); meeting.setCanDelete(false);
-							}
-						}
-						added: for (MeetingInterface meeting: added) {
-							if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue; 
-							for (MeetingInterface existing: meetings) {
-								if (existing.getApprovalStatus() != ApprovalStatus.Pending && existing.getApprovalStatus() != ApprovalStatus.Approved) continue;
-								if (existing.inConflict(meeting)) {
-									UniTimeNotifications.warn(MESSAGES.warnNewMeetingOverlaps(meeting.toString(), existing.toString()));
-									continue added;
-								}
-							}
-							meetings.add(meeting);
-						}
-						Collections.sort(meetings);
-						
-						boolean hasSelection = false;
-						for (int row = 1; row < iMeetings.getRowCount(); row++) {
-							Widget w =  iMeetings.getWidget(row, 0);
-							if (w != null && w instanceof CheckBox) {
-								CheckBox ch = (CheckBox)w;
-								if (ch.getValue()) { hasSelection = true; break; }
-							}
-						}
-
-						iMeetings.setMeetings(iEvent, meetings);
-						showCreateButtonIfApplicable();
-						ValueChangeEvent.fire(iMeetings, iMeetings.getValue());
-						
-						if (hasSelection)
-							rows: for (int row = 1; row < iMeetings.getRowCount(); row++) {
-								Widget w =  iMeetings.getWidget(row, 0);
-								if (w != null && w instanceof CheckBox) {
-									CheckBox ch = (CheckBox)w;
-									MeetingInterface meeting = iMeetings.getData(row).getMeeting();
-									for (MeetingInterface m: result.getMeetings()) {
-										if (m.getDayOfYear() == meeting.getDayOfYear() && EventInterface.equals(meeting.getLocation(), m.getLocation()) && meeting.getStartSlot() == m.getStartSlot() && meeting.getEndSlot() == m.getEndSlot()) {
-											ch.setValue(true);
-											continue rows;
-										}
-									}
-								}
-							}
-					}
-				});
-			}
-		});
-		iEventModifyMeetings.setText(MESSAGES.dialogModifyMeetings());
-		
 		iMeetingsHeader = new UniTimeHeaderPanel(MESSAGES.sectMeetings());
 		iMeetingsHeader.addButton("add", MESSAGES.buttonAddMeetings(), 100, new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				iEventAddMeetings.showDialog(getEventId(), getConflicts());
+				getEventAddMeetings().showDialog(getEventId(), getConflicts());
 			}
 		});
 		iMeetingsHeader.addButton("operations", MESSAGES.buttonMoreOperations(), 75, new ClickHandler() {
@@ -1453,7 +1337,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			for (StandardEventNoteInterface note: getProperties().getStandardNotes())
 				iStandardNotes.addItem(note.toString(), note.getNote());
 		
-		iEventAddMeetings.reset(iProperties == null ? null : iProperties.getRoomFilter(), iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
+		getEventAddMeetings().reset(iProperties == null ? null : iProperties.getRoomFilter(), iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
 		
 		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), iEventType.getWidget());
 		
@@ -1979,7 +1863,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 	public void execute(EventMeetingTable source, OperationType operation, List<EventMeetingRow> selection) {
 		switch (operation) {
 		case AddMeetings:
-			iEventAddMeetings.showDialog(getEventId(), getConflicts());
+			getEventAddMeetings().showDialog(getEventId(), getConflicts());
 			break;
 		case Delete:
 		case Cancel:
@@ -1995,8 +1879,8 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 				if ((m.getApprovalStatus() == ApprovalStatus.Approved || m.getApprovalStatus() == ApprovalStatus.Pending) && !iSelection.contains(m))
 					conflicts.add(generateConflict(m));
 			}
-			iEventModifyMeetings.reset(iProperties == null ? null : iProperties.getRoomFilter(), iSelection, iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
-			iEventModifyMeetings.showDialog(getEventId(), conflicts);
+			getEventModifyMeetings().reset(iProperties == null ? null : iProperties.getRoomFilter(), iSelection, iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
+			getEventModifyMeetings().showDialog(getEventId(), conflicts);
 			break;
 		}
 	}
@@ -2073,6 +1957,133 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			}
 			return false;
 		}
+	}
+	
+	private AddMeetingsDialog getEventAddMeetings() {
+		if (iEventAddMeetings == null) {
+			iEventAddMeetings = new AddMeetingsDialog(iSession, iProperties, new AsyncCallback<List<MeetingInterface>>() {
+				@Override
+				public void onFailure(Throwable caught) {
+					UniTimeNotifications.error(MESSAGES.failedAddMeetings(caught.getMessage()), caught);
+				}
+
+				@Override
+				public void onSuccess(List<MeetingInterface> result) {
+					LoadingWidget.getInstance().show(MESSAGES.waitCheckingRoomAvailability());
+					RPC.execute(EventRoomAvailabilityRpcRequest.checkAvailability(result, getEventId(), getEventType(), iSession.getAcademicSessionId()), new AsyncCallback<EventRoomAvailabilityRpcResponse>() {
+						@Override
+						public void onFailure(Throwable caught) {
+							LoadingWidget.getInstance().hide();
+							UniTimeNotifications.error(MESSAGES.failedRoomAvailability(caught.getMessage()), caught);
+						}
+
+						@Override
+						public void onSuccess(EventRoomAvailabilityRpcResponse result) {
+							LoadingWidget.getInstance().hide();
+							addMeetings(result.getMeetings());
+							iEventAddMeetings.reset(iProperties == null ? null : iProperties.getRoomFilter(), iProperties == null ? null : iProperties.getSelectedDates(), iProperties == null ? null : iProperties.getSelectedTime());
+						}
+					});
+				}
+			});
+		}
+		return iEventAddMeetings;
+	}
+	
+	private AddMeetingsDialog getEventModifyMeetings() {
+		if (iEventModifyMeetings == null) {
+			iEventModifyMeetings = new AddMeetingsDialog(iSession, iProperties, new AsyncCallback<List<MeetingInterface>>() {
+				@Override
+				public void onFailure(Throwable caught) {
+					UniTimeNotifications.error(MESSAGES.failedChangeMeetings(caught.getMessage()), caught);
+				}
+
+				@Override
+				public void onSuccess(List<MeetingInterface> result) {
+					final List<MeetingInterface> meetings = iMeetings.getMeetings();
+					if (!iEventType.isReadOnly())
+						iEvent.setType(getEventType());
+					RPC.execute(EventRoomAvailabilityRpcRequest.checkAvailability(result, getEventId(), getEventType(), iSession.getAcademicSessionId()), new AsyncCallback<EventRoomAvailabilityRpcResponse>() {
+						@Override
+						public void onFailure(Throwable caught) {
+							LoadingWidget.getInstance().hide();
+							UniTimeNotifications.error(MESSAGES.failedRoomAvailability(caught.getMessage()), caught);
+						}
+
+						@Override
+						public void onSuccess(EventRoomAvailabilityRpcResponse result) {
+							LoadingWidget.getInstance().hide();
+							List<MeetingInterface> added = new ArrayList<EventInterface.MeetingInterface>(result.getMeetings());
+							current: for (Iterator<MeetingInterface> i = meetings.iterator(); i.hasNext(); ) {
+								MeetingInterface meeting = i.next();
+								if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue;
+								if (!iSelection.contains(meeting)) continue;
+								for (Iterator<MeetingInterface> j = added.iterator(); j.hasNext();) {
+									MeetingInterface m = j.next();
+									if (m.getDayOfYear() == meeting.getDayOfYear() && EventInterface.equals(meeting.getLocation(), m.getLocation()) && meeting.getStartSlot() == m.getStartSlot() && meeting.getEndSlot() == m.getEndSlot()) {
+										j.remove();
+										continue current;
+									}
+								}
+								if (meeting.getId() == null) {
+									i.remove();
+								} else if (meeting.getApprovalStatus() == ApprovalStatus.Cancelled || meeting.getApprovalStatus() == ApprovalStatus.Deleted) {
+									// already cancelled or deleted
+								} else if (meeting.isCanDelete()) {
+									meeting.setApprovalStatus(ApprovalStatus.Deleted);
+									meeting.setCanApprove(false); meeting.setCanCancel(false); meeting.setCanInquire(false); meeting.setCanEdit(false); meeting.setCanDelete(false);
+								} else if (meeting.isCanCancel()) {
+									meeting.setApprovalStatus(ApprovalStatus.Cancelled);
+									meeting.setCanApprove(false); meeting.setCanCancel(false); meeting.setCanInquire(false); meeting.setCanEdit(false); meeting.setCanDelete(false);
+								}
+							}
+							added: for (MeetingInterface meeting: added) {
+								if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue; 
+								for (MeetingInterface existing: meetings) {
+									if (existing.getApprovalStatus() != ApprovalStatus.Pending && existing.getApprovalStatus() != ApprovalStatus.Approved) continue;
+									if (existing.inConflict(meeting)) {
+										UniTimeNotifications.warn(MESSAGES.warnNewMeetingOverlaps(meeting.toString(), existing.toString()));
+										continue added;
+									}
+								}
+								meetings.add(meeting);
+							}
+							Collections.sort(meetings);
+							
+							boolean hasSelection = false;
+							for (int row = 1; row < iMeetings.getRowCount(); row++) {
+								Widget w =  iMeetings.getWidget(row, 0);
+								if (w != null && w instanceof CheckBox) {
+									CheckBox ch = (CheckBox)w;
+									if (ch.getValue()) { hasSelection = true; break; }
+								}
+							}
+
+							iMeetings.setMeetings(iEvent, meetings);
+							showCreateButtonIfApplicable();
+							ValueChangeEvent.fire(iMeetings, iMeetings.getValue());
+							
+							if (hasSelection)
+								rows: for (int row = 1; row < iMeetings.getRowCount(); row++) {
+									Widget w =  iMeetings.getWidget(row, 0);
+									if (w != null && w instanceof CheckBox) {
+										CheckBox ch = (CheckBox)w;
+										MeetingInterface meeting = iMeetings.getData(row).getMeeting();
+										for (MeetingInterface m: result.getMeetings()) {
+											if (m.getDayOfYear() == meeting.getDayOfYear() && EventInterface.equals(meeting.getLocation(), m.getLocation()) && meeting.getStartSlot() == m.getStartSlot() && meeting.getEndSlot() == m.getEndSlot()) {
+												ch.setValue(true);
+												continue rows;
+											}
+										}
+									}
+								}
+						}
+					});
+				}
+			});
+			iEventModifyMeetings.setText(MESSAGES.dialogModifyMeetings());			
+		}
+		return iEventModifyMeetings;
 	}
 
 }

JavaSource/org/unitime/timetable/gwt/client/events/EventFilterBox.java
@@ -92,7 +92,7 @@ public class EventFilterBox extends UniTimeFilterBox<EventFilterRpcRequest> {
 	private ListBox iServices;
 	
 	public EventFilterBox(AcademicSessionProvider session) {
-		super(session);
+		super(session, false);
 		
 		addFilter(new FilterBox.StaticSimpleFilter("type", MESSAGES.tagEventType()) {
 			@Override
@@ -728,6 +728,11 @@ public class EventFilterBox extends UniTimeFilterBox<EventFilterRpcRequest> {
 				setAriaLabel(ARIA.eventFilter(toAriaString()));
 			}
 		});
+	}
+	
+	@Override
+	public void setValue(String value, boolean fireEvents) {
+		iFilter.getWidget().setValue(value, fireEvents);
 	}
 	
 	public void setOtherVisible(boolean visible) { iOther.setVisible(visible); }

commit b8afb0765482a2e4f632fb83507a9568502c828c

Rooms, Student Dashboards, Teaching Requests: Filter
- ensure that the type-dependent filters show up after the initial load of the solver
  - these are room types for rooms, student group types for student dashboards, and attribute types for teaching requests
  - the issue caused by commit cb8d1d9 that eliminated the second initialization call

JavaSource/org/unitime/timetable/gwt/client/events/RoomFilterBox.java
@@ -472,7 +472,9 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 			for (FilterBox.Filter filter: iFilter.getWidget().getFilters()) {
 				if (filter.getCommand().equals(type)) continue types;
 			}
-			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 5, new FilterBox.StaticSimpleFilter(type, null));
+			FilterBox.StaticSimpleFilter filter = new FilterBox.StaticSimpleFilter(type, null);
+			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 5, filter);
+			populateFilter(filter, result.getEntities(type));
 			added = true;
 		}
 		if (added) setValue(getValue(), false);

JavaSource/org/unitime/timetable/gwt/client/instructor/TeachingRequestsFilterBox.java
@@ -351,7 +351,9 @@ public class TeachingRequestsFilterBox extends UniTimeFilterBox<TeachingRequests
 			for (FilterBox.Filter filter: iFilter.getWidget().getFilters()) {
 				if (filter.getCommand().equals(type)) continue types;
 			}
-			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 1, new FilterBox.StaticSimpleFilter(type, null));
+			FilterBox.StaticSimpleFilter filter = new FilterBox.StaticSimpleFilter(type, null);
+			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 1, filter);
+			populateFilter(filter, result.getEntities(type));
 			added = true;
 		}
 		if (added) setValue(getValue(), false);

JavaSource/org/unitime/timetable/gwt/client/sectioning/SectioningStatusFilterBox.java
@@ -762,10 +762,13 @@ public class SectioningStatusFilterBox extends UniTimeFilterBox<SectioningStatus
 			for (FilterBox.Filter filter: iFilter.getWidget().getFilters()) {
 				if (filter.getCommand().equals(type)) continue types;
 			}
-			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 10, new FilterBox.StaticSimpleFilter(type, result.getTypeLabel(type)));
+			FilterBox.StaticSimpleFilter filter = new FilterBox.StaticSimpleFilter(type, result.getTypeLabel(type));
+			iFilter.getWidget().getFilters().add(iFilter.getWidget().getFilters().size() - 10, filter);
+			populateFilter(filter, result.getEntities(type));
 			added = true;
 		}
-		if (added) setValue(getValue(), false);
+		if (added)
+			setValue(getValue(), false);
 	}
 
 }