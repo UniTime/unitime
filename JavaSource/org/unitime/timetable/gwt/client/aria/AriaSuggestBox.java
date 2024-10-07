UniTime/unitime

commit 0c363e9903deac196d5ff9aa98fe64a86a95822d

Edit Class/Scheduling Subpart
- added an ability to search the list of available date patterns
- enable by setting unitime.classEdit.searchableDatePattern to true (defautls to false)
- when enabled, the Date Pattern list box is replaced by a text field with suggestions

JavaSource/org/unitime/timetable/gwt/client/aria/AriaSuggestBox.java
@@ -123,11 +123,14 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 				} else {
 					iSuggestionMenu.clearItems();
 					SuggestOracle.Suggestion first = null;
+					int index = -1;
 					for (SuggestOracle.Suggestion suggestion: response.getSuggestions()) {
 						iSuggestionMenu.addItem(new SuggestionMenuItem(suggestion));
 						if (first == null) first = suggestion;
+						if (index < 0 && suggestion.getReplacementString().equals(iText.getValue()))
+							index = iSuggestionMenu.getNumItems() - 1;
 					}
-					iSuggestionMenu.selectItem(0);
+					iSuggestionMenu.selectItem(index < 0 ? 0 : index);
 					ToolBox.setMinWidth(iSuggestionMenu.getElement().getStyle(), (iText.getElement().getClientWidth() - 4) + "px");
 					iSuggestionPopup.showRelativeTo(iText);
 					iSuggestionMenu.scrollToView();
@@ -178,6 +181,7 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 	          case KeyCodes.KEY_ENTER:
 	        	  if (isSuggestionListShowing())
 	        		  iSuggestionMenu.executeSelected();
+	        	  event.preventDefault();
 	        	  break;
 	          case KeyCodes.KEY_TAB:
 	        	  if (isSuggestionListShowing()) {
@@ -461,4 +465,6 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 	public void setAriaLabel(String text) {
 		iText.setAriaLabel(text);
 	}
+	
+	public AriaTextBox getTextBox() { return iText; }
 }

JavaSource/org/unitime/timetable/gwt/client/widgets/SearchableListBox.java
@@ -1,0 +1,174 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+package org.unitime.timetable.gwt.client.widgets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.unitime.timetable.gwt.client.ToolBox;
+import org.unitime.timetable.gwt.client.aria.AriaSuggestBox;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SuggestOracle;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
+
+/**
+ * @author Tomas Muller
+ */
+public class SearchableListBox extends Composite {
+	private ListBox iListBox;
+	private AriaSuggestBox iSuggestBox;
+	
+	public SearchableListBox() {
+		iSuggestBox = new AriaSuggestBox(new SearchableListBoxOracle());
+		initWidget(iSuggestBox);
+		iSuggestBox.addSelectionHandler(new SelectionHandler<SuggestOracle.Suggestion>() {
+			@Override
+			public void onSelection(SelectionEvent<Suggestion> event) {
+				if (event.getSelectedItem() != null)
+					iListBox.setSelectedIndex(((ListItem)event.getSelectedItem()).getIndex());
+				else
+					iListBox.setSelectedIndex(0);
+				ChangeEvent.fireNativeEvent(Document.get().createChangeEvent(), iListBox);
+			}
+		});
+		iSuggestBox.getTextBox().addFocusHandler(new FocusHandler() {
+			@Override
+			public void onFocus(FocusEvent event) {
+				iSuggestBox.showSuggestionList();
+			}
+		});
+		iSuggestBox.getTextBox().addBlurHandler(new BlurHandler() {
+			@Override
+			public void onBlur(BlurEvent event) {
+				iSuggestBox.setText(iListBox.getSelectedItemText());
+			}
+		});
+	}
+	
+	public SearchableListBox(ListBox box) {
+		this();
+		setListBox(box);
+		iSuggestBox.getElement().getStyle().clearWidth();
+	}
+	
+	public void setListBox(ListBox box) {
+		iListBox = box;
+		iSuggestBox.getElement().getStyle().setWidth(iListBox.getOffsetWidth(), Unit.PX);
+		iListBox.setVisible(false);
+		iListBox.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				iSuggestBox.setValue(iListBox.getSelectedItemText());
+			}
+		});
+		iSuggestBox.setValue(iListBox.getSelectedItemText());
+	}
+	
+	ListItem item(int index) {
+		if (index < 0 || index > iListBox.getItemCount()) return null;
+		return new ListItem(index, iListBox.getValue(index), iListBox.getItemText(index));
+	}
+	
+	public void insert(final RootPanel panel) {
+		setListBox(ListBox.wrap(panel.getElement().getFirstChildElement()));
+		panel.add(this);
+		final String onchange = panel.getElement().getFirstChildElement().getAttribute("onchange");
+		if (onchange != null)
+			iListBox.addChangeHandler(new ChangeHandler() {
+				@Override
+				public void onChange(ChangeEvent event) {
+					ToolBox.eval(onchange);
+				}
+			});
+	}
+	
+	public static class ListItem implements IsSerializable, Comparable<ListItem>, Suggestion {
+		private String iValue, iText;
+		private int iIndex;
+		public ListItem() {}
+		public ListItem(int index, String value, String text) {
+			iIndex = index;
+			iValue = value; iText = text;
+		}
+		public int getIndex() { return iIndex; }
+		public String getValue() { return iValue; }
+		public String getText() { return iText; }
+		@Override
+		public String toString() { return iText; }
+		@Override
+		public int compareTo(ListItem o) {
+			return getText().compareTo(o.getText());
+		}
+		@Override
+		public int hashCode() { return getText().hashCode(); }
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || !(o instanceof ListItem)) return false;
+			ListItem i = (ListItem)o;
+			return getValue().equals(i.getValue());
+		}
+		@Override
+		public String getDisplayString() { return getText(); }
+		@Override
+		public String getReplacementString() { return getText(); }
+	}
+	
+	class SearchableListBoxOracle extends SuggestOracle {
+		@Override
+		public void requestSuggestions(Request request, Callback callback) {
+			if (iListBox.getSelectedItemText().equals(request.getQuery())) {
+				requestDefaultSuggestions(request, callback);
+				return;
+			}
+			List<ListItem> items = new ArrayList<ListItem>();
+			for (int i = 0; i < iListBox.getItemCount(); i++) {
+				if (iListBox.getItemText(i).toLowerCase().contains(request.getQuery().toLowerCase())) {
+					items.add(item(i));
+				}
+			}
+			Response response = new Response(items);
+			callback.onSuggestionsReady(request, response);
+		}
+		
+		@Override
+		public void requestDefaultSuggestions(Request request, Callback callback) {
+			List<ListItem> items = new ArrayList<ListItem>();
+			for (int i = 0; i < iListBox.getItemCount(); i++) {
+				items.add(item(i));
+			}
+			Response response = new Response(items);
+			callback.onSuggestionsReady(request, response);
+		}
+	}
+}

JavaSource/org/unitime/timetable/defaults/ApplicationProperty.java
@@ -3588,6 +3588,11 @@ public enum ApplicationProperty {
 			+ "Example: https://www.university.edu/inventory?location=:name")
 	@Since(4.8)
 	DefaultRoomUrlLoncation("unitime.rooms.url.location"),
+	
+	@Type(Boolean.class)
+	@DefaultValue("false")
+	@Description("Edit Class/Scheduling Subpart: enable searcheable date pattern")
+	ClassEditSearcheableDatePattern("unitime.classEdit.searchableDatePattern"),
 	;
 
 	String iKey;

JavaSource/org/unitime/timetable/gwt/client/Components.java
@@ -47,6 +47,7 @@ import org.unitime.timetable.gwt.client.sectioning.ExaminationEnrollmentTable;
 import org.unitime.timetable.gwt.client.sectioning.StudentScheduleTable;
 import org.unitime.timetable.gwt.client.solver.SolverAllocatedMemory;
 import org.unitime.timetable.gwt.client.widgets.CourseNumbersSuggestBox;
+import org.unitime.timetable.gwt.client.widgets.SearchableListBox;
 
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -91,6 +92,7 @@ public enum Components {
 	instructorSurveyOffering("UniTimeGWT:InstructorSurveyOffering", true, new ComponentFactory() { public void insert(RootPanel panel) { new OfferingDetailWidget().insert(panel); } }),
 	multipleClassSetup("UniTimeGWT:MultipleClassSetupButton", true, new ComponentFactory() { public void insert(RootPanel panel) { new MultipleClassSetupButton(true).insert(panel); } }),
 	instrOfferingConfig("UniTimeGWT:InstrOfferingConfigButton", true, new ComponentFactory() { public void insert(RootPanel panel) { new InstrOfferingConfigButton(true).insert(panel); } }),
+	searchableListBox("UniTimeGWT:SearchableListBox", true, new ComponentFactory() { public void insert(RootPanel panel) { new SearchableListBox().insert(panel); } }),
 	;
 	
 	private String iId;

WebContent/help/Release-Notes.xml
@@ -47,6 +47,14 @@
 				</line>
 			</description>
 		</item>
+		<item>
+			<name>Edit Class/Scheduling Subpart</name>
+			<description>
+				<line>Added an ability to search the list of available date patterns.</line>
+				<line>Enable by setting unitime.classEdit.searchableDatePattern to true (defautls to false).</line>
+				<line>When enabled, the Date Pattern list box is replaced by a text field with suggestions.</line>
+			</description>
+		</item>
 	</category>
 	<category>
 		<title>Student Scheduling</title>

WebContent/user/classEdit.jsp
@@ -224,8 +224,15 @@
 			<TD>
 				<s:hidden name="form.datePatternEditable"/>
 				<s:if test="form.datePatternEditable == true">
+					<tt:propertyEquals name="unitime.classEdit.searchableDatePattern" value="true">
+					<span id="UniTimeGWT:SearchableListBox">
+					<s:select name="form.datePattern" list="#request.datePatternList" listKey="id" listValue="value"
+						style="min-width:200px;" onchange="var op2Obj = $doc.getElementById('op2'); if (op2Obj!=null) { op2Obj.value='updateDatePattern'; $doc.forms[0].submit(); };"/>
+					</span>
+					</tt:propertyEquals><tt:propertyNotEquals name="unitime.classEdit.searchableDatePattern" value="true">
 					<s:select name="form.datePattern" list="#request.datePatternList" listKey="id" listValue="value"
 						style="min-width:200px;" onchange="datePatternChanged();"/>
+					</tt:propertyNotEquals>
 					<img style="cursor: pointer;" src="images/calendar.png" border="0" onclick="showGwtDialog('Preview of '+classEdit_form_datePattern.options[classEdit_form_datePattern.selectedIndex].text, 'dispDatePattern.action?id='+classEdit_form_datePattern.value+'&classId='+classEdit_form_classId.value,'840','520');">
 				</s:if>
 				<s:else>

WebContent/user/schedulingSubpartEdit.jsp
@@ -133,8 +133,15 @@
 				<s:hidden name="op2" value="" id="op2"/>
 				<s:hidden name="form.datePatternEditable"/>
 				<s:if test="form.datePatternEditable == true">
+					<tt:propertyEquals name="unitime.classEdit.searchableDatePattern" value="true">
+					<span id="UniTimeGWT:SearchableListBox">
+					<s:select name="form.datePattern" list="#request.datePatternList" listKey="id" listValue="value"
+						style="min-width:200px;" onchange="var op2Obj = $doc.getElementById('op2'); if (op2Obj!=null) { op2Obj.value='updateDatePattern'; $doc.forms[0].submit(); };"/>
+					</span>
+					</tt:propertyEquals><tt:propertyNotEquals name="unitime.classEdit.searchableDatePattern" value="true">
 					<s:select name="form.datePattern" list="#request.datePatternList" listKey="id" listValue="value"
 						style="min-width:200px;" onchange="datePatternChanged();"/>
+					</tt:propertyNotEquals>
 					<img style="cursor: pointer;" src="images/calendar.png" border="0" onclick="showGwtDialog('Preview of '+schedulingSubpartEdit_form_datePattern.options[schedulingSubpartEdit_form_datePattern.selectedIndex].text, 'dispDatePattern.action?id='+schedulingSubpartEdit_form_datePattern.value+'&subpartId='+schedulingSubpartEdit_form_schedulingSubpartId.value,'840','520');">
 				</s:if>
 				<s:else>

commit e400a4438dc770c115003a44fda1a9c853d114c7

GWT: AriaSuggestBox
- only prevent default handling of an Enter keypress when explicitly indicated
- this fixes an issue of the Enter pressed on the course number selection not submitting the form on the Instructional Offerings and Classes pages
  (caused by commit 0c363e9)

JavaSource/org/unitime/timetable/gwt/client/aria/AriaSuggestBox.java
@@ -78,6 +78,7 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 	private SuggestOracle.Callback iOracleCallback;
 	
 	private String iCurrentText = null;
+	private boolean iEnterPreventDefault = false;
 	
 	public AriaSuggestBox(SuggestOracle oracle) {
 		this(new AriaTextBox(), oracle);
@@ -181,7 +182,8 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 	          case KeyCodes.KEY_ENTER:
 	        	  if (isSuggestionListShowing())
 	        		  iSuggestionMenu.executeSelected();
-	        	  event.preventDefault();
+	        	  if (iEnterPreventDefault)
+	        		  event.preventDefault();
 	        	  break;
 	          case KeyCodes.KEY_TAB:
 	        	  if (isSuggestionListShowing()) {
@@ -465,6 +467,8 @@ public class AriaSuggestBox extends Composite implements HasText, HasValue<Strin
 	public void setAriaLabel(String text) {
 		iText.setAriaLabel(text);
 	}
+	
+	public void setEnterPreventDefault(boolean enterPreventDefault) { iEnterPreventDefault = enterPreventDefault; }
 	
 	public AriaTextBox getTextBox() { return iText; }
 }

JavaSource/org/unitime/timetable/gwt/client/widgets/SearchableListBox.java
@@ -51,6 +51,7 @@ public class SearchableListBox extends Composite {
 	
 	public SearchableListBox() {
 		iSuggestBox = new AriaSuggestBox(new SearchableListBoxOracle());
+		iSuggestBox.setEnterPreventDefault(true);
 		initWidget(iSuggestBox);
 		iSuggestBox.addSelectionHandler(new SelectionHandler<SuggestOracle.Suggestion>() {
 			@Override