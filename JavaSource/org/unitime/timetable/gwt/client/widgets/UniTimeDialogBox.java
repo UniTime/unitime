44ca45f7d64e4c32f4fe0aea2a34359a9838b487
tomas-muller <muller@unitime.org>
2025-05-13 17:49:03 +0200

    GWT: Dialog Box
    - addded an ability to maximize the size of the dialog (with a new button next to the close dialog)
    - enabled for the farme dialog (e.g., when showing Suggestions or Instructor Survey)

--- JavaSource/org/unitime/timetable/gwt/client/widgets/UniTimeDialogBox.java
+++ JavaSource/org/unitime/timetable/gwt/client/widgets/UniTimeDialogBox.java
@@ -19,12 +19,17 @@
 */
 package org.unitime.timetable.gwt.client.widgets;
 
+import org.unitime.timetable.gwt.client.Components;
+import org.unitime.timetable.gwt.client.ToolBox;
 import org.unitime.timetable.gwt.client.aria.AriaDialogBox;
 import org.unitime.timetable.gwt.client.aria.AriaStatus;
 import org.unitime.timetable.gwt.resources.GwtAriaMessages;
 import org.unitime.timetable.gwt.resources.GwtMessages;
 
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -34,9 +39,11 @@ import com.google.gwt.event.logical.shared.OpenHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -46,11 +53,13 @@ public class UniTimeDialogBox extends AriaDialogBox implements HasOpenHandlers<U
 	protected static final GwtMessages MESSAGES = GWT.create(GwtMessages.class);
 	protected static GwtAriaMessages ARIA = GWT.create(GwtAriaMessages.class);
 	private FlowPanel iContainer, iControls;
-	private Anchor iClose;
+	private Anchor iClose, iMaximize;
 	private boolean iEscapeToHide = false;
 	private Command iSubmitHandler = null;
-	    
-    public UniTimeDialogBox(boolean autoHide, boolean modal) {
+	private String iWidth = null, iHeight = null;
+	private boolean iMaximized = false;
+	
+	public UniTimeDialogBox(boolean autoHide, boolean modal) {
         super(autoHide, modal);
         
 		setAnimationEnabled(true);
@@ -59,6 +68,17 @@ public class UniTimeDialogBox extends AriaDialogBox implements HasOpenHandlers<U
         iContainer = new FlowPanel();
         iContainer.addStyleName("dialogContainer");
         
+        iMaximize = new Anchor();
+        iMaximize.setTitle(MESSAGES.hintMaximizeDialog());
+        iMaximize.setStyleName("maximize");
+        iMaximize.addClickHandler(new ClickHandler() {
+        	@Override
+            public void onClick(ClickEvent event) {
+        		onMaximizeClick(event);
+            }
+        });
+        iMaximize.setVisible(false);
+
         iClose = new Anchor();
     	iClose.setTitle(MESSAGES.hintCloseDialog());
         iClose.setStyleName("close");
@@ -69,11 +89,20 @@ public class UniTimeDialogBox extends AriaDialogBox implements HasOpenHandlers<U
             }
         });
         iClose.setVisible(autoHide);
-
+        
         iControls = new FlowPanel();
         iControls.setStyleName("dialogControls");        
         iControls.add(iClose);
+        iControls.add(iMaximize);
     }
+	
+	public void setMaximizeEnabled(boolean enabled) {
+		iMaximize.setVisible(enabled);
+	}
+	
+	public boolean isMaximizeEnabled() {
+		return iMaximize.isVisible();
+	}
     
     @Override
     public void center() {
@@ -107,6 +136,45 @@ public class UniTimeDialogBox extends AriaDialogBox implements HasOpenHandlers<U
     protected void onCloseClick(ClickEvent event) {
         hide();
     }
+    
+    protected boolean isHasMenu() {
+    	return (RootPanel.get(Components.menubar_static.id()) != null || RootPanel.get(Components.menubar_dynamic.id()) != null);
+    }
+    
+    protected void maximize() {
+		boolean hasMenu = isHasMenu();
+    	getElement().getStyle().setLeft(Window.getScrollLeft(), Unit.PX);
+    	getElement().getStyle().setTop(Window.getScrollTop() + (hasMenu ? 22 : 0), Unit.PX);
+    	iContainer.getWidget(1).setWidth("calc(100vw - 15px)");
+    	iContainer.getWidget(1).setHeight(hasMenu ? "calc(100vh - 72px)" : "calc(100vh - 50px)");
+    	iMaximize.setTitle(MESSAGES.hintDemaximizeDialog());
+    	ToolBox.setSessionCookie("UniTimeDialogBox.Maximize", "1");
+    	iMaximized = true;
+    }
+    
+    protected void minimize() {
+    	if (iWidth != null)
+			iContainer.getWidget(1).getElement().getStyle().setProperty("width", iWidth);
+		else
+			iContainer.getWidget(1).getElement().getStyle().clearWidth();
+		if (iHeight != null)
+			iContainer.getWidget(1).getElement().getStyle().setProperty("height", iHeight);
+		else
+			iContainer.getWidget(1).getElement().getStyle().clearHeight();
+		
+		center();
+		iMaximize.setTitle(MESSAGES.hintMaximizeDialog());
+		ToolBox.setSessionCookie("UniTimeDialogBox.Maximize", "0");
+		iMaximized = false;
+    }
+    
+    protected void onMaximizeClick(ClickEvent event) {
+    	if (!iMaximized) {
+    		maximize();
+    	} else {
+    		minimize();
+    	}
+    }
 
     public HandlerRegistration addOpenHandler(OpenHandler<UniTimeDialogBox> handler) {
         return addHandler(handler, OpenEvent.getType());
@@ -115,7 +183,23 @@ public class UniTimeDialogBox extends AriaDialogBox implements HasOpenHandlers<U
     @Override
     public void show() {
         boolean fireOpen = !isShowing();
-        super.show();
+        if (iMaximize != null && iMaximize.isVisible()) {
+        	if (!iMaximized) {
+        		iHeight = iContainer.getWidget(1).getElement().getStyle().getProperty("height");
+        		iWidth = iContainer.getWidget(1).getElement().getStyle().getProperty("width");
+        	}
+        	iMaximized = "1".equals(ToolBox.getSessionCookie("UniTimeDialogBox.Maximize"));
+        	super.show();
+        	if (iMaximized)
+        		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+        			@Override
+    				public void execute() {
+    					maximize();
+    				}
+    			});
+        } else {
+        	super.show();
+        }
         if (fireOpen) {
             OpenEvent.fire(this, this);
         }
