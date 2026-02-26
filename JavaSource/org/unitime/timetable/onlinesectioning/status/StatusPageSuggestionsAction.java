698c37e8117357d5f63c23e77fdf5ddac875c3e9
tomas-muller <muller@unitime.org>
2026-02-26 15:35:42 +0100

    Student Scheduling Dashboard
    - added ability to filter by free-time requests, e.g., use
      ft:<=3
    - to filter for students with a free time request among the first three priorities

--- JavaSource/org/unitime/timetable/onlinesectioning/status/StatusPageSuggestionsAction.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/status/StatusPageSuggestionsAction.java
@@ -81,6 +81,7 @@ import org.unitime.timetable.onlinesectioning.model.XCourse;
 import org.unitime.timetable.onlinesectioning.model.XCourseId;
 import org.unitime.timetable.onlinesectioning.model.XCourseRequest;
 import org.unitime.timetable.onlinesectioning.model.XEnrollment;
+import org.unitime.timetable.onlinesectioning.model.XFreeTimeRequest;
 import org.unitime.timetable.onlinesectioning.model.XInstructor;
 import org.unitime.timetable.onlinesectioning.model.XOffering;
 import org.unitime.timetable.onlinesectioning.model.XOverride;
@@ -1085,6 +1086,38 @@ public class StatusPageSuggestionsAction implements OnlineSectioningAction<List<
 					studentMaxTot += maxsTot.get(maxsTot.size() - i - 1);
 				}
 				return min <= studentMaxTot && studentMinTot <= max;
+			}
+			
+			if ("ft".equals(attr) || "free-time".equals(attr)) {
+				int min = 0, max = Integer.MAX_VALUE;
+				Credit prefix = Credit.eq;
+				String number = term;
+				if (number.startsWith("<=")) { prefix = Credit.le; number = number.substring(2); }
+				else if (number.startsWith(">=")) { prefix =Credit.ge; number = number.substring(2); }
+				else if (number.startsWith("<")) { prefix = Credit.lt; number = number.substring(1); }
+				else if (number.startsWith(">")) { prefix = Credit.gt; number = number.substring(1); }
+				else if (number.startsWith("=")) { prefix = Credit.eq; number = number.substring(1); }
+				try {
+					int a = Integer.parseInt(number);
+					switch (prefix) {
+						case eq: min = max = a; break; // = a
+						case le: max = a; break; // <= a
+						case ge: min = a; break; // >= a
+						case lt: max = a - 1; break; // < a
+						case gt: min = a + 1; break; // > a
+					}
+				} catch (NumberFormatException e) {}
+				if (term.contains("..")) {
+					try {
+						String a = term.substring(0, term.indexOf('.'));
+						String b = term.substring(term.indexOf("..") + 2);
+						min = Integer.parseInt(a); max = Integer.parseInt(b);
+					} catch (NumberFormatException e) {}
+				}
+				if (min == 0 && max == Integer.MAX_VALUE) return true;
+				for (XRequest r: student().getRequests())
+					if (r instanceof XFreeTimeRequest && min <= r.getPriority() + 1 && r.getPriority() + 1 <= max) return true;
+				return false;
 			}
 			
 			if ("fc".equals(attr) || "first-choice-credit".equals(attr)) {
