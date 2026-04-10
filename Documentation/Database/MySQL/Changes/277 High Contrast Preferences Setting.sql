3b98c809345c14c43b55d2d06fd3b2ef50437f5c
tomas-muller <muller@unitime.org>
2026-04-10 14:28:23 +0200

    WCAG: High Contrast Preferences Setting
    - register the new highContrastPreferences setting that can be used to switch between how preferences are being colored
      - yes: (which is the default now) will use preference colors as backgrounds
      - no: will use the new (much darker) preference colors that satisfy the 4.5:1 contrast ratio
      - legacy: will use the old non-ADA compliant colors

---
+++ Documentation/Database/MySQL/Changes/277 High Contrast Preferences Setting.sql
@@ -0,0 +1,35 @@
+/*
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * The Apereo Foundation licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+select 32767 * next_hi into @id from hibernate_unique_key;
+
+insert into settings 
+	(uniqueid, name, default_value, allowed_values, description) values
+	(@id + 0, 'highContrastPreferences', 'yes', 'yes,no,legacy', 'High Contrast Preferences: Use preference levels with color backgrounds.');
+
+update hibernate_unique_key set next_hi=next_hi+1;
+
+/*
+ * Update database version
+ */
+  
+update application_config set value='277' where name='tmtbl.db.version';
+
+commit;
