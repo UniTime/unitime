UniTime/unitime

commit 0d797d97c75557bfb1d48a979c64a3a7f5a05f10

WCAG: Events
- various changes to improve WCAG compliance of Events pages

JavaSource/org/unitime/timetable/gwt/client/events/EventResourceTimetable.java
@@ -52,6 +52,7 @@ import org.unitime.timetable.gwt.client.widgets.SimpleForm;
 import org.unitime.timetable.gwt.client.widgets.ServerDateTimeFormat;
 import org.unitime.timetable.gwt.client.widgets.TimeSelector.TimeUtils;
 import org.unitime.timetable.gwt.client.widgets.UniTimeDialogBox;
+import org.unitime.timetable.gwt.client.widgets.UniTimeDockPanel;
 import org.unitime.timetable.gwt.client.widgets.UniTimeHeaderPanel;
 import org.unitime.timetable.gwt.client.widgets.UniTimeWidget;
 import org.unitime.timetable.gwt.client.widgets.FilterBox.Chip;
@@ -433,6 +434,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 		iHeader = new UniTimeHeaderPanel();
 		iPanel.addHeaderRow(iHeader);
 		iWeekPanel = new WeekSelector(iSession);
+		iWeekPanel.setAriaLabel(ARIA.inputWeekSelector());
 		iWeekPanel.addValueChangeHandler(new ValueChangeHandler<WeekSelector.Interval>() {
 			@Override
 			public void onValueChange(ValueChangeEvent<WeekSelector.Interval> e) {
@@ -451,6 +453,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 			}
 		});
 		iRoomPanel = new RoomSelector();
+		iRoomPanel.setAriaLabel(ARIA.inputRoomSelector());
 		iRoomPanel.setFilter(new WeekSelector.Filter<ResourceInterface>() {
 			@Override
 			public boolean filter(ResourceInterface room) {
@@ -505,7 +508,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 			}, KeyUpEvent.getType());
 		}
 		
-		iDockPanel = new DockPanel();
+		iDockPanel = new UniTimeDockPanel();
 		iDockPanel.setStyleName("unitime-EventResults");
 		iDockPanel.setSpacing(0);
 		iDockPanel.add(iGridOrTablePanel, DockPanel.SOUTH);
@@ -917,6 +920,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 				if (event.getType() == EventType.Unavailabile && !iEvents.hasChip(new Chip("type", "Not Available"))) continue;
 				for (MeetingInterface meeting: event.getMeetings()) {
 					if (filterEvent(event, meeting)) continue;
+					if (meeting.getDayOfWeek() == 0) continue;
 					if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue;
 					if (firstSlot > meeting.getStartSlot()) firstSlot = meeting.getStartSlot();
 					if (lastSlot < meeting.getEndSlot()) lastSlot = meeting.getEndSlot();
@@ -1485,6 +1489,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 				ta.setCharacterWidth(80);
 				ta.setText(GWT.getHostPageBaseURL() + (result.hasHash() ? "export?x=" + result.getHash() : "export?q=" + result.getQuery()));
 				UniTimeWidget<TextArea> w = new UniTimeWidget<TextArea>(ta);
+				Roles.getTextboxRole().setAriaLabelProperty(ta.getElement(), MESSAGES.opExportICalendar());
 				w.setHint(MESSAGES.hintCtrlCToCopy());
 				form.addRow(w);
 				w.getElement().getStyle().setMarginBottom(5, Unit.PX);

JavaSource/org/unitime/timetable/gwt/client/Lookup.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 import org.unitime.timetable.gwt.client.aria.AriaStatus;
 import org.unitime.timetable.gwt.client.aria.AriaTextBox;
+import org.unitime.timetable.gwt.client.widgets.FocusableScrollPanel;
 import org.unitime.timetable.gwt.client.widgets.UniTimeDialogBox;
 import org.unitime.timetable.gwt.client.widgets.UniTimeTable;
 import org.unitime.timetable.gwt.client.widgets.UniTimeTableHeader;
@@ -111,7 +112,7 @@ public class Lookup extends UniTimeDialogBox implements HasValue<PersonInterface
 		iPanel.add(queryPanel);
 		iPanel.setCellHorizontalAlignment(queryPanel, HasHorizontalAlignment.ALIGN_CENTER);
 		iTable = new UniTimeTable<PersonInterface>();
-		iScroll = new ScrollPanel(iTable);
+		iScroll = new FocusableScrollPanel(iTable);
 		iScroll.setStyleName("unitime-ScrollPanel");
 		iPanel.add(iScroll);
 		setWidget(iPanel);

JavaSource/org/unitime/timetable/gwt/client/ToolBox.java
@@ -406,12 +406,14 @@ public class ToolBox {
 	}
 	
 	public static Element firstInputElement(Element e) {
+		if ("input".equalsIgnoreCase(e.getTagName()) || "select".equalsIgnoreCase(e.getTagName()) || "textarea".equalsIgnoreCase(e.getTagName()))
+			return e;
 		NodeList<Element> inputs = e.getElementsByTagName("input");
 		if (inputs != null && inputs.getLength() > 0) return inputs.getItem(0);
 		inputs = e.getElementsByTagName("select");
 		if (inputs != null && inputs.getLength() > 0) return inputs.getItem(0);
 		inputs = e.getElementsByTagName("textarea");
 		if (inputs != null && inputs.getLength() > 0) return inputs.getItem(0);
-		return e;
+		return null;
 	}
 }

JavaSource/org/unitime/timetable/gwt/client/events/AddMeetingsDialog.java
@@ -851,14 +851,17 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		iPanels.clear();
 		
 		P box = new P("box"); iRoomAvailability.add(box);
+		Roles.getGridRole().set(box.getElement());
 		
 		P row = new P("row"); box.add(row);
-		
+		Roles.getRowRole().set(row.getElement());
+				
 		row.add(new P("corner"));
 		
 		for (int i = iIndex; i < iIndex + iStep && i < getRooms().size(); i++) {
 			final Entity room = getRooms().get(i);
 			final P p = new P("room");
+			Roles.getColumnheaderRole().set(p.getElement());
 			String displayName = room.getProperty("display", null);
 			p.setHTML(MESSAGES.singleRoomSelection(room.getName() + (displayName != null && !displayName.isEmpty() ? "<br>" + displayName : ""), room.getProperty("type", null), room.getProperty("capacity", null)));
 			if ("1".equals(room.getProperty("ignoreRoomCheck", "0")))
@@ -906,8 +909,10 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		
 		for (final Integer date: getDates()) {
 			row = new P("row"); box.add(row);
+			Roles.getRowRole().set(row.getElement());
 			
 			final P day = new P("date");
+			Roles.getRowheaderRole().set(day.getElement());
 			final Date d = iDates.getDate(date);
 			day.setHTML(MESSAGES.dateTimeHeader(sDayOfWeek.format(d), sDateFormat.format(d), TimeUtils.slot2short(getStartSlot()), TimeUtils.slot2short(getEndSlot())));
 			row.add(day);
@@ -955,6 +960,7 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 				final Set<MeetingConflictInterface> conflicts = getConflicts(date, room);
 				
 				final P p = new P("cell");
+				Roles.getGridcellRole().set(p.getElement());
 				
 				if (conflicts == null || conflicts.isEmpty()) {
 					p.addStyleName("free");
@@ -1032,14 +1038,17 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		iPanels.clear();
 		
 		P box = new P("box"); iRoomAvailability.add(box);
+		Roles.getGridRole().set(box.getElement());
 		
 		P row = new P("row"); box.add(row);
+		Roles.getRowRole().set(row.getElement());
 		
 		row.add(new P("corner"));
 		
 		for (int i = iIndex; i < iIndex + iStep && i < getDates().size(); i++) {
 			final Integer date = getDates().get(i);
 			final P day = new P("room");
+			Roles.getColumnheaderRole().set(day.getElement());
 			final Date d = iDates.getDate(date);
 			day.setHTML(MESSAGES.dateTimeHeader(sDayOfWeek.format(d), sDateFormat.format(d), TimeUtils.slot2short(getStartSlot()), TimeUtils.slot2short(getEndSlot())));
 			row.add(day);
@@ -1083,9 +1092,11 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 		
 		for (int ri = 0; ri < getRooms().size(); ri++) {
 			row = new P("row"); box.add(row);
+			Roles.getRowRole().set(row.getElement());
 			
 			final Entity room = getRooms().get(ri);
 			final P prm = new P("date");
+			Roles.getRowheaderRole().set(prm.getElement());
 			String displayName = room.getProperty("display", null);
 			prm.setHTML(MESSAGES.singleRoomSelection(room.getName() + (displayName != null && !displayName.isEmpty() ? "<br>" + displayName : ""), room.getProperty("type", null), room.getProperty("capacity", null)));
 			if ("1".equals(room.getProperty("ignoreRoomCheck", "0")))
@@ -1138,6 +1149,7 @@ public class AddMeetingsDialog extends UniTimeDialogBox {
 				final Date d = iDates.getDate(date);
 				
 				final P p = new P("cell");
+				Roles.getGridcellRole().set(p.getElement());
 				
 				if (conflicts == null || conflicts.isEmpty()) {
 					p.addStyleName("free");

JavaSource/org/unitime/timetable/gwt/client/events/EventAdd.java
@@ -26,6 +26,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.unitime.timetable.gwt.client.Lookup;
+import org.unitime.timetable.gwt.client.aria.ImageButton;
 import org.unitime.timetable.gwt.client.events.AcademicSessionSelectionBox.AcademicSession;
 import org.unitime.timetable.gwt.client.events.EventMeetingTable.EventMeetingRow;
 import org.unitime.timetable.gwt.client.events.EventMeetingTable.OperationType;
@@ -47,6 +48,7 @@ import org.unitime.timetable.gwt.client.widgets.UniTimeWidget;
 import org.unitime.timetable.gwt.command.client.GwtRpcResponseList;
 import org.unitime.timetable.gwt.command.client.GwtRpcService;
 import org.unitime.timetable.gwt.command.client.GwtRpcServiceAsync;
+import org.unitime.timetable.gwt.resources.GwtAriaMessages;
 import org.unitime.timetable.gwt.resources.GwtConstants;
 import org.unitime.timetable.gwt.resources.GwtMessages;
 import org.unitime.timetable.gwt.resources.GwtResources;
@@ -105,7 +107,6 @@ import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasValue;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.MenuBar;
@@ -120,6 +121,7 @@ import com.google.gwt.user.client.ui.Widget;
  * @author Tomas Muller
  */
 public class EventAdd extends Composite implements EventMeetingTable.Implementation, AcademicSessionSelectionBox.AcademicSessionFilter {
+	private static final GwtAriaMessages ARIA = GWT.create(GwtAriaMessages.class);
 	private static final GwtRpcServiceAsync RPC = GWT.create(GwtRpcService.class);
 	private static final GwtResources RESOURCES = GWT.create(GwtResources.class);
 	private static final GwtMessages MESSAGES = GWT.create(GwtMessages.class);
@@ -206,7 +208,8 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 					row.add(new Label(contact.getName(MESSAGES), false));
 					row.add(new Label(contact.hasEmail() ? contact.getEmail() : "", false));
 					row.add(new Label(contact.hasPhone() ? contact.getPhone() : "", false));
-					Image remove = new Image(RESOURCES.delete());
+					ImageButton remove = new ImageButton(RESOURCES.delete());
+					remove.setAriaLabel(ARIA.buttonDeleteThisLine());
 					remove.addStyleName("remove");
 					remove.addClickHandler(new ClickHandler() {
 						@Override
@@ -634,6 +637,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 		iStandardNotesBox.addStyleName("unitime-StandardNotesBox");
 		iStandardNotesBox.setText(MESSAGES.dialogStandardNotes());
 		iStandardNotesBox.setWidget(standardNotesForm);
+		Roles.getListboxRole().setAriaLabelledbyProperty(iStandardNotes.getElement(), Id.of(iStandardNotesBox.getCaption().asWidget().getElement()));
 		standardNotesFooter.addButton("select", MESSAGES.buttonSelect(), new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
@@ -1295,7 +1299,8 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 				row.add(new Label(contact.getName(MESSAGES), false));
 				row.add(new Label(contact.hasEmail() ? contact.getEmail() : "", false));
 				row.add(new Label(contact.hasPhone() ? contact.getPhone() : "", false));
-				Image remove = new Image(RESOURCES.delete());
+				ImageButton remove = new ImageButton(RESOURCES.delete());
+				remove.setAriaLabel(ARIA.buttonDeleteThisLine());
 				remove.addStyleName("remove");
 				remove.addClickHandler(new ClickHandler() {
 					@Override
@@ -1487,6 +1492,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			final CourseRelatedObjectLine line = new CourseRelatedObjectLine();
 			
 			final ListBox subject = new ListBox();
+			Roles.getListboxRole().setAriaLabelProperty(subject.getElement(), MESSAGES.colSubject());
 			subject.addStyleName("subject");
 			subject.addItem("-", "");
 			subject.setSelectedIndex(0);
@@ -1515,6 +1521,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			row.add(subject);
 			
 			final ListBox course = new ListBox();
+			Roles.getListboxRole().setAriaLabelProperty(course.getElement(), MESSAGES.colCourseNumber());
 			course.addStyleName("course");
 			course.addItem(MESSAGES.itemNotApplicable(), "");
 			course.setSelectedIndex(0);
@@ -1564,6 +1571,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			row.add(course);
 			
 			final ListBox subpart = new ListBox();
+			Roles.getListboxRole().setAriaLabelProperty(subpart.getElement(), MESSAGES.colConfigOrSubpart());
 			subpart.addStyleName("subpart");
 			subpart.addItem(MESSAGES.itemNotApplicable(), "");
 			subpart.setSelectedIndex(0);
@@ -1631,6 +1639,7 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			row.add(subpart);
 			
 			final ListBox clazz = new ListBox();
+			Roles.getListboxRole().setAriaLabelProperty(clazz.getElement(), MESSAGES.colClassNumber());
 			clazz.addStyleName("class");
 			clazz.addItem(MESSAGES.itemNotApplicable(), "");
 			clazz.setSelectedIndex(0);
@@ -1686,8 +1695,8 @@ public class EventAdd extends Composite implements EventMeetingTable.Implementat
 			
 			row.add(clazz);
 			
-			Image remove = new Image(RESOURCES.delete());
-			remove.addStyleName("remove");
+			ImageButton remove = new ImageButton(RESOURCES.delete());
+			remove.setAriaLabel(ARIA.buttonDeleteThisLine());
 			remove.addClickHandler(new ClickHandler() {
 				@Override
 				public void onClick(ClickEvent event) {

JavaSource/org/unitime/timetable/gwt/client/events/EventFilterBox.java
@@ -44,6 +44,8 @@ import org.unitime.timetable.gwt.shared.EventInterface.FilterRpcResponse;
 import org.unitime.timetable.gwt.shared.EventInterface.RequestSessionDetails;
 import org.unitime.timetable.gwt.shared.EventInterface.SessionMonth;
 
+import com.google.gwt.aria.client.Id;
+import com.google.gwt.aria.client.Roles;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
@@ -67,6 +69,7 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.CheckBox;
@@ -117,6 +120,7 @@ public class EventFilterBox extends UniTimeFilterBox<EventFilterRpcRequest> {
 		iSponsors = new ListBox();
 		iSponsors.setMultipleSelect(true);
 		iSponsors.setWidth("100%"); iSponsors.setVisibleItemCount(3);
+		Roles.getListboxRole().setAriaLabelProperty(iSponsors.getElement(), MESSAGES.tagSponsor());
 		
 		addFilter(new FilterBox.CustomFilter("sponsor", MESSAGES.tagSponsor(), iSponsors) {
 			@Override
@@ -216,6 +220,7 @@ public class EventFilterBox extends UniTimeFilterBox<EventFilterRpcRequest> {
 		iServices = new ListBox();
 		iServices.setMultipleSelect(false);
 		iServices.setWidth("100%");
+		Roles.getListboxRole().setAriaLabelProperty(iServices.getElement(), MESSAGES.tagService());
 		
 		addFilter(new FilterBox.CustomFilter("service", MESSAGES.tagService(), iServices) {
 			@Override
@@ -263,10 +268,12 @@ public class EventFilterBox extends UniTimeFilterBox<EventFilterRpcRequest> {
 		});
 		
 		Label reqLab = new Label(MESSAGES.propRequestedBy());
+		reqLab.getElement().setId(DOM.createUniqueId());
 
 		iRequested = new AriaSuggestBox(new RequestedByOracle());
 		iRequested.setStyleName("unitime-TextArea");
 		iRequested.setWidth("200px");
+		Roles.getTextboxRole().setAriaLabelledbyProperty(iRequested.getValueBox().getElement(), Id.of(reqLab.getElement()));
 		
 		final CheckBox conflicts = new CheckBox(MESSAGES.checkDisplayConflicts());
 		conflicts.getElement().getStyle().setMarginLeft(10, Unit.PX);

JavaSource/org/unitime/timetable/gwt/client/events/EventMeetingTable.java
@@ -2308,14 +2308,6 @@ public class EventMeetingTable extends UniTimeTable<EventMeetingTable.EventMeeti
 			showLine(0, false);
 		}
 		
-		public MultiLinePCell(P... values) {
-			super("multiple-lines");
-			iValue = new ArrayList<P>();
-			for (P value: values)
-				iValue.add(value);
-			showLine(0, false);
-		}
-		
 		public void showLine(int line, boolean hasNext) {
 			clear();
 			add(line >= 0 && line < iValue.size() ? iValue.get(line) : new P("blank-line"));

JavaSource/org/unitime/timetable/gwt/client/events/RoomFilterBox.java
@@ -32,6 +32,8 @@ import org.unitime.timetable.gwt.shared.AcademicSessionProvider;
 import org.unitime.timetable.gwt.shared.EventInterface.FilterRpcResponse;
 import org.unitime.timetable.gwt.shared.EventInterface.RoomFilterRpcRequest;
 
+import com.google.gwt.aria.client.Id;
+import com.google.gwt.aria.client.Roles;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
@@ -52,6 +54,7 @@ import com.google.gwt.event.dom.client.MouseDownHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Label;
@@ -79,6 +82,7 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 		iDepartments = new ListBox();
 		iDepartments.setMultipleSelect(false);
 		iDepartments.setWidth("100%");
+		Roles.getListboxRole().setAriaLabelProperty(iDepartments.getElement(), MESSAGES.tagDepartment());
 		
 		iDepartmentFilter = new FilterBox.CustomFilter("department", MESSAGES.tagDepartment(), iDepartments) {
 			@Override
@@ -161,6 +165,7 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 		iBuildings = new ListBox();
 		iBuildings.setMultipleSelect(true);
 		iBuildings.setWidth("100%"); iBuildings.setVisibleItemCount(3);
+		Roles.getListboxRole().setAriaLabelProperty(iBuildings.getElement(), MESSAGES.tagBuilding());
 		
 		addFilter(new FilterBox.CustomFilter("building", MESSAGES.tagBuilding(), iBuildings) {
 			@Override
@@ -215,17 +220,21 @@ public class RoomFilterBox extends UniTimeFilterBox<RoomFilterRpcRequest> {
 		});
 		
 		Label l1 = new Label(MESSAGES.propMin());
+		l1.getElement().setId(DOM.createUniqueId());
 
 		iMin = new TextBox();
 		iMin.setStyleName("unitime-TextArea");
 		iMin.setMaxLength(10); iMin.getElement().getStyle().setWidth(50, Unit.PX);
+		Roles.getTextboxRole().setAriaLabelledbyProperty(iMin.getElement(), Id.of(l1.getElement()));
 		
 		Label l2 = new Label(MESSAGES.propMax());
+		l2.getElement().setId(DOM.createUniqueId());
 		l2.getElement().getStyle().setMarginLeft(10, Unit.PX);
 
 		iMax = new TextBox();
 		iMax.setMaxLength(10); iMax.getElement().getStyle().setWidth(50, Unit.PX);
 		iMax.setStyleName("unitime-TextArea");
+		Roles.getTextboxRole().setAriaLabelledbyProperty(iMax.getElement(), Id.of(l2.getElement()));
 		
 		final CheckBox events = new CheckBox(MESSAGES.checkOnlyEventLocations());
 		events.getElement().getStyle().setMarginLeft(10, Unit.PX);

commit d1288fca9db663bbeb33ccfaa638b71c825af96e

Events: Time Grid
- fixed an issue in the start/end slot computation that caused Monday meetings to be ignored
- introduced accidentally in commit 0d797d9

JavaSource/org/unitime/timetable/gwt/client/events/EventResourceTimetable.java
@@ -575,6 +575,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 					if (event.getType() == EventType.Unavailabile && !iEvents.hasChip(new Chip("type", "Not Available"))) continue;
 					for (MeetingInterface meeting: event.getMeetings()) {
 						if (filterEvent(event, meeting)) continue;
+						if (meeting.isArrangeHours()) continue;
 						if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue;
 						if (firstSlot > meeting.getStartSlot()) firstSlot = meeting.getStartSlot();
 						if (lastSlot < meeting.getEndSlot()) lastSlot = meeting.getEndSlot();
@@ -920,7 +921,7 @@ public class EventResourceTimetable extends Composite implements EventMeetingTab
 				if (event.getType() == EventType.Unavailabile && !iEvents.hasChip(new Chip("type", "Not Available"))) continue;
 				for (MeetingInterface meeting: event.getMeetings()) {
 					if (filterEvent(event, meeting)) continue;
-					if (meeting.getDayOfWeek() == 0) continue;
+					if (meeting.isArrangeHours()) continue;
 					if (meeting.getApprovalStatus() != ApprovalStatus.Pending && meeting.getApprovalStatus() != ApprovalStatus.Approved) continue;
 					if (firstSlot > meeting.getStartSlot()) firstSlot = meeting.getStartSlot();
 					if (lastSlot < meeting.getEndSlot()) lastSlot = meeting.getEndSlot();